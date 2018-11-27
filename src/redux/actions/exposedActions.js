a64e3ec9904b079ad3eb2cd5adf2f9f0b0e20311
Sascha Bieberstein <sabieber@users.noreply.github.com>
2018-11-27 21:04:09 +0100

    [update] Fires events when the visibility of an element changes (#47)
    * Fires events when the visibility of an element changes
    
    The details of the event include the element itself and the new visibility of that element.
    
    * Code lint
    
    * Prevents spamming the visibility change event unnecessarily
    
    * Fires the event when active left panel is changed
    
    * Also fires visibility event for active panel upon opening sidebar

--- src/redux/actions/exposedActions.js
+++ src/redux/actions/exposedActions.js
@@ -1,19 +1,31 @@
 import isDataElementPanel from 'helpers/isDataElementPanel';
+import { fireEvent } from 'helpers/loadDocument';
 
 // viewer
 export const enableAllElements = () => ({ type: 'ENABLE_ALL_ELEMENTS', payload: {} });
 export const openElement = dataElement => (dispatch, getState) => {
   const state = getState();
 
-  if (state.viewer.disabledElements[dataElement] && state.viewer.disabledElements[dataElement].disabled) {
+  const isElementDisabled = state.viewer.disabledElements[dataElement] && state.viewer.disabledElements[dataElement].disabled;
+  const isElementOpen = isDataElementPanel(dataElement, state) ? state.viewer.activeLeftPanel === dataElement : state.viewer.openElements[dataElement];
+
+  if (isElementDisabled || isElementOpen) {
     return;
   }
 
   if (isDataElementPanel(dataElement, state)) {
-    dispatch({ type: 'OPEN_ELEMENT', payload: { dataElement: 'leftPanel' } });
-    dispatch({ type: 'SET_ACTIVE_LEFT_PANEL', payload: { dataElement } });
+    if (!state.viewer.openElements['leftPanel']) {
+      dispatch({ type: 'OPEN_ELEMENT', payload: { dataElement: 'leftPanel' } });
+      fireEvent('visibilityChanged', { element: 'leftPanel', isVisible: true });
+    }
+    dispatch(setActiveLeftPanel(dataElement));
   } else {
     dispatch({ type: 'OPEN_ELEMENT', payload: { dataElement } });
+    fireEvent('visibilityChanged', { element: dataElement, isVisible: true });
+
+    if (dataElement === 'leftPanel'  && !state.viewer.openElements['leftPanel']) {
+      fireEvent('visibilityChanged', { element: state.viewer.activeLeftPanel, isVisible: true });
+    }
   }
 };
 export const openElements = dataElements => dispatch => {
@@ -24,14 +36,23 @@ export const openElements = dataElements => dispatch => {
 export const closeElement = dataElement => (dispatch, getState) => {
   const state = getState();
 
-  if (state.viewer.disabledElements[dataElement] && state.viewer.disabledElements[dataElement].disabled) {
+  const isElementDisabled = state.viewer.disabledElements[dataElement] && state.viewer.disabledElements[dataElement].disabled;
+  const isElementClosed = isDataElementPanel(dataElement, state) ? state.viewer.activeLeftPanel !== dataElement : !state.viewer.openElements[dataElement];
+
+  if (isElementDisabled || isElementClosed) {
     return;
   }
 
   if (isDataElementPanel(dataElement, state) && state.viewer.openElements['leftPanel']) {
     dispatch({ type: 'CLOSE_ELEMENT', payload: { dataElement: 'leftPanel' } });
-  } else if (state.viewer.openElements[dataElement]) {
+    fireEvent('visibilityChanged', { element: 'leftPanel', isVisible: false });
+  } else {
     dispatch({ type: 'CLOSE_ELEMENT', payload: { dataElement } });
+    fireEvent('visibilityChanged', { element: dataElement, isVisible: false });
+
+    if (dataElement === 'leftPanel'  && state.viewer.openElements['leftPanel']) {
+      fireEvent('visibilityChanged', { element: state.viewer.activeLeftPanel, isVisible: false });
+    }
   }
 };
 export const closeElements = dataElements => dispatch => {
@@ -46,7 +67,7 @@ export const toggleElement = dataElement => (dispatch, getState) => {
     return;
   }
 
-  if (state.viewer.openElements[dataElement]) { 
+  if (state.viewer.openElements[dataElement]) {
     dispatch(closeElement(dataElement));
   } else {
     dispatch(openElement(dataElement));
@@ -58,7 +79,12 @@ export const setActiveLeftPanel = dataElement => (dispatch, getState) => {
   const state = getState();
 
   if (isDataElementPanel(dataElement, state)) {
-    dispatch({ type: 'SET_ACTIVE_LEFT_PANEL', payload: { dataElement } });
+    if (state.viewer.activeLeftPanel !== dataElement) {
+      dispatch({ type: 'CLOSE_ELEMENT', payload: { dataElement: state.viewer.activeLeftPanel } });
+      fireEvent('visibilityChanged', { element: state.viewer.activeLeftPanel, isVisible: false });
+      dispatch({ type: 'SET_ACTIVE_LEFT_PANEL', payload: { dataElement } });
+      fireEvent('visibilityChanged', { element: dataElement, isVisible: true });
+    }
   } else {
     const panelDataElements = [
       ...state.viewer.customPanels.map(({ panel }) => panel.dataElement),
