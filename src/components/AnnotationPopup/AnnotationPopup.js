7daf46149c3c639e827b8e397980397ebde9618e
unknown <ayip@pdftron.com>
2018-12-06 13:30:32 -0800

    Undo removing "closeElement" call from deselect event

--- src/components/AnnotationPopup/AnnotationPopup.js
+++ src/components/AnnotationPopup/AnnotationPopup.js
@@ -97,6 +97,7 @@ class AnnotationPopup extends React.PureComponent {
         canModify: core.canModify(annotation)
       });
     } else {
+      //this.props.closeElement('annotationPopup');
       this.setState({ ...this.initialState });
     }
   }
