26d7685095f2b79bf49e10a13a4638b75f03f8e3
justinjung04 <justinjung04@gmail.com>
2018-12-18 10:01:13 -0800

    Fixed error in closeDocument

--- src/apis/closeDocument.js
+++ src/apis/closeDocument.js
@@ -3,6 +3,6 @@ import actions from 'actions';
 
 export default ({ dispatch }) => () => core.closeDocument(dispatch).then(() => {
   dispatch(actions.setDocumentLoaded(false));
-  store.dispatch(actions.closeElements([ 'pageNavOverlay', 'passwordModal' ]));
+  dispatch(actions.closeElements([ 'pageNavOverlay', 'passwordModal' ]));
   dispatch(actions.setOutlines([]));
 });
