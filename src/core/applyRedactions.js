8b403092c9526c16e58eacc85be9d138ee22d13a
unknown <ayip@pdftron.com>
2018-12-28 12:57:18 -0800

    Update comments

--- src/core/applyRedactions.js
+++ src/core/applyRedactions.js
@@ -1,5 +1,10 @@
 /**
  * https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#applyRedactions__anchor
+ * @fires deleteAnnotations on AnnotationManger if redaction is on top of another annotation
+ * @see https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#deleteAnnotations__anchor
+ * @fires annotationChanged on AnnotationManager if delete was fired
+ * @see https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#event:annotationChanged__anchor
+ * @fires notify on DocumentViewer if requires delete notification
  */
 export default annotations =>  {
     window.docViewer.getAnnotationManager().applyRedactions(annotations);
--- src/core/isFullPDFEnabled.js
+++ src/core/isFullPDFEnabled.js
@@ -1,3 +1,4 @@
 /**
+ * Check to see if the full API is enabled
  */
 export default () => window.CoreControls.isFullPDFEnabled();
