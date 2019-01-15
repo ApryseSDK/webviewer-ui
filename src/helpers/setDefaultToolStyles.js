03328c5575d749f306fd6ef7bf62ef7aa375874c
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-01-15 13:24:24 -0800

    add tests for it, refactor some of the code in setDefaultToolStyles.js

--- src/helpers/setDefaultToolStyles.js
+++ src/helpers/setDefaultToolStyles.js
@@ -1,4 +1,5 @@
 import core from 'core';
+import defaultToolStylesMap from 'constants/defaultToolStylesMap';
 
 const setDefaultToolStyles = () => {
   const toolModeMap = core.getToolModeMap();
@@ -6,26 +7,28 @@ const setDefaultToolStyles = () => {
   Object.keys(toolModeMap).forEach(toolName => {
     let toolStyles = localStorage.getItem(`toolData-${toolName}`);
 
-    if (toolStyles) {
-      toolStyles = getParsedToolStyles(toolStyles);  
-    } else {
-      toolStyles = defaultToolStylesMap[toolName];
+    if (!toolStyles && defaultToolStylesMap[toolName]) {
+      toolStyles = JSON.stringify(defaultToolStylesMap[toolName]);
     }
 
     if (toolStyles) {
       const tool = toolModeMap[toolName];
+
+      toolStyles = getParsedToolStyles(toolStyles);
       tool.setStyles(oldStyle => ({
         ...oldStyle,
         ...toolStyles
       }));
     }
+
+
   });
 };
 
 const getParsedToolStyles = toolStyles => {
   return JSON.parse(toolStyles, (_, styles) => {
     Object.entries(styles).forEach(([ key, style ]) => {
-      if (typeof style === 'object') {
+      if (isKeyColorProperty(key) && typeof style === 'object') {
         styles[key] = new window.Annotations.Color(style.R, style.G, style.B, style.A); 
       }
     });
@@ -34,55 +37,6 @@ const getParsedToolStyles = toolStyles => {
   });
 };
 
-const convertHexToColor = hexColor => {
-  const rgba = hexColor.match(/.{2}/g).map(c => parseInt(c, 16));
-
-  return new window.Annotations.Color(rgba[0], rgba[1], rgba[2], rgba[3]);
-};
-
-// default styles of a tool is determined by core
-// following are the tools that we want to change the default styles due to design
-// we could change the default styles in core but we don't want the legacy UI to be affected, so we do it here
-const defaultToolStylesMap = {
-  AnnotationCreateFreeHand: {
-    StrokeColor: convertHexToColor('E44234'),
-  },
-  AnnotationCreateFreeHand2: {
-    StrokeColor: convertHexToColor('00CC63'),
-  },
-  AnnotationCreateFreeHand3: {
-    StrokeColor: convertHexToColor('4E7DE9'),
-  },
-  AnnotationCreateFreeHand4: {
-    StrokeColor: convertHexToColor('000000'),
-  },
-  AnnotationCreateTextHighlight: {
-    StrokeColor: convertHexToColor('FFE6A2'),
-  },
-  AnnotationCreateTextHighlight2: {
-    StrokeColor: convertHexToColor('92E8E8'),
-  },
-  AnnotationCreateTextHighlight3: {
-    StrokeColor: convertHexToColor('80E5B1'),
-  },
-  AnnotationCreateTextHighlight4: {
-    StrokeColor: convertHexToColor('F1A099'),
-  },
-  AnnotationCreateTextUnderline: {
-    StrokeColor: convertHexToColor('E44234'),
-  },
-  AnnotationCreateTextSquiggly: {
-    StrokeColor: convertHexToColor('E44234'),
-  },
-  AnnotationCreateTextStrikeout: {
-    StrokeColor: convertHexToColor('E44234'),
-  },
-  AnnotationCreateFreeText: {
-    TextColor: convertHexToColor('E44234'),
-  },
-  AnnotationCreateSticky: {
-    StrokeColor: convertHexToColor('FFE6A2')
-  },
-};
+const isKeyColorProperty = key => ['TextColor', 'StrokeColor', 'BorderColor'].includes(key);
 
 export default setDefaultToolStyles;
