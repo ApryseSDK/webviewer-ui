112e3fafe4316b4be5a9d9654859a3a414b6cedc
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-02-01 13:35:13 -0800

    [update] resolves #153

--- src/constants/supportedFiles.js
+++ src/constants/supportedFiles.js
@@ -1,3 +1,5 @@
 export const supportedPDFExtensions = ['pdf', 'jpg', 'jpeg', 'png'];
 export const supportedOfficeExtensions = ['docx', 'xlsx', 'pptx', 'md'];
 export const supportedBlackboxExtensions = ['docx', 'xlsx', 'pptx', 'doc', 'xls', 'csv', 'ppt', 'htm', 'html', 'tif', 'tiff', 'jp2', 'md', 'txt', 'pdf', 'jpg', 'jpeg', 'png', 'rtf', 'odf', 'odt', 'odg', 'odp', 'ods', 'dwg', 'dgn', 'dxf'];
+export const supportedExtensions = [...supportedPDFExtensions, ...supportedOfficeExtensions, ...supportedBlackboxExtensions, 'xod'].filter((extension, index, self) => self.indexOf(extension) === index);
+export const supportedClientOnlyExtensions = [...supportedPDFExtensions, ...supportedOfficeExtensions];
