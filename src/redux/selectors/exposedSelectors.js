ed08a61427e80d06dfa8050101b40f5b764fec36
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-02-07 15:39:27 -0800

    [new] Add a new API for custom notes filtering in the notes panel (#158)

--- src/redux/selectors/exposedSelectors.js
+++ src/redux/selectors/exposedSelectors.js
@@ -65,6 +65,7 @@ export const getColorMap = state => state.viewer.colorMap;
 export const getCurrentPalette = (state, colorMapKey) => state.viewer.colorMap[colorMapKey] && state.viewer.colorMap[colorMapKey].currentPalette;
 export const getIconColor = (state, colorMapKey) => state.viewer.colorMap[colorMapKey] && state.viewer.colorMap[colorMapKey].iconColor; 
 export const getSwipeOrientation = state => state.viewer.swipeOrientation;
+export const getCustomNoteFilter = state => state.viewer.customNoteFilter;
 
 // document
 export const getDocument = state => state.document;
