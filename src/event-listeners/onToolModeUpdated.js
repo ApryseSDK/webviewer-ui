008d944d964f4abfe4f3eba5cf37115bb48f383b
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-02-22 05:38:28 -0800

    close zoomOverlay when tool mode is switched to Marquee zoom

--- src/event-listeners/onToolModeUpdated.js
+++ src/event-listeners/onToolModeUpdated.js
@@ -13,6 +13,10 @@ export default dispatch => (e, newTool, oldTool) => {
     dispatch(actions.closeElement('toolsOverlay'));
   }
 
+  if (newTool && newTool.name === 'MarqueeZoomTool') {
+    dispatch(actions.closeElement('zoomOverlay'));
+  }
+
   dispatch(actions.setActiveToolNameAndStyle(newTool));
   $(document).trigger('toolModeChanged', [newTool, oldTool]);
 };
