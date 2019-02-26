d88b3b5b24ab2418dac088a6791e6e5525891a1c
unknown <ayip@pdftron.com>
2019-02-26 12:55:03 -0800

    Fix for using redaction with webViewer Server

---
+++ src/core/isApplyRedactionEnabled.js
@@ -0,0 +1,5 @@
+/**
+ * https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#isApplyRedactionEnabled__anchor
+ * @see https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#event:isApplyRedactionEnabled__anchor
+ */
+export default () => window.docViewer.getAnnotationManager().isApplyRedactionEnabled();
