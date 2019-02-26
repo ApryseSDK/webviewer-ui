3f8b72eeeac90af78f4311dca6d96aa3d84c78a2
AndrewYip <45575633+awy3@users.noreply.github.com>
2019-02-26 09:44:05 -0800

    [bugfix] Fix translation bugs (#169)
    * Fix translation bugs
    
    * Small text and style changes

--- src/helpers/applyRedactions.js
+++ src/helpers/applyRedactions.js
@@ -28,8 +28,8 @@ const webViewerServerApply = (annotations, dispatch) => {
 };
 
 const webViewerApply = (annotations, dispatch) => {
-    const message = i18next.t('message.applyRedactionWarningMessage');
-    const title = i18next.t('message.applyRedactionWarningTitle');
+    const message = i18next.t('option.redaction.warningPopupMessage');
+    const title = i18next.t('option.redaction.warningPopupTitle');
     const confirmBtnText = i18next.t('action.apply');
 
     const warning = {
