b424cc746b1b17f43032537452ae76391cb6fd61
unknown <ayip@pdftron.com>
2019-03-01 09:22:31 -0800

    reordering some code

--- src/apis/enableAnnotations.js
+++ src/apis/enableAnnotations.js
@@ -17,13 +17,12 @@ export default store => (enable = true) =>  {
     if (!core.isCreateRedactionEnabled()) {
       elements = elements.filter(ele => ele !== 'redactionButton');
     }
-
-    core.showAnnotations(core.getAnnotationsList());
     getAnnotationCreateToolNames().forEach(toolName => {
       core.getTool(toolName).disabled = false;
     });
 
     store.dispatch(actions.enableElements(elements, PRIORITY_ONE));
+    core.showAnnotations(core.getAnnotationsList());
 
   } else {
     console.warn('enableAnnotations(false) is deprecated, please use disableAnnotations() instead');
