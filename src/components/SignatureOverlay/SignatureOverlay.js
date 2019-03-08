3635a51241d896fa9dc6c27341f9e657d48da6b6
ZhijieZhang <zhijiezhang0124@gmail.com>
2019-03-07 16:06:04 -0800

    [update] Update comment in SignatureOverlay

--- src/components/SignatureOverlay/SignatureOverlay.js
+++ src/components/SignatureOverlay/SignatureOverlay.js
@@ -60,8 +60,10 @@ class SignatureOverlay extends React.PureComponent {
       freeHandAnnot && !freeHandAnnot.getPaths().length
     ) {
       // location of signatureTool will be set when clicking on a signature widget
-      // we want to clear location when the overlay is closed without selecting any default signatures 
+      // we want to clear location when the overlay is closed without any default signatures selected
       // to prevent signature from being drawn to the previous location
+      // however the overlay will be closed without any default signature selected if we clicked the "add signature" button(which opens the signature modal)
+      // we don't want to clear the location in the case because we still want the signature to be automatically added to the widget after the create button is hit in the modal 
       this.signatureTool.clearLocation();
     }
   }
