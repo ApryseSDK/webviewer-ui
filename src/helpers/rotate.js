70c479ff94ac3e30032c821ad566c4baa54be826
Andy <sd.s0uldrag0n@gmail.com>
2019-03-12 14:21:31 -0700

    [bugfix] resolves #179 (#193)
    * [bugfix] resolves #179
    
    Position sorting now sorts using the closest edge to the top.
    Page rotation amount in radians is now available in the state.
    Changed sort strategy API to accept a contextual parameter.
    
    * Reverted sort strategy breaking changes.
    Changed strategy to get the rotation value from helper instead.
    Changed rotation state from radians to orientation value.
    
    * Reverted missed time strategy change.

---
+++ src/helpers/rotate.js
@@ -0,0 +1,20 @@
+export const rotateRad = (cx, cy, x, y, radians) => {
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx; // xcos(theta) + ysin(theta)
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy; // -ysin(theta) + xcos(theta)
+
+    return { x: nx, y: ny };
+};
+
+export const rotateDeg = (cx, cy, x, y, angle) => {
+    const radians = (Math.PI/180) * angle;
+    const s = Math.sin(radians);
+    const c = Math.cos(radians);
+
+    const nx = (c * (x - cx)) + (s * (y - cy)) + cx;
+    const ny = (c * (y - cy)) - (s * (x - cx)) + cy;
+
+    return { x: nx, y: ny };
+};
