cc3b1d623998616a96c64494bca2fe7c61e35ff5
Sardor Isakov <sisardor@gmail.com>
2019-03-13 17:02:55 -0700

    Update rename constant `documentTypes` to `workerTypes`

--- src/helpers/documentTypeParamToEngineType.js
+++ src/helpers/documentTypeParamToEngineType.js
@@ -1,11 +1,11 @@
-import { engineTypes, documentTypes } from 'constants/types';
+import { engineTypes, workerTypes } from 'constants/types';
 
 export default (documentType, pdftronServer) => {
   if (pdftronServer) {
     return engineTypes.PDFTRON_SERVER;
-  } else if (documentType === documentTypes.PDF || documentType === documentTypes.OFFICE || documentType === documentTypes.ALL) {
+  } else if (documentType === workerTypes.PDF || documentType === workerTypes.OFFICE || documentType === workerTypes.ALL) {
     return engineTypes.PDFNETJS;
-  } else if (documentType === documentTypes.XOD) {
+  } else if (documentType === workerTypes.XOD) {
     return engineTypes.UNIVERSAL;
   } else {
     return engineTypes.AUTO;
