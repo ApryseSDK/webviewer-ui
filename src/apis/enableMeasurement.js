0c48fdf6607c94b41571c7844dc6ec2012c486b4
justinjung04 <justinjung04@gmail.com>
2019-03-14 11:41:07 -0700

    Enable measurement overlay with enableMeasurement API

--- src/apis/enableMeasurement.js
+++ src/apis/enableMeasurement.js
@@ -2,5 +2,5 @@ import { PRIORITY_ONE } from 'constants/actionPriority';
 import actions from 'actions';
 
 export default store => () => {
-  store.dispatch(actions.enableElement('measurementToolGroupButton', PRIORITY_ONE));
+  store.dispatch(actions.enableElements([ 'measurementToolGroupButton', 'measurementOverlay' ], PRIORITY_ONE));
 };
