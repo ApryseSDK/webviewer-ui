cdc55dbb5c49ca09bd3ec2b21487ce923531297d
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-03-25 13:04:37 -0700

    MeasurementsOverlay Refactor (#216)
    * Remove isTool prop in StylePopup. Instead we check if props.style has Scale and Precision to decide if we should render MeasurementsOverlay. Also add proptypes to MeasurementsOverlay
    
    * Refactoring MeasurementsOverlay
    
    * [bugfix] Fixed issue where merge conflicts didn't get resolved properly in StylePopup
    
    * rename MeasurementsOverlay to MeasurementOption
    
    * Improve some styling issues
    
    * Add space before opening bracket. Add more options and use inline functions
    
    * Set scales onChange instead of onBlur. This will fix an issue where changing the scale won't apply immediately to the tool and cause annotation to have the wrong scale
    
    * Change the signature for onScaleChange so that the parameter has the same form as this.props.scale
    
    * i18ned Scale and Precision
    
    * Add documentation for important props and some methods

--- src/components/MeasurementsDropdown/MeasurementsDropdown.js
+++ src/components/MeasurementsDropdown/MeasurementsDropdown.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import core from 'core';
-
 import MeasurementsDropdownItem from '../MeasurementsDropdownItem';
 
 class MeasurementsDropdown extends React.PureComponent {
-
   onClick = (e,item) => {
     e.stopPropagation();
     this.setState({ selectedItem: item });
@@ -15,6 +12,7 @@ class MeasurementsDropdown extends React.PureComponent {
   render() { 
     const { dropdownList, selectedItem } = this.props;
     const { isDropdownOpen } = this.props;
+
     let sortedDropdownList;
     if (window.innerWidth < 640){
       sortedDropdownList = dropdownList.filter(item => item !== selectedItem).concat([selectedItem]);
@@ -23,21 +21,18 @@ class MeasurementsDropdown extends React.PureComponent {
     }
     return (
     <div className="MeasurementsDropdown">  
-      { !isDropdownOpen && <MeasurementsDropdownItem 
-        onClick={
-          e=>{
+      {!isDropdownOpen && 
+        <MeasurementsDropdownItem 
+          onClick={e => {
             e.stopPropagation();
             this.props.onDropdownChange();
-          }
-        }
-        content={selectedItem}
-                           />
+          }}
+          content={selectedItem}
+        />
       }
-      { isDropdownOpen &&
+      {isDropdownOpen &&
         sortedDropdownList.map((item, i) => {
-          return <MeasurementsDropdownItem key={i} content={item} onClick={e=>{
-this.onClick(e,item);
-}} />;
+          return <MeasurementsDropdownItem key={i} content={item} onClick={e=> this.onClick(e,item)} />;
         })
       }
     </div>
