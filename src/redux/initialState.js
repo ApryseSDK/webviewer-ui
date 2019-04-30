b7d98dbcfca36102fe105234bf4cb271962ab12d
Ramzi Chennafi <rchennafi@pdftron.com>
2019-04-29 18:15:54 -0700

    Added single server mode for life check.

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -196,6 +196,7 @@ export default {
     engineType: documentTypeParamToEngineType(getHashParams('documentType'), getHashParams('pdftronServer', '')),
     fullAPI: getHashParams('pdfnet', false),
     pdftronServer: getHashParams('pdftronServer', ''),
+    singleServerMode: getHashParams('singleServerMode', ''),
     disableWebsockets: getHashParams('disableWebsockets', false),
     preloadWorker: getHashParams('preloadWorker', false),
     serverUrl: getHashParams('server_url', ''),
