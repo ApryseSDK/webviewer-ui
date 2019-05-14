0eb278a0809b72146b5917291d66c7f8026b93ec
Kristian Hein <khein@pdftron.com>
2019-05-13 18:45:16 -0700

    revert documentContainer

--- src/components/DocumentContainer/DocumentContainer.js
+++ src/components/DocumentContainer/DocumentContainer.js
@@ -93,7 +93,7 @@ class DocumentContainer extends React.PureComponent {
     handleWindowResize(this.props, this.container.current);
   }
 
-  onWheel = e => {
+  onWheel = e => {    
     if (e.metaKey || e.ctrlKey) {
       e.preventDefault();
       this.wheelToZoom(e);
@@ -121,7 +121,7 @@ class DocumentContainer extends React.PureComponent {
     const newPage = currentPage - getNumberOfPagesToNavigate(displayMode);
 
     core.setCurrentPage(Math.max(newPage, 1));
-    this.container.current.scrollTop = scrollHeight - clientHeight;
+    this.container.current.scrollTop = scrollHeight - clientHeight;    
   }
 
   pageDown = () => {
@@ -153,7 +153,7 @@ class DocumentContainer extends React.PureComponent {
 
   getClassName = props => {
     const { isLeftPanelOpen, isRightPanelOpen, isHeaderOpen, isSearchOverlayOpen } = props;
-
+    
     return [
       'DocumentContainer',
       isLeftPanelOpen ? 'left-panel' : '',
@@ -167,7 +167,7 @@ class DocumentContainer extends React.PureComponent {
     let className;
 
     if (isIE) {
-      className = getClassNameInIE(this.props);
+      className = getClassNameInIE(this.props);  
     } else {
       className = this.getClassName(this.props);
     }
@@ -194,13 +194,12 @@ const mapStateToProps = state => ({
   displayMode: selectors.getDisplayMode(state),
   totalPages: selectors.getTotalPages(state),
   swipeOrientation: selectors.getSwipeOrientation(state),
-  toolButtonObjects: selectors.getToolButtonObjects(state),
-  isInAccessibleMode: true,
+  toolButtonObjects: selectors.getToolButtonObjects(state)
 });
 
 const mapDispatchToProps = dispatch => ({
   dispatch,
-  openElement: dataElement => dispatch(actions.openElement(dataElement))
+  openElement: dataElement => dispatch(actions.openElement(dataElement)) 
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DocumentContainer);
