331f533eaf34171b2d715b391b37886e69681f0a
justinjung04 <justinjung04@gmail.com>
2019-06-04 10:46:29 -0700

    [update] Removed initial fit mode setting for consistency

--- src/index.js
+++ src/index.js
@@ -281,12 +281,6 @@ if (window.CanvasRenderingContext2D) {
           }
         };
 
-        if (window.innerWidth <= 640) {
-          core.fitToWidth();
-        } else {
-          core.fitToPage();
-        }
-
         $(document).trigger('viewerLoaded');
       }
     );
