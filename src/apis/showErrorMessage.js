a21dc422d48fe483fa2acc2d8b5b07586a66536a
justinjung04 <justinjung04@gmail.com>
2019-06-04 14:37:53 -0700

    [doc] added doc for showErrorMessage

--- src/apis/showErrorMessage.js
+++ src/apis/showErrorMessage.js
@@ -1,3 +1,22 @@
+/**
+ * Displays the custom error message
+ * @method WebViewer#showErrorMessage
+ * @param {string} message An error message
+ * @example // 5.1 and after
+WebViewer(...)
+  .then(function(instance) {
+    instance.showErrorMessage('My error message');
+  });
+ * @example // 4.0 ~ 5.0
+var viewerElement = document.getElementById('viewer');
+var viewer = new PDFTron.WebViewer(...);
+
+viewerElement.addEventListener('ready', function() {
+  var instance = viewer.getInstance();
+  instance.showErrorMessage('My error message');
+});
+ */
+
 import actions from 'actions';
 
 export default store => message => {
