1da7bcea550d1eb24eb587258bc6eeee2de5bbf3
khein-pdftron <khein@pdftron.com>
2019-06-13 14:17:50 -0700

    [bugfix] fixed issue where thumbnail weren't updated properly upon page operations (#275)
    * fixed issues
    
    * fixed bugs from testing

--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -53,13 +53,18 @@ class Thumbnail extends React.PureComponent {
   }
 
   onLayoutChanged(e, changes) {
-    const { contentChanged } = changes;
+    const { contentChanged, moved, added, removed } = changes;
     const { index } = this.props;
 
     const currentPage = index + 1;
-    const didLayoutChange = contentChanged.some(changedPage => currentPage + '' === changedPage);
+    const currentPageStr = currentPage + '';
 
-    if (didLayoutChange) {
+    const isPageAdded = added.indexOf(currentPage) > -1;
+    const didPageChange = contentChanged.some(changedPage => currentPageStr === changedPage);
+    const didPageMove = Object.keys(moved).some(movedPage => currentPageStr === movedPage);
+    const isPageRemoved = removed.indexOf(currentPage) > -1;
+
+    if (isPageAdded || didPageChange || didPageMove || isPageRemoved) {
       const { thumbContainer } = this;
       const { current } = thumbContainer;
 
