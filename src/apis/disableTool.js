2725bc9b6896a8aa74553fadfba43c220443f1da
James Park <james.park.victoria@gmail.com>
2019-06-13 15:25:43 -0700

    Revert deleted files

---
+++ src/apis/disableTool.js
@@ -0,0 +1,11 @@
+import core from 'core';
+import { PRIORITY_ONE } from 'constants/actionPriority';
+import actions from 'actions';
+import selectors from 'selectors';
+
+export default store => toolName => {
+  const dataElement = selectors.getToolButtonDataElement(store.getState(), toolName); 
+
+  store.dispatch(actions.disableElement(dataElement, PRIORITY_ONE));
+  core.getTool(toolName).disabled = true;
+};
