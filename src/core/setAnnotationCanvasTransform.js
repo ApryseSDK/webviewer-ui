bd16c9b4d991dbbe5a1e8d10155320383043f18e
justinjung04 <justinjung04@gmail.com>
2019-06-17 14:01:04 -0700

    temporary fix for unsupported core methods

--- src/core/setAnnotationCanvasTransform.js
+++ src/core/setAnnotationCanvasTransform.js
@@ -1,7 +1,9 @@
 /**
  * https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#setAnnotationCanvasTransform__anchor
- * @see https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#event:setAnnotationCanvasTransform__anchor
+ * @see https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#setAnnotationCanvasTransform__anchor
  */
 export default (annotCanvasContext, zoom, rotation) => {
-  window.docViewer.getAnnotationManager().setAnnotationCanvasTransform(annotCanvasContext, zoom, rotation);
+  if (window.docViewer.getAnnotationManager().setAnnotationCanvasTransform) {
+    window.docViewer.getAnnotationManager().setAnnotationCanvasTransform(annotCanvasContext, zoom, rotation);
+  }
 };
