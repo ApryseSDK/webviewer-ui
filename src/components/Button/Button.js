e33a84851ae52520ddf29392019786a0c129e5a6
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-07-05 11:52:58 -0700

    In the process of refactoring buttons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,63 +1,72 @@
 import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import Icon from 'components/Icon';
 
 import './Button.scss';
 
-class Button extends React.PureComponent {
-  static propTypes = {
-    isDisabled: PropTypes.bool,
-    isActive: PropTypes.bool,
-    mediaQueryClassName: PropTypes.string,
-    img: PropTypes.string,
-    label: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-    ]),
-    title: PropTypes.string,
-    color: PropTypes.string,
-    dataElement: PropTypes.string,
-    className: PropTypes.string,
-    onClick: PropTypes.func.isRequired,
+const Button = props => {
+  const {
+    isDisabled, 
+    isActive, 
+    mediaQueryClassName, 
+    img, 
+    label, 
+    color, 
+    dataElement,
+    onClick,
+    className
+  } = props;
+
+  
+  if (isDisabled) {
+    return null;
   }
+  
+  const handleClick = e => onClick(e);
+  const buttonClass = classNames({
+    Button: true,
+    active: isActive,
+    inactive: !isActive,
+    label,
+    icon: !label,
+    [mediaQueryClassName]: mediaQueryClassName,
+    [className]: className
+  });
+  const isBase64 = img && img.trim().indexOf('data:') === 0;
+  // if there is no file extension then assume that this is a glyph
+  const isGlyph = img && (img.indexOf('.') === -1 || img.indexOf('<svg') === 0) && !isBase64;
 
-  onClick = e => {
-    this.props.onClick(e);
-  }
+  return (
+    <div className={buttonClass} data-element={dataElement} onClick={handleClick}>
+      {isGlyph &&
+        <Icon glyph={img} color={color} />
+      }
+      {img && !isGlyph &&
+        <img src={img} />
+      }
+      {label &&
+        <p>{label}</p>
+      }
+    </div>
+  );
+};
 
-  render() {
-    const { isDisabled, isActive, mediaQueryClassName, img, label, color, dataElement } = this.props;
-
-    if (isDisabled) {
-      return null;
-    }
-
-    const className = [
-      'Button',
-      this.props.className ? this.props.className : '',
-      isActive ? 'active' : 'inactive',
-      label ? 'label' : 'icon',
-      mediaQueryClassName ? mediaQueryClassName : '',
-    ].join(' ').trim();
-    const isBase64 = img && img.trim().indexOf('data:') === 0;
-    // if there is no file extension then assume that this is a glyph
-    const isGlyph = img && (img.indexOf('.') === -1 || img.indexOf('<svg') === 0) && !isBase64;
-
-    return (
-      <div className={className} data-element={dataElement} onClick={this.onClick}>
-        {isGlyph &&
-          <Icon glyph={img} color={color} />
-        }
-        {img && !isGlyph &&
-          <img src={img} />
-        }
-        {label &&
-          <p>{label}</p>
-        }
-      </div>
-    );
-  }
-}
+Button.propTypes = {
+  isDisabled: PropTypes.bool,
+  isActive: PropTypes.bool,
+  mediaQueryClassName: PropTypes.string,
+  img: PropTypes.string,
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  title: PropTypes.string,
+  color: PropTypes.string,
+  dataElement: PropTypes.string,
+  className: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Button;
