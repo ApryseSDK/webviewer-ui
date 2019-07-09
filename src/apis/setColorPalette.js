5649d99b5d7c3a76c50536eaa4925f97addc1de6
ZhijieZhang <zhijiezhang0124@gmail.com>
2019-07-08 22:23:17 -0700

    Revert changes to setIconColor and setColorPalette

--- src/apis/setColorPalette.js
+++ src/apis/setColorPalette.js
@@ -1,13 +1,14 @@
 import actions from 'actions';
-import { getDataWithKey, mapToolNameToKey } from 'constants/map';
+import selectors from 'selectors';
 import mapPaletteToAnnotationColorProperty from 'constants/mapPaletteToAnnotationColorProperty';
 import mapAnnotationColorPropertyToPalette from 'constants/mapAnnotationColorPropertyToPalette';
 
 export default store => (toolName, colorPalette) => {
-  const availablePalettes = getDataWithKey(mapToolNameToKey(toolName)).availablePalettes;
+  const state = store.getState();
+  const availablePalettes = selectors.getAvailablePalettes(state, toolName);
   const property = mapPaletteToAnnotationColorProperty[colorPalette];
   if (availablePalettes.includes(property)) {
-    store.dispatch(actions.setColorPalette(mapToolNameToKey(toolName), property));
+    store.dispatch(actions.setColorPalette(toolName, mapPaletteToAnnotationColorProperty[colorPalette]));
   } else {
     console.warn(`${toolName} does not have ${colorPalette} color, available colors are: ${availablePalettes.map(palette => mapAnnotationColorPropertyToPalette[palette]).join(', ')} `);
   }
--- src/apis/setIconColor.js
+++ src/apis/setIconColor.js
@@ -24,15 +24,16 @@ viewerElement.addEventListener('ready', function() {
  */
 
 import actions from 'actions';
-import { getDataWithKey, mapToolNameToKey } from 'constants/map';
+import selectors from 'selectors';
 import mapPaletteToAnnotationColorProperty from 'constants/mapPaletteToAnnotationColorProperty';
 import mapAnnotationColorPropertyToPalette from 'constants/mapAnnotationColorPropertyToPalette';
 
 export default store => (toolName, colorPalette) => {
-  const availablePalettes = getDataWithKey(mapToolNameToKey(toolName)).availablePalettes;
+  const state = store.getState();
+  const availablePalettes = selectors.getAvailablePalettes(state, toolName);
   const property = mapPaletteToAnnotationColorProperty[colorPalette];
   if (availablePalettes.includes(property)) {
-    store.dispatch(actions.setIconColor(mapToolNameToKey(toolName), property));
+    store.dispatch(actions.setIconColor(toolName, mapPaletteToAnnotationColorProperty[colorPalette]));
   } else {
     console.warn(`${toolName} does not have ${colorPalette} color, available colors are: ${availablePalettes.map(palette => mapAnnotationColorPropertyToPalette[palette]).join(', ')} `);
   }
