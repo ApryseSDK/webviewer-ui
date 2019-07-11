7c6659cd922031acda8f48b21d7ea90248adeae7
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-07-11 11:57:31 -0700

    let users be able to change the scale and precision after made an annotation

--- src/components/MeasurementsDropdownItem/MeasurementsDropdownItem.js
+++ src/components/MeasurementsDropdownItem/MeasurementsDropdownItem.js
@@ -7,23 +7,26 @@ class MeasurementsDropdownItem extends React.PureComponent {
   static propTypes = {
     onClick: PropTypes.func,
     isSelected: PropTypes.bool,
-    content: PropTypes.string
-  }
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  };
 
   onClick = e => {
     this.props.onClick(e);
-  }
+  };
 
-  render() { 
+  render() {
     const { isSelected } = this.props;
     return (
-      <div className={['MeasurementsDropdownItem', isSelected? 'selected': ''].join(' ').trim()} onClick={this.onClick}>
-        <div className="Content">
-          {this.props.content}
-        </div>
+      <div
+        className={['MeasurementsDropdownItem', isSelected ? 'selected' : '']
+          .join(' ')
+          .trim()}
+        onClick={this.onClick}
+      >
+        <div className="Content">{this.props.content}</div>
       </div>
     );
   }
 }
- 
+
 export default MeasurementsDropdownItem;
