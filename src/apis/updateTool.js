cde261e4a1318508180521b31f15035924644cbc
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-07-23 18:05:37 -0700

    [bugfix] Fixed issue where updateTool doesn't work because of a merge error

--- src/apis/updateTool.js
+++ src/apis/updateTool.js
@@ -25,44 +25,9 @@ viewerElement.addEventListener('ready', function() {
   });
 });
  */
-
-import actions from 'actions';
-
-import setHeaderItems from './setHeaderItems';
-
 export default store => (toolName, properties) => {
-  if (properties.buttonGroup === null) {
-    setHeaderItems(store)(function(header) {
-      let alreadyExist = false;
-      header.getHeader('default').headers.default.forEach(function(element) {
-        if (element.toolName === toolName) {
-          alreadyExist = true;
-        }
-      });
-      if (!alreadyExist) {
-        header.getHeader('default').get('miscToolGroupButton').insertBefore({
-          type: 'toolButton',
-          toolName,
-          hidden: [ 'tablet', 'mobile' ]
-        });
-        header.getHeader('tools').get('miscToolGroupButton').insertBefore({
-          type: 'toolButton',
-          toolName,
-          hidden: [ 'desktop' ]
-        });
-      }
-    });
-  } else {
-    setHeaderItems(store)(function(header) {
-      const returnDefaultHeader = header.headers.default.filter(function(element) {
-        return element.toolName !== toolName;
-      });
-      header.getHeader('default').update(returnDefaultHeader);
-      const returnToolsHeader = header.headers.tools.filter(function(element) {
-        return element.toolName !== toolName;
-      });
-      header.getHeader('tools').update(returnToolsHeader);
-    });
-  }
-  store.dispatch(actions.updateTool(toolName, properties));
+  store.dispatch({
+    type: 'UPDATE_TOOL',
+    payload: { toolName, properties },
+  });
 };
