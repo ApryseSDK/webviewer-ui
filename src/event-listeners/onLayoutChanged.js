e294a44e8868f81d1c3699e483c4c606628025be
unknown <ayip@pdftron.com>
2019-08-09 12:10:10 -0700

    Fix deleting pages

--- src/event-listeners/onLayoutChanged.js
+++ src/event-listeners/onLayoutChanged.js
@@ -5,8 +5,12 @@ import getDefaultPageLabels from 'helpers/getDefaultPageLabels';
 export default dispatch => (e, { added, removed }) => {
   if (added.length || removed.length) {
     const totalPages = core.getTotalPages();
-
-    dispatch(actions.setPageLabels(getDefaultPageLabels(totalPages)));
     dispatch(actions.setTotalPages(totalPages));
+
+    setTimeout(() => {
+      // this handler get binded before it annotationManager.onLayoutChange handler
+      // so use 'setTimeout' so the annotationManager.onLayoutChange handler finish before we call 'setPageLabels'
+      dispatch(actions.setPageLabels(getDefaultPageLabels(totalPages)));
+    }, 0);
   }
 };
