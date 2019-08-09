15a746d9fe5523b560d0db39ae51894b0d444e55
unknown <ayip@pdftron.com>
2019-08-09 14:56:57 -0700

    Update comments

--- src/event-listeners/onLayoutChanged.js
+++ src/event-listeners/onLayoutChanged.js
@@ -8,7 +8,7 @@ export default dispatch => (e, { added, removed }) => {
     dispatch(actions.setTotalPages(totalPages));
 
     setTimeout(() => {
-      // this 'onLayoutChange' handler get trigger before the 'onLayoutChange' handler on 'viewerPageManager' get trigger (so page numbers aren't updated yet)
+      // this 'onLayoutChange' handler get trigger before the other 'onLayoutChange' event handler in core (that update page numbers ) get triggered
       // use 'setTimeout' so the other 'onLayoutChange' handler finishs before we call 'setPageLabels'
       dispatch(actions.setPageLabels(getDefaultPageLabels(totalPages)));
     }, 0);
