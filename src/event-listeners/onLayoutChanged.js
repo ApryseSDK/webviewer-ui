dad5953df8a9fecc09f5f60081da148c9bc3ea7e
AndrewYip <45575633+awy3@users.noreply.github.com>
2019-08-12 10:12:08 -0700

    [bugfix] Deleting pages with annotations on the last page fix (#324)

--- src/event-listeners/onLayoutChanged.js
+++ src/event-listeners/onLayoutChanged.js
@@ -5,8 +5,12 @@ import getDefaultPageLabels from 'helpers/getDefaultPageLabels';
 export default dispatch => (e, { added, removed }) => {
   if (added.length || removed.length) {
     const totalPages = core.getTotalPages();
-
-    dispatch(actions.setPageLabels(getDefaultPageLabels(totalPages)));
     dispatch(actions.setTotalPages(totalPages));
+
+    setTimeout(() => {
+      // this 'onLayoutChange' handler get trigger before the other 'onLayoutChange' event handler in core (that added by annotationManager to update page numbers) gets triggered
+      // use 'setTimeout' so the other 'onLayoutChange' handler finishes before we call 'setPageLabels'
+      dispatch(actions.setPageLabels(getDefaultPageLabels(totalPages)));
+    }, 0);
   }
 };
