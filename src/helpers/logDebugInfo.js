32a2f24b486d6df65b7c296dd189a2e34851b1a1
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-08-16 10:49:51 -0700

    Add logging info for WebViewer Server and Full API

---
+++ src/helpers/logDebugInfo.js
@@ -0,0 +1,35 @@
+import packageConfig from '../../package.json';
+
+/* eslint-disable no-console */
+export default ({ pdftronServer, fullAPI }) => {
+  // log UI and Core versions and warn/error if necessary
+  const coreVersion = window.CoreControls.DocumentViewer.prototype.version;
+  const uiVersion = packageConfig.version;
+  if (coreVersion && uiVersion) {
+    // we are using semantic versioning (ie ###.###.###) so the first number is the major version, follow by the minor version, and the patch number
+    const [coreMajorVersion, coreMinorVersion] = coreVersion
+      .split('.')
+      .map(version => parseInt(version, 10));
+    const [uiMajorVersion, uiMinorVersion] = uiVersion
+      .split('.')
+      .map(version => parseInt(version, 10));
+
+    console.log(
+      `[WebViewer] UI version: ${uiVersion}, Core version: ${coreVersion}`,
+    );
+
+    if (coreMajorVersion < uiMajorVersion) {
+      console.error(
+        `[WebViewer] Version Mismatch: UI requires Core version ${uiVersion} and above.`,
+      );
+    } else if (coreMinorVersion < uiMinorVersion) {
+      console.warn(
+        `[WebViewer] Version Mismatch: UI requires Core version ${uiVersion} and above.`,
+      );
+    }
+  }
+
+  console.log(`[WebViewer] Is using WebViewer Server: ${!!pdftronServer}`);
+
+  console.log(`[WebViewer] Is using Full API: ${!!fullAPI}`);
+};
