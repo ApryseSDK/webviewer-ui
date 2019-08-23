d03d9644aa31f115e7c38cab2173945779e8bda4
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-08-23 13:15:03 -0700

    use Hooks for react-i18next

--- src/components/AnnotationOverlay/AnnotationOverlay.js
+++ src/components/AnnotationOverlay/AnnotationOverlay.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import core from 'core';
 import selectors from 'selectors';
@@ -10,64 +9,59 @@ import './AnnotationOverlay.scss';
 
 const MAX_CHARACTERS = 100;
 
-const AnnotationOverlay = (props) => {
-  const isDisabled = useSelector(state => 
-    selectors.isElementDisabled(state, 'annotationOverlay')
+const AnnotationOverlay = () => {
+  const isDisabled = useSelector(state =>
+    selectors.isElementDisabled(state, 'annotationOverlay'),
   );
+  const [t] = useTranslation();
   const [annotation, setAnnotation] = useState();
   const [overlayPosition, setOverlayPosition] = useState({
     left: 0,
-    top: 0
+    top: 0,
   });
-  
-  const am = core.getAnnotationManager();
-  
+
   useEffect(() => {
     const onMouseHover = (e, mouseEvent) => {
-      const annotation = am.getAnnotationByMouseEvent(mouseEvent);
+      const annotation = core
+        .getAnnotationManager()
+        .getAnnotationByMouseEvent(mouseEvent);
+
       setAnnotation(annotation);
       if (annotation) {
         setOverlayPosition({
           left: mouseEvent.clientX + 20,
-          top: mouseEvent.clientY + 20
+          top: mouseEvent.clientY + 20,
         });
       }
-    }
+    };
     core.addEventListener('mouseMove', onMouseHover);
     return () => {
-      core.removeEventListener('mouseMove', onMouseHover)
-    }
+      core.removeEventListener('mouseMove', onMouseHover);
+    };
   }, []);
-  let contents = annotation ? annotation.getContents() : null;
-  if (!isDisabled && contents) {
-    const { left, top } = overlayPosition;
-    const repliesCount = annotation.getReplies().length || 0;
-    // display upto MAX_CHARACTERS characters
-    if (contents.length > MAX_CHARACTERS) {
-      contents = contents.slice(0, MAX_CHARACTERS);
-      contents += '...';
-    }
-    const { t } = props;
-    return (
-      <div 
-        className="Overlay AnnotationOverlay"
-        data-element="annotationOverlay"
-        style={{ left, top }}
-      >
-        <div className="author">
-          {annotation.Author}
-        </div>
-        <div>
-          {contents}
-        </div>
-        <div>
-          {repliesCount > 0 && `${t('action.reply')} (${repliesCount})`}
-        </div>
-      </div>
-    );
-  } else {
-    return null;
-  }
-}
 
-export default connect()(withTranslation()(AnnotationOverlay));
+  const contents = annotation?.getContents();
+  const replies = annotation?.getReplies();
+
+  return isDisabled || !contents ? null : (
+    <div
+      className="Overlay AnnotationOverlay"
+      data-element="annotationOverlay"
+      style={{ ...overlayPosition }}
+    >
+      <div className="author">{core.getDisplayAuthor(annotation)}</div>
+      <div className="contents">
+        {contents.length > MAX_CHARACTERS
+          ? `${contents.slice(0, MAX_CHARACTERS)}...`
+          : contents}
+      </div>
+      {replies.length > 0 && (
+        <div className="replies">
+          {`${t('action.reply')} (${replies.length})`}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AnnotationOverlay;
