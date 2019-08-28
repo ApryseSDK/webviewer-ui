6d84ff994c5a4ef3d5db5a9da201cb7f62621201
achenPDFTron <achen@pdftron.com>
2019-08-28 10:20:38 -0700

    can get data element

--- test/cypress/integration/waterMarkModal.spec.js
+++ test/cypress/integration/waterMarkModal.spec.js
@@ -1,38 +1,12 @@
-
+/**
+ * For these tests to work
+ * npm run start must be ran in the UI project
+ */
 describe ('Tests for watermark modal', () => {
-  // it('Visit the app asynchronously', async () => {
-  //   cy.visit ('/');
-  //   const $iframe = await cy.wrap(cy.get("iframe"));
-  //   const b = $iframe.contents().find("body")
-  //   console.log(b);
-  // });
-  it ('Visit the app', async () => {
-    // Cypress.Commands.add('iframe', { prevSubject: 'element' }, $iframe => {
-    //   return new Cypress.Promise(resolve => {
-    //       $iframe.on('load', () => {
-    //           resolve($iframe.contents().find('body'));
-    //       });
-    //   });
-    // });
-  // for <iframe id="foo" src="bar.html"></iframe>
-  // cy.get('#foo').iframe().find('.bar').should('contain', 'Success!');
-
+  beforeEach(() => {
     cy.visit ('/');
-
-    // const b = await temp();
-    // console.log(b);
-    
-    cy.get("iframe").then(($iframe) => {
-      // query into the iframe
-      const $body = $iframe.contents().find('body');
-
-    cy.wrap($body).find('.HeaderItems');
-    });
+  });
+  it ('Visit the app', () => {
+    cy.get('[data-element="menuButton"]');
   });
 });
-
-async function temp() {
-  // const $iframe = await cy.get("iframe");
-  // return $iframe.contents().find('body');
-  return cy.get("iframe");
-}
