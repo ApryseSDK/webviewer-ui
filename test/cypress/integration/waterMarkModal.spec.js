6c299e661b7341275ddf71ecfe1fd6b04f4ffea3
achenPDFTron <achen@pdftron.com>
2019-08-28 12:14:11 -0700

    can sort of test

--- test/cypress/integration/waterMarkModal.spec.js
+++ test/cypress/integration/waterMarkModal.spec.js
@@ -3,6 +3,13 @@
  * npm run start must be ran in the UI project
  */
 describe ('Tests for watermark modal', () => {
+  // it(() => {
+  //   cy.server();
+  //   cy.route({
+  //     method: 'GET',
+  //     url: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+  //   }).as('getAccount');
+  // });
   beforeEach(() => {
     cy.server();
     cy.route({
@@ -10,9 +17,11 @@ describe ('Tests for watermark modal', () => {
       url: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
     }).as('getAccount');
     cy.visit ('/');
-    cy.wait('@getAccount');
     cy.wait('@getAccount').then((xhr) => {
-      cy.window().should('have.property', 'appReady', true);
+      assert.isNotNull(xhr.response.body.data, '1st API call has data');
+    });
+    cy.wait('@getAccount').then((xhr) => {
+      assert.isNotNull(xhr.response.body.data, '2nd API call has data')
     });
   });
 
@@ -36,6 +45,20 @@ describe ('Tests for watermark modal', () => {
     cy.get( '[data-element="watermarkModal"]').should("visible");
 
     cy.get( '[data-element="watermarkModalCloseButton"]').click();
+
+    cy.get( '[data-element="watermarkModal"]').should("not.visible");
+  });
+
+  it ('Should be able to close watermark modal by clicking outside of it', () => {
+    cy.get('[data-element="menuButton"]').click();
+    cy.get('[data-element="printButton"]').click();
+    cy.get( '[data-element="printModal"]').should("visible");
+
+    cy.get('.apply-watermark').click();
+
+    cy.get( '[data-element="watermarkModal"]').should("visible");
+
+    cy.get( '[data-element="watermarkModal"]').click('topLeft');
 
     cy.get( '[data-element="watermarkModal"]').should("not.visible");
   });
