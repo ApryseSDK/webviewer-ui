8043b58003bea0e9124a5d65c87e54de7127eb58
achenPDFTron <achen@pdftron.com>
2019-08-30 15:24:48 -0700

    got tests to work

--- test/cypress/integration/waterMarkModal.spec.js
+++ test/cypress/integration/waterMarkModal.spec.js
@@ -3,6 +3,60 @@ const ID = {
   WATERMARK_APPLIED: 'watermark-applied',
   TEST_RESET: 'test-reset',
   TEST_PERSIST_CHANGE: 'test-persist-change',
+  TEST_PERSIST_CHANGE_EXISTING_WATERMARK: 'test-persist-change-existing-watermark',
+};
+
+const WATERMARK = {
+  // Draw diagonal watermark in middle of the document
+  diagonal: {
+    fontSize: 52, // or even smaller size
+    fontFamily: 'sans-serif',
+    color: 'red',
+    opacity: 100, // from 0 to 100
+    text: 'Watermark'
+  },
+  headerLeft: {
+    fontSize: 30, // or even smaller size
+    fontFamily: 'sans-serif',
+    color: 'blue',
+    opacity: 100, // from 0 to 100
+    text: 'header left'
+  },
+  headerRight: {
+    fontSize: 30, // or even smaller size
+    fontFamily: 'sans-serif',
+    color: 'green',
+    opacity: 100, // from 0 to 100
+    text: 'header right'
+  },
+  headerCenter: {
+    fontSize: 30, // or even smaller size
+    fontFamily: 'sans-serif',
+    color: 'yellow',
+    opacity: 100, // from 0 to 100
+    text: 'header center'
+  },
+  footerLeft: {
+    fontSize: 30, // or even smaller size
+    fontFamily: 'sans-serif',
+    color: 'blue',
+    opacity: 100, // from 0 to 100
+    text: 'header left'
+  },
+  footerRight: {
+    fontSize: 30, // or even smaller size
+    fontFamily: 'sans-serif',
+    color: 'green',
+    opacity: 100, // from 0 to 100
+    text: 'header right'
+  },
+  footerCenter: {
+    fontSize: 30, // or even smaller size
+    fontFamily: 'sans-serif',
+    color: 'yellow',
+    opacity: 100, // from 0 to 100
+    text: 'header center'
+  },
 };
 /**
  * For these tests to work
@@ -151,58 +205,7 @@ describe ('Tests for watermark modal', () => {
     beforeEach(() => {
       cy.window()
         .then({ timeout: 30000 }, window => {
-          window.docViewer.setWatermark({
-            // Draw diagonal watermark in middle of the document
-            diagonal: {
-              fontSize: 52, // or even smaller size
-              fontFamily: 'sans-serif',
-              color: 'red',
-              opacity: 100, // from 0 to 100
-              text: 'Watermark'
-            },
-            headerLeft: {
-              fontSize: 30, // or even smaller size
-              fontFamily: 'sans-serif',
-              color: 'blue',
-              opacity: 100, // from 0 to 100
-              text: 'header left'
-            },
-            headerRight: {
-              fontSize: 30, // or even smaller size
-              fontFamily: 'sans-serif',
-              color: 'green',
-              opacity: 100, // from 0 to 100
-              text: 'header right'
-            },
-            headerCenter: {
-              fontSize: 30, // or even smaller size
-              fontFamily: 'sans-serif',
-              color: 'yellow',
-              opacity: 100, // from 0 to 100
-              text: 'header center'
-            },
-            footerLeft: {
-              fontSize: 30, // or even smaller size
-              fontFamily: 'sans-serif',
-              color: 'blue',
-              opacity: 100, // from 0 to 100
-              text: 'header left'
-            },
-            footerRight: {
-              fontSize: 30, // or even smaller size
-              fontFamily: 'sans-serif',
-              color: 'green',
-              opacity: 100, // from 0 to 100
-              text: 'header right'
-            },
-            footerCenter: {
-              fontSize: 30, // or even smaller size
-              fontFamily: 'sans-serif',
-              color: 'yellow',
-              opacity: 100, // from 0 to 100
-              text: 'header center'
-            },
-          });
+          window.docViewer.setWatermark(WATERMARK);
 
           window.docViewer.refreshAll();
           window.docViewer.updateView();
@@ -229,6 +232,76 @@ describe ('Tests for watermark modal', () => {
           cy.get( '[data-element="printModal"]').should('visible');
 
           cy.get( '[data-element="printModal"]').find('.apply-watermark').should('visible');
+        });
+    });
+
+    it('Should be able to persist watermark modal changes when existing watermark appear/dissapear', () => {
+      cy.window()
+        .then(async(window) => {
+          window.docViewer.setWatermark({});
+
+          window.docViewer.refreshAll();
+          window.docViewer.updateView();
+
+          await window.docViewer.getWatermark();
+
+          cy.get('[data-element="menuButton"]').click();
+          cy.get('[data-element="printButton"]').click();
+          cy.get( '[data-element="printModal"]').should('visible');
+
+          cy.get( '[data-element="printModal"]').find('.apply-watermark').click();
+
+          cy.get('[data-element="watermarkModal"]').find('form').within(() => {
+            cy.get('.text-input').type('Pamela');
+            cy.get('select').first().find('option').eq(2).invoke('val').then((val) => {
+              cy.get('select').first().select(val);
+              cy.get('select').first().focus().blur();
+            });
+            cy.get('select').last().find('option').eq(11).invoke('val').then((val) => {
+              cy.get('select').last().select(val);
+              cy.get('select').first().focus().blur();
+            });
+          });
+    
+          // wait for changes to canvas
+          cy.timeout(1000);
+    
+          cy.get('[data-element="watermarkModal"]').find('.form-container').matchImageSnapshot(ID.TEST_PERSIST_CHANGE_EXISTING_WATERMARK);
+          cy.get('[data-element="watermarkModal"]').find('.ok.button').click();
+
+          cy.get('[data-element="printModalCloseButton"]').click()
+            .then(async() => {
+              window.docViewer.setWatermark(WATERMARK);
+              window.docViewer.refreshAll();
+              window.docViewer.updateView();
+
+              await window.docViewer.getWatermark();
+            });
+
+          cy.get('[data-element="menuButton"]').click();
+          cy.get('[data-element="printButton"]').click();
+          cy.get( '[data-element="printModal"]').find('.apply-watermark').should('not.visible');
+
+          cy.get('[data-element="printModalCloseButton"]').click()
+            .then(async() => {
+              window.docViewer.setWatermark({});
+              window.docViewer.refreshAll();
+              window.docViewer.updateView();
+
+              await window.docViewer.getWatermark();
+            });
+
+          cy.get('[data-element="menuButton"]').click();
+          cy.get('[data-element="printButton"]').click();
+          cy.get( '[data-element="printModal"]').find('.apply-watermark').should('visible');
+
+          cy.get( '[data-element="printModal"]').find('.apply-watermark').click();
+
+          // wait for canvas to load
+          cy.get('[data-element="watermarkModal"]').find('canvas', {timeout: 5000});
+
+          cy.get('[data-element="watermarkModal"]').find('.form-container').matchImageSnapshot(ID.TEST_PERSIST_CHANGE_EXISTING_WATERMARK);
+
         });
     });
   });
