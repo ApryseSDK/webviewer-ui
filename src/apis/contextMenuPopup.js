fb2da7323be2a994676c5dbc80d0d76b4b00306b
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-09-09 17:20:02 -0700

    Add documentations for annotationPopup, contextMenuPopup and textPopup

--- src/apis/contextMenuPopup.js
+++ src/apis/contextMenuPopup.js
@@ -1,3 +1,13 @@
+/**
+ * An instance of Popup that can be used to edit items in the context menu popup component
+ * @name WebViewer#contextMenuPopup
+ * @see WebViewer.Popup
+ * @example // 6.0 and after
+WebViewer(...)
+  .then(function (instance) {
+    instance.contextMenuPopup.someAPI();
+  });
+ */
 import createPopupAPI from 'helpers/createPopupAPI';
 
 export default store => createPopupAPI(store, 'contextMenuPopup');
