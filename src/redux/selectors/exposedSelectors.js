87de1f1c3df33fcc5609760ee56bcd287932343d
achenPDFTron <achen@pdftron.com>
2019-09-11 11:55:24 -0700

    Removed temp worked around

--- src/redux/selectors/exposedSelectors.js
+++ src/redux/selectors/exposedSelectors.js
@@ -99,9 +99,7 @@ export const isEmbedPrintSupported = state => {
     window.navigator.userAgent.indexOf('Chrome') > -1 &&
     window.navigator.userAgent.indexOf('Edge') === -1;
   const isPDF = getDocumentType(state) === workerTypes.PDF;
-  // return isPDF && isChrome && state.viewer.useEmbeddedPrint;
-  // TODO remove this later
-  return false;
+  return isPDF && isChrome && state.viewer.useEmbeddedPrint;
 };
 
 export const getColorMap = state => state.viewer.colorMap;
