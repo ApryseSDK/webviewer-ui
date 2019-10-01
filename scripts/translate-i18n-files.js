474297d390f8e67c3d835afe2669d494be4926d6
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-10-01 13:12:11 -0700

    Trying to add missing translations

--- scripts/translate-i18n-files.js
+++ scripts/translate-i18n-files.js
@@ -10,28 +10,37 @@ const baseFile = `${i18nFolder}/translation-en.json`;
 const filesToTranslate = fs
   .readdirSync(i18nFolder)
   // filter out .DS_Store
-  .filter(fileName => fileName.endsWith('.json')) 
+  .filter(fileName => fileName.endsWith('.json'))
   .map(fileName => `${i18nFolder}/${fileName}`)
   .filter(filePath => filePath !== baseFile);
 
 const runTranslation = () => {
   const baseTranslationData = require(baseFile);
-  filesToTranslate
-    .forEach(async file => {
-      const translationData = require(file);
-      const updatedData = {};
-      const languageCode = file.match(/-(.+?)\.json/i)[1];
+  filesToTranslate.forEach(async file => {
+    const translationData = require(file);
+    const updatedData = {};
+    const languageCode = file.match(/-(.+?)\.json/i)[1];
 
-      if (!languageCode) {
-        console.error(`Can't determine language code of ${file}`);
-      }
+    if (!languageCode) {
+      console.error(`Can't determine language code of ${file}`);
+    }
 
-      await addMissingKey(baseTranslationData, translationData, updatedData, languageCode);
-      fs.writeFileSync(file, JSON.stringify(updatedData, null, 2));
-    });
+    await addMissingKey(
+      baseTranslationData,
+      translationData,
+      updatedData,
+      languageCode,
+    );
+    fs.writeFileSync(file, JSON.stringify(updatedData, null, 2));
+  });
 };
 
-const addMissingKey = (baseTranslationData, translationData, result, languageCode) =>
+const addMissingKey = (
+  baseTranslationData,
+  translationData,
+  result,
+  languageCode,
+) =>
   new Promise(resolve => {
     Promise.all(
       Object.keys(baseTranslationData).map(
@@ -43,22 +52,20 @@ const addMissingKey = (baseTranslationData, translationData, result, languageCod
                 baseTranslationData[key],
                 translationData[key],
                 result[key],
-                languageCode
+                languageCode,
               );
+            } else if (translationData[key]) {
+              result[key] = translationData[key];
             } else {
-              if (translationData[key]) {
-                result[key] = translationData[key];
-              } else {
-                const [translated] = await translate.translate(
-                  baseTranslationData[key],
-                  mapI18nCodeToGoogleTranslationCode(languageCode)
-                );
-                result[key] = translated;
-              }
+              const [translated] = await translate.translate(
+                baseTranslationData[key],
+                mapI18nCodeToGoogleTranslationCode(languageCode),
+              );
+              result[key] = translated;
             }
             resolve();
-          })
-      )
+          }),
+      ),
     ).then(resolve);
   });
 
@@ -77,8 +84,10 @@ const mapI18nCodeToGoogleTranslationCode = code => {
   };
 
   if (!map[code]) {
-    throw Error(`${code} is missing in the map object in translate-i18n-files.js. Please add it to the map and then run the script again`);
-  } 
+    throw Error(
+      `${code} is missing in the map object in translate-i18n-files.js. Please add it to the map and then run the script again`,
+    );
+  }
 
   return map[code];
 };
