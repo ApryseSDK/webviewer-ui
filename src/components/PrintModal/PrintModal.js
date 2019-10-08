8fba6caae1730afc7944e85019748c4d272559f0
achenPDFTron <54084743+achenPDFTron@users.noreply.github.com>
2019-10-08 11:33:12 -0700

    [Bugfix] tooltip text overflow (#370)

--- src/components/PrintModal/PrintModal.js
+++ src/components/PrintModal/PrintModal.js
@@ -187,16 +187,17 @@ class PrintModal extends React.PureComponent {
           pendingCanvas => pendingCanvas !== id,
         );
         this.positionCanvas(canvas, pageIndex);
-        this.drawAnnotationsOnCanvas(canvas, pageNumber).then(() => {
-          const img = document.createElement('img');
-          img.src = canvas.toDataURL();
-          img.onload = () => {
-            this.setState(({ count }) => ({
-              count: count < 0 ? -1 : count + 1,
-            }));
-            resolve(img);
-          };
-        });
+        this.drawAnnotationsOnCanvas(canvas, pageNumber)
+          .then(() => {
+            const img = document.createElement('img');
+            img.src = canvas.toDataURL();
+            img.onload = () => {
+              this.setState(({ count }) => ({
+                count: count < 0 ? -1 : count + 1,
+              }));
+              resolve(img);
+            };
+          });
       };
 
       const id = core.getDocument().loadCanvasAsync({
