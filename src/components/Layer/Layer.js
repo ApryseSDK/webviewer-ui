9143bd72afc403825aa4c239b6a04f59cf2c0b26
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-10-17 15:40:04 -0700

    Read the value of children property to render sub layers

--- src/components/Layer/Layer.js
+++ src/components/Layer/Layer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+
+import Icon from 'components/Icon';
+import Input from 'components/Input';
 
 import core from 'core';
-
-import Input from 'components/Input';
 
 import './Layer.scss';
 
@@ -13,26 +13,63 @@ class Layer extends React.PureComponent {
     layer: PropTypes.object.isRequired,
     layers: PropTypes.arrayOf(PropTypes.object).isRequired,
     index: PropTypes.number.isRequired,
-  }
+  };
 
+  state = {
+    isExpanded: false,
+  };
 
   onChange = e => {
     const { index, layers } = this.props;
-
     layers[index].visible = e.target.checked;
     const doc = core.getDocument();
     doc.setLayersArray(layers);
 
     window.docViewer.refreshAll();
     window.docViewer.updateView();
-  }
+  };
+
+  onClickExpand = () => {
+    this.setState(prevState => ({
+      isExpanded: !prevState.isExpanded,
+    }));
+  };
 
   render() {
+    const { isExpanded } = this.state;
     const { layer } = this.props;
+    const hasSubLayers = layer.children.length > 0;
 
     return (
       <div className="Layer">
-        <Input id={layer.name} type="checkbox" label={layer.name} onChange={this.onChange} defaultChecked={layer.visible} />
+        <div className="layer-wrapper">
+          <div className="padding">
+            {hasSubLayers && (
+              <div
+                className={`arrow ${
+                  isExpanded ? 'expanded' : 'collapsed'
+                }`}
+                onClick={this.onClickExpand}
+              >
+                <Icon glyph="ic_chevron_right_black_24px" />
+              </div>
+            )}
+          </div>
+          <Input
+            id={layer.name}
+            type="checkbox"
+            label={layer.name}
+            onChange={this.onChange}
+            defaultChecked={layer.visible}
+          />
+        </div>
+        {hasSubLayers && isExpanded && (
+          <div className="sub-layers">
+            {layer.children.map((subLayer, i) => (
+              <Layer key={i} layer={subLayer} layers={layer.children} index={i} />
+            ))}
+          </div>
+        )}
       </div>
     );
   }
