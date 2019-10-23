155a919b46bd927f968ee61076df6201fbdb8af6
khein-pdftron <khein@pdftron.com>
2019-10-22 21:33:11 -0700

    Fix for page text showing up in front of canvas sometimes (#383)

--- src/event-listeners/onPageComplete.js
+++ src/event-listeners/onPageComplete.js
@@ -7,7 +7,7 @@ export default store => pageIndex => {
       const textContainer = document.createElement('div');
       textContainer.tabIndex = 0;
       textContainer.textContent = `Page ${pageIndex + 1}.\n${text}\nEnd of page ${pageIndex + 1}.`;
-      textContainer.style = 'height: 100%; font-size: 5px; overflow: auto;';
+      textContainer.style = 'height: 100%; font-size: 5px; overflow: auto; position: relative; z-index: -99999';
       textContainer.id = `pageText${pageIndex}`;
       document.getElementById(`pageContainer${pageIndex}`).appendChild(textContainer);
     });
