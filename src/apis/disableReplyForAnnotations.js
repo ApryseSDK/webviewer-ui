ae6a3612352f7db41ff376252f6720461f7f0d05
Jackie Huang <jhuang@pdftron.com>
2019-10-29 14:09:49 -0700

    Update comment

--- src/apis/disableReplyForAnnotations.js
+++ src/apis/disableReplyForAnnotations.js
@@ -1,14 +1,20 @@
 /**
  * Disable reply for annotations determined by the function passed in as parameter
- * @param Function that takes an annotation and returns if the reply of this annotations should be disabled.
+ * @method WebViewer#disableReplyForAnnotations
+ * @param {WebViewer~isReplyDisabled} isReplyDisabled Function that takes an annotation and returns if the reply of the annotation should be disabled.
  * @example
  WebViewer(...)
   .then(function(instance) {
-    /
     instance.disableReplyForAnnotations(function(annotation) {
       return annotation instanceof instance.Annotations.FreeHandAnnotation;
     });
   });
+ */
+/**
+ * Callback that gets passed to {@link WebViewer#disableReplyForAnnotations disableReplyForAnnotations}
+ * @callback WebViewer~isReplyDisabled
+ * @param {Annotations} annotation Annotation object
+ * @returns {boolean} Whether the reply of the annotation should be disabled.
  */
 import actions from 'actions';
 
