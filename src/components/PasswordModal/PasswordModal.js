787d8e4c7050ced20ae7c0c10bf269772d42a52d
Jackie Huang <jhuang@pdftron.com>
2019-11-04 14:28:11 -0800

    Refactor PasswordModal with hooks

--- src/components/PasswordModal/PasswordModal.js
+++ src/components/PasswordModal/PasswordModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
@@ -11,94 +11,98 @@ import selectors from 'selectors';
 
 import './PasswordModal.scss';
 
-class PasswordModal extends React.PureComponent {
-  static propTypes = {
-    isOpen: PropTypes.bool,
-    attempt: PropTypes.number.isRequired,
-    checkPassword: PropTypes.func,
-    setPasswordAttempts: PropTypes.func.isRequired,
-    t: PropTypes.func.isRequired,
-    closeElement: PropTypes.func.isRequired,
-  }
+const propTypes = {
+  isOpen: PropTypes.bool,
+  attempt: PropTypes.number.isRequired,
+  checkPassword: PropTypes.func,
+  setPasswordAttempts: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+  closeElement: PropTypes.func.isRequired,
+};
 
-  constructor() {
-    super();
-    this.maxAttempts = 3;
-    this.passwordInput = React.createRef();
-    this.initialState = {
-      password: '',
-      userCanceled: false,
-    };
-    this.state = this.initialState;
-  }
+const PasswordModal = props => {
+  const {
+    isOpen,
+    attempt,
+    checkPassword,
+    t,
+    closeElement,
+  } = props;
+  const initialState = {
+    password: '',
+    userCancelled: false,
+  };
+  const passwordInput = React.createRef();
+  const maxAttempts = 3;
+  const [password, setPassword] = useState(initialState.password);
+  const [userCancelled, setUserCancelled] = useState(initialState.userCancelled);
 
-  componentDidUpdate(prevProps) {
-    if (!prevProps.isOpen && this.props.isOpen) {
-      this.props.closeElement('progressModal');
+  useEffect(() => {
+    if (isOpen) {
+      closeElement('progressModal');
     }
-    if (prevProps.isOpen && !this.props.isOpen) {
+    if (!isOpen) {
       // when a user enters the correct password or calls core.closeDocument
       // reset state in case user loads another password-protected document
-      this.setState(this.initialState);
+      setPassword(initialState.password);
+      setUserCancelled(initialState.userCanceled);
     }
-    if (this.passwordInput.current) {
-      this.passwordInput.current.focus();
+    if (passwordInput.current) {
+      passwordInput.current.focus();
     }
-  }
+  }, [isOpen,passwordInput]);
 
-  handleInputChange = e => {
-    this.setState({ password: e.target.value });
-  }
+  const handleInputChange = e => {
+    setPassword(e.target.value);
+  };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    this.props.checkPassword(this.state.password);
-  }
+    checkPassword(password);
+  };
 
-  handleCancel = () => {
-    this.setState({ userCanceled: true });
-  }
+  const handleCancel = () => {
+    setUserCancelled(true);
+  };
 
-  renderContent = () => {
-    const userExceedsMaxAttempts = this.props.attempt === this.maxAttempts;
-
+  const renderContent = () => {
+    const userExceedsMaxAttempts = attempt === maxAttempts;
     if (userExceedsMaxAttempts) {
-      return this.renderMaxAttemptsContent();
+      return renderMaxAttemptsContent();
     }
-    if (this.state.userCanceled) {
-      return this.renderUserCancelContent();
+    if (userCancelled) {
+      return renderUserCancelContent();
     }
 
-    return this.renderEnterPasswordContent();
-  }
+    return renderEnterPasswordContent();
+  };
 
-  renderMaxAttemptsContent = () => <p>{this.props.t('message.encryptedAttemptsExceeded')}</p>
+  const renderMaxAttemptsContent = () => <p>{t('message.encryptedAttemptsExceeded')}</p>;
 
-  renderUserCancelContent = () => <p>{this.props.t('message.encryptedUserCancelled')}</p>
+  const renderUserCancelContent = () => <p>{t('message.encryptedUserCancelled')}</p>;
 
-  renderEnterPasswordContent = () => {
-    const { t } = this.props;
-    const wrongPassword = this.props.attempt !== 0;
+  const renderEnterPasswordContent = () => {
+    const wrongPassword = attempt !== 0;
 
     return (
       <div className="wrapper">
         <div className="header">{t('message.passwordRequired')}</div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="enter">
             <div>{t('message.enterPassword')}</div>
             <input
               className={`${wrongPassword ? 'wrong' : 'correct'}`}
               type="password"
-              ref={this.passwordInput}
-              value={this.state.password}
-              onChange={this.handleInputChange}
+              ref={passwordInput}
+              value={password}
+              onChange={handleInputChange}
             />
           </div>
           {wrongPassword &&
             <div className="incorrect-password">
               {t('message.incorrectPassword', {
-                remainingAttempts: this.maxAttempts - this.props.attempt,
+                remainingAttempts: maxAttempts - attempt,
               })}
             </div>
           }
@@ -106,31 +110,27 @@ class PasswordModal extends React.PureComponent {
             <Button
               dataElement="passwordSubmitButton"
               label={t('action.submit')}
-              onClick={this.handleSubmit}
+              onClick={handleSubmit}
             />
             <Button
               dataElement="passwordCancelButton"
               label={t('action.cancel')}
-              onClick={this.handleCancel}
+              onClick={handleCancel}
             />
           </div>
         </form>
       </div>
     );
-  }
+  };
 
-  render() {
-    const className = getClassName('Modal PasswordModal', this.props);
-
-    return (
-      <div className={className} data-element="passwordModal">
-        <div className="container">
-          {this.renderContent()}
-        </div>
+  return (
+    <div className={getClassName('Modal PasswordModal', props)} data-element="passwordModal">
+      <div className="container">
+        {renderContent()}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   isOpen: selectors.isElementOpen(state, 'passwordModal'),
@@ -142,5 +142,7 @@ const mapDispatchToProps = {
   setPasswordAttempts: actions.setPasswordAttempts,
   closeElement: actions.closeElement,
 };
+
+PasswordModal.propTypes = propTypes;
 
 export default connect(mapStateToProps, mapDispatchToProps)(withTranslation()(PasswordModal));
