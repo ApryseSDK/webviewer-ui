566d4f1eeb328f61a8b7fe278812cbfe8b7d2fc7
Jackie Huang <jhuang@pdftron.com>
2019-11-04 15:10:51 -0800

    Refactor ToggleElementOverlay with hooks

--- src/components/ToggleElementOverlay/ToggleElementOverlay.js
+++ src/components/ToggleElementOverlay/ToggleElementOverlay.js
@@ -1,102 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import core from 'core';
 
 import ToggleElementButton from 'components/ToggleElementButton';
-
 import { zoomTo } from 'helpers/zoom';
 import selectors from 'selectors';
 import actions from 'actions';
 
 import './ToggleElementOverlay.scss';
 
-class ToggleElementOverlay extends React.PureComponent {
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-    isActive: PropTypes.bool,
-  }
+const propTypes = {
+  onClick: PropTypes.func.isRequired,
+  isActive: PropTypes.bool,
+};
 
-  constructor(props) {
-    super(props);
-    this.state = { value: '100' };
-  }
+const ToggleElementOverlay = ({
+  onClick,
+  isActive,
+}) => {
+  const [value, setValue] = useState('100');
 
-  componentDidMount() {
-    core.addEventListener('documentLoaded', this.onDocumentLoaded);
-    core.addEventListener('zoomUpdated', this.onZoomUpdated);
-  }
+  useEffect(() => {
+    const onDocumentLoaded = () => setValue(Math.ceil(core.getZoom() * 100).toString());
+    const onZoomUpdated = () => setValue(Math.ceil(core.getZoom() * 100).toString());
 
-  componentWillUnmount() {
-    core.removeEventListener('documentLoaded', this.onDocumentLoaded);
-    core.removeEventListener('zoomUpdated', this.onZoomUpdated);
-  }
+    core.addEventListener('documentLoaded', onDocumentLoaded);
+    core.addEventListener('zoomUpdated', onZoomUpdated);
 
-  onDocumentLoaded = () => {
-    this.setState({ value: Math.ceil(core.getZoom() * 100).toString() });
-  }
+    return () => {
+      core.removeEventListener('documentLoaded', onDocumentLoaded);
+      core.removeEventListener('zoomUpdated', onZoomUpdated);
+    };
+  });
 
-  onZoomUpdated = () => {
-    this.setState({ value: Math.ceil(core.getZoom() * 100).toString() });
-  }
-
-  onKeyPress = e => {
+  const onKeyPress = e => {
     if (e.nativeEvent.key === 'Enter' || e.nativeEvent.keyCode === 13) {
       const zoom = Math.ceil(core.getZoom() * 100).toString();
       if (e.target.value === zoom) {
         return;
       }
       if (e.target.value === '') {
-        this.setState({ value: zoom });
+        setValue(zoom);
       } else {
         zoomTo(Number(e.target.value) / 100);
       }
     }
-  }
+  };
 
-  onChange = e => {
+  const onChange = e => {
     const re = /^(\d){0,4}$/;
     if (re.test(e.target.value) || e.target.value === '') {
-      this.setState({ value: e.target.value });
+      setValue(e.target.value);
     }
-  }
+  };
 
-  onBlur = e => {
+  const onBlur = e => {
     const zoom = Math.ceil(core.getZoom() * 100).toString();
     if (e.target.value === zoom) {
       return;
     }
     if (e.target.value === '' || isNaN(Number(e.target.value))) {
-      this.setState({ value: zoom });
+      setValue(zoom);
     } else {
-      this.setState({ value: Number(e.target.value).toString() });
+      setValue(Number(e.target.value).toString());
       zoomTo(e.target.value / 100);
     }
-  }
+  };
 
-  render() {
-    const { isActive, onClick } = this.props;
-    return (
-      <div className="ToggleElementOverlay">
-        <div className={['OverlayContainer', isActive ? 'active' : ''].join(' ').trim()}>
-          <div className="OverlayText" onClick={onClick}>
-            <input
-              type="text"
-              className="textarea"
-              value={this.state.value}
-              onChange={this.onChange}
-              onKeyPress={this.onKeyPress}
-              onBlur={this.onBlur}
-              tabIndex={-1}
-            />
-            <span>%</span>
-          </div>
-          <ToggleElementButton className="OverlayButton" img="ic-triangle" element="zoomOverlay" dataElement="zoomOverlay"/>
+  return (
+    <div className="ToggleElementOverlay">
+      <div className={['OverlayContainer', isActive ? 'active' : ''].join(' ').trim()}>
+        <div className="OverlayText" onClick={onClick}>
+          <input
+            type="text"
+            className="textarea"
+            value={value}
+            onChange={onChange}
+            onKeyPress={onKeyPress}
+            onBlur={onBlur}
+            tabIndex={-1}
+          />
+          <span>%</span>
         </div>
+        <ToggleElementButton className="OverlayButton" img="ic-triangle" element="zoomOverlay" dataElement="zoomOverlay"/>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+ToggleElementOverlay.propTypes = propTypes;
 
 const mapStateToProps = state => ({
   isActive: selectors.isElementOpen(state, 'zoomOverlay'),
