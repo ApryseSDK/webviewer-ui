cbbab0def6bb44584e73d22a46adb89cf1feb9dc
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-11-12 15:43:09 -0800

    parseInt before pass back

--- src/helpers/loadScript.js
+++ src/helpers/loadScript.js
@@ -42,7 +42,7 @@ const loadConfig = () =>
 
       window.parent.postMessage({
         type: 'requestConfig',
-        id: getHashParams('id'),
+        id: parseInt(getHashParams('id'), 10),
       }, '*');
     }
   });
