517049cebb1153a723cfc5535809cc629a767a15
Logan Bittner <lbittner@pdftron.com>
2019-11-18 14:02:55 -0800

    Fixes rest of typescript problems!

--- src/apis/setHeaderItems.js
+++ src/apis/setHeaderItems.js
@@ -1,7 +1,7 @@
 /**
  * Customize header. Refer to <a href='https://www.pdftron.com/documentation/web/guides/customizing-header' target='_blank'>Customizing header</a> for details.
  * @method WebViewer#setHeaderItems
- * @param {WebViewer~headerCallback} headerCallback Callback function to perform different operations on the header.
+ * @param {WebViewer.headerCallback} headerCallback Callback function to perform different operations on the header.
  * @example
 // Adding save annotations button
 WebViewer(...)
@@ -54,7 +54,7 @@ WebViewer(...)
  */
 /**
  * Callback that gets passed to {@link CoreControls.ReaderControl#setHeaderItems setHeaderItems}.
- * @callback WebViewer~headerCallback
+ * @callback WebViewer.headerCallback
  * @param {Header} header Header instance with helper functions
  * @ignore
  */
