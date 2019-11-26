d4a6404239ee98c5cfd3090688f2febdbbd0f3c9
achenPDFTron <54084743+achenPDFTron@users.noreply.github.com>
2019-11-26 11:15:58 -0800

    [BugFix] Fixed the issue where getDisplayAuthor wasn't used when generating the comments page for printing.

--- src/components/PrintModal/PrintModal.js
+++ src/components/PrintModal/PrintModal.js
@@ -392,7 +392,7 @@ class PrintModal extends React.PureComponent {
 
     info.className = 'note__info';
     info.innerHTML = `
-      Author: ${annotation.Author || ''} &nbsp;&nbsp;
+      Author: ${core.getDisplayAuthor(annotation) || ''} &nbsp;&nbsp;
       Subject: ${annotation.Subject} &nbsp;&nbsp;
       Date: ${dayjs(annotation.DateCreated).format('D/MM/YYYY h:mm:ss A')}
     `;
