72885e5559751e6abfc038de200eb1e3104af5fb
achenPDFTron <achen@pdftron.com>
2019-12-14 09:33:24 -0800

    Removed height of 100% and removed duplicate node

--- src/event-listeners/onPageComplete.js
+++ src/event-listeners/onPageComplete.js
@@ -5,11 +5,18 @@ export default store => pageIndex => {
   if (state.viewer.isAccessibleMode) {
     core.getDocument().loadPageText(pageIndex, text => {
       const textContainer = document.createElement('div');
+      const className = 'accessible-text';
+      textContainer.classList.add(className);
       textContainer.tabIndex = 0;
       textContainer.textContent = `Page ${pageIndex + 1}.\n${text}\nEnd of page ${pageIndex + 1}.`;
-      textContainer.style = 'height: 100%; font-size: 5px; overflow: auto; position: relative; z-index: -99999';
+      textContainer.style = 'font-size: 5px; overflow: auto; position: relative; z-index: -99999';
       textContainer.id = `pageText${pageIndex}`;
-      document.getElementById(`pageContainer${pageIndex}`).appendChild(textContainer);
+      const pageContainerElement = document.getElementById(`pageContainer${pageIndex}`);
+      const existingTextContainer = pageContainerElement.querySelector(`.${className}`);
+      if (existingTextContainer) {
+        pageContainerElement.removeChild(existingTextContainer);
+      }
+      pageContainerElement.appendChild(textContainer);
     });
   }
 };
