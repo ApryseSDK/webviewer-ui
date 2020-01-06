8fc9ce4ca341a3ec15913f069adfbeaf01e902ad
Logan Bittner <lbittner@pdftron.com>
2020-01-06 11:57:59 -0800

    Make entire options object optional

--- src/apis/loadDocument.js
+++ src/apis/loadDocument.js
@@ -2,7 +2,7 @@
  * Load a document inside WebViewer UI.
  * @method WebViewerInstance#loadDocument
  * @param {(string|File|Blob)} documentPath Path to the document OR <a href='https://developer.mozilla.org/en-US/docs/Web/API/File' target='_blank'>File object</a> if opening local file.
- * @param {object} options Additional options
+ * @param {object} [options] Additional options
  * @param {string} [options.extension] The extension of the file. If file is a blob/file object or a URL without an extension then this is necessary so that WebViewer knows what type of file to load.
  * @param {string} [options.filename] Filename of the document, which is used when downloading the PDF.
  * @param {object} [options.customHeaders] An object of custom HTTP headers to use when retrieving the document from the specified url.
