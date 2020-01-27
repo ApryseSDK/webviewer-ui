5b32abc8421fa608ff40686788e54deac29b6cb9
Andrey <a.so777@live.ca>
2020-01-27 12:51:50 -0800

    nn -> n

--- src/components/LinkModal/LinkModal.js
+++ src/components/LinkModal/LinkModal.js
@@ -81,7 +81,7 @@ const LinkModal = () => {
             linksToDelete.push(core.getAnnotationById(linkId));
           });
           core.deleteAnnotations(linksToDelete);
-          annot.unnassociateLinks();
+          annot.unassociateLinks();
         }
         
         const link = newLink(annot.X, annot.Y, annot.Width, annot.Height);
