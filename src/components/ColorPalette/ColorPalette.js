23b496c9c001e683b9d36a1ad7d071a9e8f784e6
Kristian Hein <khein@pdftron.com>
2020-01-28 17:14:48 -0800

    fix

--- src/components/ColorPalette/ColorPalette.js
+++ src/components/ColorPalette/ColorPalette.js
@@ -22,12 +22,6 @@ class ColorPalette extends React.PureComponent {
 
   constructor(props) {
     super(props);
-    // this.palette = [
-    //   ['#F1A099', '#FFC67B', '#FFE6A2', '#80E5B1', '#92E8E8', '#A6A1E6', '#E2A1E6'],
-    //   ['#E44234', '#FF8D00', '#FFCD45', '#00CC63', '#25D2D1', '#4E7DE9', '#C544CE'],
-    //   ['#88271F', '#B54800', '#F69A00', '#007A3B', '#167E7D', '#2E4B8B', '#76287B'],
-    //   ['#FFFFFF', '#CDCDCD', '#9C9C9C', '#696969', '#373737', '#000000'],
-    // ];
     this.palette = [
       ['#000000', '#ffffff', '#e6261f', '#eb7532', '#f7d038', '#a3e048'],
       ['#33bbe6', '#4355db', '#d23be7'],
@@ -65,11 +59,13 @@ class ColorPalette extends React.PureComponent {
 
   render() {
     const { property, color, overridePalette } = this.props;
+    console.log(color.toHexString());
 
     const allowTransparent = !(property === 'TextColor' || property === 'StrokeColor');
 
     const palette = overridePalette || this.palette;
 
+    console.log(palette);
     return (
       <div className="ColorPalette" data-element="colorPalette">
         {allowTransparent &&
@@ -99,9 +95,10 @@ class ColorPalette extends React.PureComponent {
               <div
                 className={classNames({
                   cell: true,
-                  active: color.toHexString() === bg.toLowerCase(),
+                  active: color.toHexString().toLowerCase() === bg.toLowerCase(),
+                  border: bg.toLowerCase() === '#ffffff',
                 })}
-                style={{ backgroundColor: bg, border: '1px solid' }}
+                style={{ backgroundColor: bg }}
               />
             </div>,
           ),
