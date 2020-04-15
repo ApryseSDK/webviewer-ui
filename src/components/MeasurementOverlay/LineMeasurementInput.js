9b16001b7f396e20f0f37830922036c4794658ed
Armando <bollain@gmail.com>
2020-04-14 18:13:48 -0700

    Add onkey for Enter When enter is pressed in edit mode for length or angle, finish annot

--- src/components/MeasurementOverlay/LineMeasurementInput.js
+++ src/components/MeasurementOverlay/LineMeasurementInput.js
@@ -100,6 +100,7 @@ function LineMeasurementInput(props) {
   const onAngleChange = event => {
     setAngle(event.target.value);
     setLineAngle(event);
+    finishAnnotation();
   };
 
   const computeAngle = useCallback(() => {
@@ -133,7 +134,7 @@ function LineMeasurementInput(props) {
           onBlur={event => validateLineLength(event)}
           onKeyDown={event => {
             if (event.key === 'Enter') {
-              validateLineLength(event);
+              onInputChanged(event);
             }
           }}
         /> {unit}
@@ -146,6 +147,11 @@ function LineMeasurementInput(props) {
           max="360"
           value={angle}
           onChange={event => onAngleChange(event)}
+          onKeyDown={event => {
+            if (event.key === 'Enter') {
+              onAngleChange(event);
+            }
+          }}
         /> &deg;
       </div>
     </div>
