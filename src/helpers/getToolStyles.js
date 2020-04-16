e791f068a9fb883bf9c992fc24c51ea719ef99c0
ZhijieZhang <zhijiezhang0124@gmail.com>
2020-04-16 13:14:48 -0700

    fix(TextHighlightCreateTool): don't show the opacity slider when the browser(IE11) doesn't support multiply blending

--- src/helpers/getToolStyles.js
+++ src/helpers/getToolStyles.js
@@ -2,7 +2,27 @@ import core from 'core';
 
 export default toolName => {
   const tool = core.getTool(toolName);
-  if (tool) {
-    return tool.defaults;
+  const ToolNames = window.Tools.ToolNames;
+
+  if (tool?.name === ToolNames.REDACTION) {
+    // don't want sliders to show up for redaction tool
+    tool.defaults = {
+      ...tool.defaults,
+      StrokeThickness: null,
+      Opacity: null,
+      FontSize: null,
+    };
   }
+
+  const isHighlightTool =
+    tool?.name === ToolNames.HIGHLIGHT ||
+    tool?.name === ToolNames.HIGHLIGHT2 ||
+    tool?.name === ToolNames.HIGHLIGHT3 ||
+    tool?.name === ToolNames.HIGHLIGHT4;
+
+  if (isHighlightTool && !core.isBlendModeSupported('multiply')) {
+    tool.defaults.Opacity = null;
+  }
+
+  return tool?.defaults;
 };
