57c5e4b4e3fa9807114a95eb52f69d1f9274eae2
Kristian Hein <kristianhein@gmail.com>
2020-05-05 14:18:41 -0700

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import 'core-js/stable';
+import "regenerator-runtime/runtime";
+console.log('9');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
