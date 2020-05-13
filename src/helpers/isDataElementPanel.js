5836d7c7be3f7c5289097e5fe27721c6c5a1e851
Kristian Hein <kristianhein@gmail.com>
2020-05-13 14:42:29 -0700

    missing file

---
+++ src/helpers/isDataElementPanel.js
@@ -0,0 +1,14 @@
+export default (dataElement, state) => getLeftPanelDataElements(state).includes(dataElement);
+
+export const getLeftPanelDataElements = state => {
+  const defaultLeftPanels = [
+    'notesPanel',
+    'thumbnailsPanel',
+    'outlinesPanel',
+    'layersPanel',
+    'bookmarksPanel',
+  ];
+  const customPanels = state.viewer.customPanels.map(({ panel }) => panel.dataElement);
+
+  return [...defaultLeftPanels, ...customPanels];
+};
