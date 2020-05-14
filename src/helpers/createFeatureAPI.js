9314a74d89bec2d1cdb0c706ed2ca7e1ad93d2eb
Kristian Hein <kristianhein@gmail.com>
2020-05-13 18:26:56 -0700

    fix for annotations feature

--- src/helpers/createFeatureAPI.js
+++ src/helpers/createFeatureAPI.js
@@ -35,6 +35,7 @@ export default (enable, store) => features => {
       dataElements: [
         'notesPanel',
         'toggleNotesButton',
+        'toggleToolsButton',
         'toolsButton',
         'linkButton',
         'toolsHeader',
