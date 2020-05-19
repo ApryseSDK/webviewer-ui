f8abaf751f42add8c14650a063d37230f3d32e77
ZhijieZhang <zhijiezhang0124@gmail.com>
2020-05-19 08:04:33 -0700

    docs: fix typo for instance.unselectThumbnailPages

--- src/apis/unselectThumbnailPages.js
+++ src/apis/unselectThumbnailPages.js
@@ -1,12 +1,12 @@
 /**
  * Unselect selected thumbnails
- * @method WebViewerInstance#unselelctThumbnailPages
+ * @method WebViewerInstance#unselectThumbnailPages
  * @param {Array<number>} pageNumbers array of page numbers to unselect
  * @example // 6.1 and after
 WebViewer(...)
   .then(function(instance) {
     const pageNumbersToUnselect = [1, 2];
-    instance.unselelctThumbnailPages(pageNumbersToUnselect);
+    instance.unselectThumbnailPages(pageNumbersToUnselect);
   });
  */
 import selectors from 'selectors';
@@ -16,7 +16,7 @@ export default store => pageNumbers => {
   const selectedIndex = selectors.getSelectedThumbnailPageIndexes(store.getState());
 
   if (!pageNumbers || !Array.isArray(pageNumbers)) {
-    console.warn(`Invalid input, 'unselelctThumbnailPages' expect an array of numbers`);
+    console.warn(`Invalid input, 'unselectThumbnailPages' expect an array of numbers`);
     return;
   }
 
