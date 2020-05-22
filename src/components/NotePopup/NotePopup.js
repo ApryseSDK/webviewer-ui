15388c2d4d0ead6b8ccc3b7ec33f6b09dd682d64
ZhijieZhang <zhijiezhang0124@gmail.com>
2020-05-21 21:44:13 -0700

    fix(NotePopup): set status of an annotation triggers two annotationChanged events

--- src/components/NotePopup/NotePopup.js
+++ src/components/NotePopup/NotePopup.js
@@ -85,12 +85,40 @@ const NotePopup = ({ annotation, setIsEditing }) => {
   };
 
   const handleStateUpdate = state => {
-    const stateModel = 'Review';
-    const stateAnnot = core.updateAnnotationState(annotation, state, stateModel);
-    const author = core.getDisplayAuthor(stateAnnot);
+    const stateAnnot = createStateAnnot(annotation, state);
+
+    annotation.addReply(stateAnnot);
+
+    const annotManager = core.getAnnotationManager();
+    annotManager.addAnnotation(stateAnnot);
+    annotManager.trigger('addReply', [stateAnnot, annotation, annotManager.getRootAnnotation(annotation)]);
+  };
+
+  const createStateAnnot = (annotation, state) => {
+    // TODO: the code below is copied from annotManager.updateAnnotationState in WebViewer to work around the issue
+    // in https://github.com/PDFTron/webviewer-ui/issues/620
+    // the implement before wasn't causing any actual issues, but it was confusing and unnecessary to trigger two annotationChanged events when a status is set
+    // A proper fix should be done once https://trello.com/c/zWlkygNb/1023-consider-adding-a-setlocalizationhandler-to-corecontrols is implemented
+    // at that time, we could use the translation handler(t) internally in updateAnnotationState before setting the contents, and use that function instead in this component
+
+    const stateAnnot = new Annotations.StickyAnnotation();
+
+    stateAnnot['InReplyTo'] = annotation['Id'];
+    stateAnnot['X'] = annotation['X'];
+    stateAnnot['Y'] = annotation['Y'];
+    stateAnnot['PageNumber'] = annotation['PageNumber'];
+    stateAnnot['Subject'] = 'Sticky Note';
+    stateAnnot['Author'] = core.getCurrentUser();
+    stateAnnot['State'] = state;
+    stateAnnot['StateModel'] = 'Review';
+    stateAnnot['Hidden'] = true;
+
+    const displayAuthor = core.getDisplayAuthor(stateAnnot);
     const stateMessage = t(`option.state.${state.toLowerCase()}`);
-    const message = `${stateMessage} ${t('option.state.setBy')} ${author}`;
-    core.setNoteContents(stateAnnot, message);
+    const contents = `${stateMessage} ${t('option.state.setBy')} ${displayAuthor}`;
+    stateAnnot.setContents(contents);
+
+    return stateAnnot;
   };
 
   const isReply = annotation.isReply();
