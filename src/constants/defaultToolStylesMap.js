eaa8231a007b0cc74487b74de5a0fe4343f96de3
Kristian Hein <kristianhein@gmail.com>
2020-05-26 13:36:06 -0700

    preset color changes

--- src/constants/defaultToolStylesMap.js
+++ src/constants/defaultToolStylesMap.js
@@ -129,43 +129,43 @@ export default {
     StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
   },
   AnnotationCreateFreeHand: {
-    StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
+    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
   },
   AnnotationCreateFreeHand2: {
-    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
+    StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
   },
   AnnotationCreateFreeHand3: {
     StrokeColor: { R: 0, G: 0, B: 0, A: 1 },
   },
   AnnotationCreateFreeHand4: {
-    StrokeColor: { R: 37, G: 210, B: 209, A: 1 },
+    StrokeColor: { R: 0, G: 122, B: 59, A: 1 },
   },
   AnnotationCreateFreeText: {
     TextColor: { R: 78, G: 125, B: 233, A: 1 },
   },
   AnnotationCreateFreeText2: {
-    TextColor: { R: 228, G: 66, B: 52, A: 1 },
-  },
-  AnnotationCreateFreeText3: {
     TextColor: { R: 0, G: 0, B: 0, A: 1 },
   },
+  AnnotationCreateFreeText3: {
+    TextColor: { R: 228, G: 66, B: 52, A: 1 },
+  },
   AnnotationCreateFreeText4: {
-    TextColor: { R: 37, G: 210, B: 209, A: 1 },
+    TextColor: { R: 0, G: 122, B: 59, A: 1 },
   },
   AnnotationCreateCallout: {
     TextColor: { R: 78, G: 125, B: 233, A: 1 },
   },
   AnnotationCreateCallout2: {
-    TextColor: { R: 228, G: 66, B: 52, A: 1 },
-  },
-  AnnotationCreateCallout3: {
     TextColor: { R: 0, G: 0, B: 0, A: 1 },
   },
+  AnnotationCreateCallout3: {
+    TextColor: { R: 228, G: 66, B: 52, A: 1 },
+  },
   AnnotationCreateCallout4: {
-    TextColor: { R: 37, G: 210, B: 209, A: 1 },
+    TextColor: { R: 0, G: 122, B: 59, A: 1 },
   },
   AnnotationCreateSticky: {
-    StrokeColor: { R: 255, G: 230, B: 162, A: 1 },
+    StrokeColor: { R: 255, G: 205, B: 69, A: 1 },
   },
   AnnotationCreateSticky2: {
     StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
@@ -174,19 +174,19 @@ export default {
     StrokeColor: { R: 255, G: 141, B: 0, A: 1 },
   },
   AnnotationCreateSticky4: {
-    StrokeColor: { R: 128, G: 229, B: 177, A: 1 },
+    StrokeColor: { R: 0, G: 122, B: 59, A: 1 },
   },
   AnnotationCreateTextHighlight: {
-    StrokeColor: { R: 255, G: 230, B: 162, A: 1 },
+    StrokeColor: { R: 255, G: 205, B: 69, A: 1 },
   },
   AnnotationCreateTextHighlight2: {
-    StrokeColor: { R: 128, G: 229, B: 177, A: 1 },
+    StrokeColor: { R: 37, G: 210, B: 209, A: 1 },
   },
   AnnotationCreateTextHighlight3: {
-    StrokeColor: { R: 146, G: 232, B: 232, A: 1 },
+    StrokeColor: { R: 0, G: 204, B: 99, A: 1 },
   },
   AnnotationCreateTextHighlight4: {
-    StrokeColor: { R: 241, G: 160, B: 153, A: 1 },
+    StrokeColor: { R: 226, G: 161, B: 230, A: 1 },
   },
   AnnotationCreateTextUnderline: {
     StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
@@ -195,33 +195,33 @@ export default {
     StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
   },
   AnnotationCreateTextUnderline3: {
-    StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
+    StrokeColor: { R: 0, G: 0, B: 0, A: 1 },
   },
   AnnotationCreateTextUnderline4: {
-    StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
+    StrokeColor: { R: 0, G: 122, B: 59, A: 1 },
   },
   AnnotationCreateTextSquiggly: {
     StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
   },
   AnnotationCreateTextSquiggly2: {
-    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
+    StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
   },
   AnnotationCreateTextSquiggly3: {
-    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
+    StrokeColor: { R: 0, G: 0, B: 0, A: 1 },
   },
   AnnotationCreateTextSquiggly4: {
-    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
+    StrokeColor: { R: 0, G: 122, B: 59, A: 1 },
   },
   AnnotationCreateTextStrikeout: {
     StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
   },
   AnnotationCreateTextStrikeout2: {
-    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
+    StrokeColor: { R: 78, G: 125, B: 233, A: 1 },
   },
   AnnotationCreateTextStrikeout3: {
-    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
+    StrokeColor: { R: 0, G: 0, B: 0, A: 1 },
   },
   AnnotationCreateTextStrikeout4: {
-    StrokeColor: { R: 228, G: 66, B: 52, A: 1 },
+    StrokeColor: { R: 0, G: 122, B: 59, A: 1 },
   },
 };
