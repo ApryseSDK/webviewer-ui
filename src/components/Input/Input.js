5f46634c54cb4bcc68512d51a4142083f4ae113f
ZhijieZhang <zhijiezhang0124@gmail.com>
2020-05-27 12:23:18 -0700

    feat(Input): allow users to be able to disable input components using instance.disableElements

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import './Input.scss';
+
+import selectors from 'selectors';
 
 const propTypes = {
   id: PropTypes.string.isRequired,
@@ -9,20 +12,36 @@ const propTypes = {
   name: PropTypes.string,
   defaultChecked: PropTypes.bool,
   onChange: PropTypes.func,
-  label: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.object,
-  ]).isRequired,
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
   checked: PropTypes.bool,
   disabled: PropTypes.bool,
+  dataElement: PropTypes.string.isRequired,
 };
 
-const Input = React.forwardRef((props, ref) => (
-  <React.Fragment>
-    <input className="Input" ref={ref} {...props}/>
-    <label className="Input" htmlFor={props.id}>{props.label}</label>
-  </React.Fragment>
-));
+const Input = React.forwardRef((props, ref) => {
+  const isDisabled = useSelector(state => selectors.isElementDisabled(state, props.dataElement));
+
+  const inputProps = omit(props, ['dataElement', 'label']);
+
+  return isDisabled ? null : (
+    <React.Fragment>
+      <input className="Input" ref={ref} {...inputProps} />
+      <label className="Input" htmlFor={props.id} data-element={props.dataElement}>
+        {props.label}
+      </label>
+    </React.Fragment>
+  );
+});
+
+const omit = (obj, keysToOmit) => {
+  return Object.keys(obj).reduce((result, key) => {
+    if (!keysToOmit.includes(key)) {
+      result[key] = obj[key];
+    }
+
+    return result;
+  }, {});
+};
 
 Input.propTypes = propTypes;
 
