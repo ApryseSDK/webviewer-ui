5489c14c3e9b03b02856ed9dd0903dd58590dd15
Armando <bollain@gmail.com>
2020-06-03 20:12:15 -0700

    Fix API doc

--- src/apis/setAnnotationContentOverlayHandler.js
+++ src/apis/setAnnotationContentOverlayHandler.js
@@ -1,19 +1,19 @@
 import action from 'actions';
 
 /**
- * Adds a custom overlay to annotations if that annotation currently support it. Currently only the Ellipsis annotation supports it.
+ * Adds a custom overlay to annotations on mouseHover, overriding the existing overlay.
  * @method WebViewerInstance#setAnnotationContentOverlayHandler
  * @param {function} customOverlayHandler a function that takes an annotation and returns a DOM Element, which is rendered as a tooltip when hovering over the annotation
  *  * @example
 WebViewer(...)
   .then(function(instance) {
     instance.setAnnotationContentOverlayHandler(annotation => {
-        var div = document.createElement("div")
-        div.appendChild(document.createTextNode(`Created by: ${annotation.Author}`))
-        div.appendChild(document.createElement('br'))
-        div.appendChild(document.createTextNode(`Created on ${annotation.DateCreated}`))
-        return div
-    })
+        const div = document.createElement('div');
+        div.appendChild(document.createTextNode(`Created by: ${annotation.Author}`));
+        div.appendChild(document.createElement('br'));
+        div.appendChild(document.createTextNode(`Created on ${annotation.DateCreated}`));
+        return div;
+    });
   });
  */
 
