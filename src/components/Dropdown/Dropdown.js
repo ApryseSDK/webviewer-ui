6dd4d89dccf28b981eb678303e4db9e9c5bb3494
Liam Ross <liamross.code@gmail.com>
2020-06-25 14:52:33 -0700

    fix(Dropdown): added memoization to prevent rerendering

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import Icon from 'components/Icon';
 import useOnClickOutside from 'hooks/useOnClickOutside';
 import PropTypes from 'prop-types';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import useArrowFocus from '../../hooks/useArrowFocus';
 import './Dropdown.scss';
@@ -40,54 +40,67 @@ function Dropdown({ items, currentSelectionKey, translationPrefix, onClickItem }
   useArrowFocus(isOpen, onClose, overlayRef);
 
   const onClickOutside = useCallback(e => {
-    if (buttonRef.current.contains(e.target)) {
-      return;
+    if (!buttonRef.current.contains(e.target)) {
+      setIsOpen(false);
     }
-    setIsOpen(false);
   }, []);
   useOnClickOutside(overlayRef, onClickOutside);
 
-  const onClickDropdownItem = (e, key) => {
-    e.stopPropagation();
+  const onClickDropdownItem = useCallback(
+    (e, key) => {
+      e.stopPropagation();
+      onClickItem(key);
+      setIsOpen(false);
+      buttonRef.current.focus();
+    },
+    [onClickItem],
+  );
 
-    onClickItem(key);
-    setIsOpen(false);
-    buttonRef.current.focus();
-  };
-
-  const renderDropdownItems = () => {
-    return items.map(key => (
-      <button
-        key={key}
-        className={classNames('Dropdown__item', { active: key === currentSelectionKey })}
-        onClick={e => onClickDropdownItem(e, key)}
-        tabIndex={isOpen ? undefined : -1} // Just to be safe.
-      >
-        {t(`${translationPrefix}.${key}`)}
-      </button>
-    ));
-  };
+  const dropdownItems = useMemo(
+    () =>
+      items.map(key => (
+        <button
+          key={key}
+          className={classNames('Dropdown__item', { active: key === currentSelectionKey })}
+          onClick={e => onClickDropdownItem(e, key)}
+          tabIndex={isOpen ? undefined : -1} // Just to be safe.
+        >
+          {t(`${translationPrefix}.${key}`)}
+        </button>
+      )),
+    [currentSelectionKey, isOpen, items, onClickDropdownItem, t, translationPrefix],
+  );
 
   const optionIsSelected = items.some(key => key === currentSelectionKey);
+
+  const buttonStyle = useMemo(
+    () => ({ width: `${(itemsWidth || DEFAULT_WIDTH) + 2}px` }),
+    [itemsWidth],
+  );
 
   return (
     <div className="Dropdown__wrapper">
       <button
         className="Dropdown"
-        style={{ width: `${(itemsWidth || DEFAULT_WIDTH) + 2}px` }}
+        style={buttonStyle}
         data-element="dropdown"
         onClick={onToggle}
         ref={buttonRef}
       >
         <div className="picked-option">
           {optionIsSelected && (
-            <div className="picked-option-text">{t(`${translationPrefix}.${currentSelectionKey}`)}</div>
+            <div className="picked-option-text">
+              {t(`${translationPrefix}.${currentSelectionKey}`)}
+            </div>
           )}
           <Icon className="down-arrow" glyph="icon-chevron-down" />
         </div>
       </button>
-      <div className={classNames('Dropdown__items', { 'hide': !isOpen })} ref={overlayRef}>
-        {renderDropdownItems()}
+      <div
+        className={classNames('Dropdown__items', { 'hide': !isOpen })}
+        ref={overlayRef}
+      >
+        {dropdownItems}
       </div>
     </div>
   );
