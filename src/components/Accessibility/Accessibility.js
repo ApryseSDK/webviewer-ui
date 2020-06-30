f46aebbe85d2c15eb5a9e42341a4ea0766defef9
Armando <bollain@gmail.com>
2020-06-30 14:47:34 -0700

    When notes panel disabled dont show in accessibility header

--- src/components/Accessibility/Accessibility.js
+++ src/components/Accessibility/Accessibility.js
@@ -9,6 +9,7 @@ import './Accessibility.scss';
 class Accessibility extends React.PureComponent {
   static propTypes = {
     isAccessibleMode: PropTypes.bool,
+    isNotesPanelDisabled: PropTypes.bool,
   }
 
   state = {
@@ -24,13 +25,14 @@ class Accessibility extends React.PureComponent {
   }
 
   render() {
-    const { isAccessibleMode } = this.props;
+    const { isAccessibleMode, isNotesPanelDisabled } = this.props;
     const { isVisible } = this.state;
 
     if (!isAccessibleMode) {
       return null;
     }
 
+    const skiptoNotes = isNotesPanelDisabled ? null : <div className="skip-to-notes" onFocus={this.onFocus} onBlur={this.onBlur} tabIndex={0}>Notes</div>;
     const className = `Accessibility ${isVisible ? 'visible' : 'hidden'}`;
 
     return (
@@ -38,7 +40,7 @@ class Accessibility extends React.PureComponent {
         <div>Skip to: </div>
         <input className="skip-to-hack" tabIndex={-1}></input>
         <div className="skip-to-document" onFocus={this.onFocus} onBlur={this.onBlur} tabIndex={0}>Document</div>
-        <div className="skip-to-notes" onFocus={this.onFocus} onBlur={this.onBlur} tabIndex={0}>Notes</div>
+        {skiptoNotes}
       </div>
     );
   }
@@ -46,6 +48,7 @@ class Accessibility extends React.PureComponent {
 
 const mapStateToProps = state => ({
   isAccessibleMode: selectors.isAccessibleMode(state),
+  isNotesPanelDisabled: selectors.isElementDisabled(state, 'notesPanel')
 });
 
 export default connect(mapStateToProps)(Accessibility);
