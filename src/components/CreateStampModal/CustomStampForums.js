8d8492c42b079c0224dccb1d2eba9695667603a9
Sardor Isakov <sisardor@gmail.com>
2020-06-29 23:11:44 -0700

    Update

--- src/components/CreateStampModal/CustomStampForums.js
+++ src/components/CreateStampModal/CustomStampForums.js
@@ -32,19 +32,15 @@ const CustomStampForums = ({ state, setState, closeModal, createDynamicStamp }) 
 
   const [stampTextInputValue, setStampText] = useState('Draft');
   const [t] = useTranslation();
-  const [formatInput, setFormatInput] = useState(false);
   const [colorInput, setColorInput] = useState(DEFAULT_COLOR);
   const stampTool = core.getTool(TOOL_NAME);
-  const timestampOptions =  window.Tools.RubberStampCreateTool['TIMESTAMP_CHOICHES'];
 
-  // const [timestampFormat, setTimestampFormat] = useState(timestampOptions[0].value);
   const txt = updateTimestampLabel(usernameCheckbox, dateCheckbox, timeCheckbox);
   const [timestampFormat, setTimestampFormat] = useState(txt);
 
   const canvasRef = useRef();
   const canvasContainerRef = useRef();
   const inputRef = useRef();
-  const customFormatInputRef = useRef();
 
   const updateCanvas = (title, subtitle, color) => {
     const parameters = {
@@ -81,82 +77,15 @@ const CustomStampForums = ({ state, setState, closeModal, createDynamicStamp }) 
     updateCanvas(stampTextInputValue, timestampFormat, value);
   };
 
-  const handleCustomTimeFormat = e => {
-    setTimestampFormat(e.target.value);
-    updateCanvas(stampTextInputValue, e.target.value, colorInput);
-  };
-
-  const changeTimeMode = () => {
-    setFormatInput(false);
-  };
-
-  const changeFormat = e => {
-    const value = e.target.value;
-    if (value === 'other') {
-      setFormatInput(true);
-    } else {
-      setFormatInput(false);
-      setTimestampFormat(value);
-      updateCanvas(stampTextInputValue, value,  colorInput);
-    }
-  };
-
-  const isEnabled = (formatInput);
-  useEffect(() => {
-    if (isEnabled) {
-      customFormatInputRef.current.focus();
-    }
-  }, [isEnabled]);
-
   useEffect(() => {
     updateCanvas(stampTextInputValue, timestampFormat, colorInput);
   }, []);
-
-
-  let formatDropdownElement = null;
-  if (!formatInput) {
-    formatDropdownElement = <div className="StyleOption" style={{ width: '60%', alignSelf: 'center' }}>
-      <select
-        className="styles-input"
-        value={timestampFormat}
-        onChange={changeFormat}
-        style={{ textTransform: 'none', width: '100%' }}
-      >
-        {timestampOptions.map(item => {
-          return (<option key={item.id} value={item.value}>{item.value}</option>);
-        })}
-      </select>
-    </div>;
-  }
-  let formatInputElement = null;
-  if (formatInput) {
-    formatInputElement = <div style={{ width: '60%', alignSelf: 'center' }}>
-      <input style={{ width: '100%' }}
-        className="text-customstamp-input"
-        ref={customFormatInputRef}
-        type="text"
-        value={timestampFormat}
-        onChange={handleCustomTimeFormat}
-      />
-    </div>;
-  }
-
-  let cancelBtn = null;
-  if (formatInputElement) {
-    cancelBtn = <div style={{ width: '20%', alignSelf: 'center', textAlign: 'end' }}>
-      <button onClick={() => changeTimeMode()} style={{ margin:0 }}>{t('action.cancel')}</button>
-    </div>;
-  }
-
-
-
 
   const handleDateInputChange = () => {
     setDateCheckbox(!dateCheckbox);
     const txt = updateTimestampLabel(usernameCheckbox, !dateCheckbox, timeCheckbox);
     setTimestampFormat(txt);
     updateCanvas(stampTextInputValue, txt, colorInput);
-
   };
 
   const handleTimeInputChange = () => {
@@ -217,12 +146,6 @@ const CustomStampForums = ({ state, setState, closeModal, createDynamicStamp }) 
           label={t('option.customStampModal.Time')}
         />
       </div>
-      {/* <div style={{ marginTop: 10, marginBottom: 8, display: 'flex' }}>
-        <div style={{ width: '20%', alignSelf: 'center' }}> {t('option.customStampModal.timestampText')} </div>
-        {formatDropdownElement}
-        {formatInputElement}
-        {cancelBtn}
-      </div> */}
       <div className="divider-horizontal"></div>
       <div className="footer">
         <div className="stamp-close" onClick={closeModal}>
