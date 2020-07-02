2c33370f076f5b2f4e8f0463020dcfeb779b033d
Kristian Hein <kristianhein@gmail.com>
2020-07-02 10:34:10 -0700

    added back style tag to html file needed for production css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -68,7 +68,7 @@ const App = ({ removeEventHandlers }) => {
     };
     tabletBreakpoint.addListener(onBreakpoint);
 
-    dispatch(actions.setToolbarGroup('toolbarGroup-Annotate'));
+    // dispatch(actions.setToolbarGroup('toolbarGroup-Annotate'));
     return removeEventHandlers;
     // eslint-disable-next-line
   }, []);
