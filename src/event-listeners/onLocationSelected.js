21d36ae975f0569b198e1c6df7a600fd0ef5a994
Kristian Hein <kristianhein@gmail.com>
2020-07-06 16:59:39 -0700

    Fix for clicking on signature widget not selecting insert toolbar group

--- src/event-listeners/onLocationSelected.js
+++ src/event-listeners/onLocationSelected.js
@@ -7,6 +7,7 @@ export default store => () => {
   if (!signatureTool.isEmptySignature()) {
     signatureTool.addSignature();
   } else {
+    !selectors.isElementDisabled(store.getState(), 'toolbarGroup-Insert') && store.dispatch(actions.setToolbarGroup('toolbarGroup-Insert'));
     // this condition is usually met when we click on a signature widget but UI doesn't know which signature to draw
     // we check if there are saved signatures in the signature overlay to determine which component we should open
     store.dispatch(actions.setActiveToolGroup('signatureTools'));
