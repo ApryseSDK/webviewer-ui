c4e42d8a7f65cb25d4f8c4eb4d9c61859ca7c20d
Diego Felix <difel110185@gmail.com>
2020-07-08 16:18:27 -0700

    Uncommenting elements closing event dispatch on document unloading and changing SearchOverlay to SearchPanel since searchOverlay is no longer a DOM element (#732)

--- src/event-listeners/onDocumentUnloaded.js
+++ src/event-listeners/onDocumentUnloaded.js
@@ -1,7 +1,7 @@
 import actions from 'actions';
 
 export default dispatch => () => {
-  // dispatch(actions.closeElements(['pageNavOverlay', 'searchOverlay', 'leftPanel']));
+  dispatch(actions.closeElements(['pageNavOverlay', 'searchPanel', 'leftPanel']));
   dispatch(actions.setOutlines([]));
   dispatch(actions.setTotalPages(0));
 };
