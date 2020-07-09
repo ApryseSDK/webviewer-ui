fb2caaff99b7705be685b9983133fed920bd8931
Liam Ross <liamross.code@gmail.com>
2020-07-09 16:49:00 -0700

    feat: updated a11y component

--- src/components/Accessibility/Accessibility.js
+++ src/components/Accessibility/Accessibility.js
@@ -1,54 +1,62 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import selectors from 'selectors';
-
 import './Accessibility.scss';
+import classNames from 'classnames';
+import actions from 'actions';
 
-class Accessibility extends React.PureComponent {
-  static propTypes = {
-    isAccessibleMode: PropTypes.bool,
-    isNotesPanelDisabled: PropTypes.bool,
-  }
+function Accessibility() {
+  const dispatch = useDispatch();
+  const [isVisible, setIsVisible] = useState(false);
 
-  state = {
-    isVisible: false,
-  }
+  const isAccessibleMode = useSelector(selectors.isAccessibleMode);
 
-  onFocus = () => {
-    this.setState({ isVisible: true });
-  }
+  const isNotesPanelOpen = useSelector(state => selectors.isElementOpen(state, 'notesPanel'));
+  const isNotesPanelDisabled = useSelector(state => selectors.isElementDisabled(state, 'notesPanel'));
 
-  onBlur = () => {
-    this.setState({ isVisible: false });
-  }
+  const isSearchPanelOpen = useSelector(state => selectors.isElementOpen(state, 'searchPanel'));
+  const isSearchPanelDisabled = useSelector(state => selectors.isElementDisabled(state, 'searchPanel'));
 
-  render() {
-    const { isAccessibleMode, isNotesPanelDisabled } = this.props;
-    const { isVisible } = this.state;
+  const onFocus = () => setIsVisible(true);
+  const onBlur = () => setIsVisible(false);
 
-    if (!isAccessibleMode) {
-      return null;
+  const onSkipToDocument = () => {
+    document.getElementById('pageText1').focus();
+  };
+
+  const onSkipToSearch = () => {
+    if (isSearchPanelOpen) {
+      const searchEl = document.getElementById('SearchPanel__input');
+      searchEl && searchEl.focus();
+    } else {
+      dispatch(actions.openElement('searchPanel'));
     }
+  };
 
-    const skiptoNotes = isNotesPanelDisabled ? null : <div className="skip-to-notes" onFocus={this.onFocus} onBlur={this.onBlur} tabIndex={0}>Notes</div>;
-    const className = `Accessibility ${isVisible ? 'visible' : 'hidden'}`;
+  const onSkipToNotes = () => {
+    if (isNotesPanelOpen) {
+      const noteEl = document.getElementById('NotesPanel__input');
+      noteEl && noteEl.focus();
+    } else {
+      dispatch(actions.openElement('notesPanel'));
+    }
+  };
 
-    return (
-      <div className={className} data-element="accessibility">
-        <div>Skip to: </div>
-        <input className="skip-to-hack" tabIndex={-1}></input>
-        <div className="skip-to-document" onFocus={this.onFocus} onBlur={this.onBlur} tabIndex={0}>Document</div>
-        {skiptoNotes}
-      </div>
-    );
+  if (!isAccessibleMode) {
+    return null;
   }
+
+  const a11yClass = classNames('Accessibility', isVisible ? 'visible' : 'hidden');
+
+  return (
+    <div className={a11yClass} data-element="accessibility" onFocus={onFocus} onBlur={onBlur}>
+      <div>Skip to: </div>
+
+      <button onClick={onSkipToDocument}>Document</button>
+      {isSearchPanelDisabled ? null : <button onClick={onSkipToSearch}>Search</button>}
+      {isNotesPanelDisabled ? null : <button onClick={onSkipToNotes}>Notes</button>}
+    </div>
+  );
 }
 
-const mapStateToProps = state => ({
-  isAccessibleMode: selectors.isAccessibleMode(state),
-  isNotesPanelDisabled: selectors.isElementDisabled(state, 'notesPanel')
-});
-
-export default connect(mapStateToProps)(Accessibility);
+export default Accessibility;
