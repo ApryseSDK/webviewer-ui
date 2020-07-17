baa1be20fae25619c106680fd0f19f708ab3c786
Kristian Hein <kristianhein@gmail.com>
2020-07-17 13:54:21 -0700

    undid dropdown test fix

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -26,13 +26,13 @@ function Dropdown({ items, currentSelectionKey, translationPrefix, onClickItem }
 
   const [itemsWidth, setItemsWidth] = useState(DEFAULT_WIDTH);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useLayoutEffect(() => {
-    // Default to always a number.
-    const clientWidth = (overlayRef.current && overlayRef.current.clientWidth) || DEFAULT_WIDTH;
-    if (clientWidth !== itemsWidth) {
-      setItemsWidth(overlayRef.current.clientWidth);
-    }
-  });
+  // useLayoutEffect(() => {
+  //   // Default to always a number.
+  //   const clientWidth = (overlayRef.current && overlayRef.current.clientWidth) || DEFAULT_WIDTH;
+  //   if (clientWidth !== itemsWidth) {
+  //     setItemsWidth(overlayRef.current.clientWidth);
+  //   }
+  // });
 
   const onClose = useCallback(() => setIsOpen(false), []);
   const onToggle = useCallback(() => setIsOpen(prev => !prev), []);
