d5db7bc733c23784b259f2f480a8946888593a41
khein-pdftron <khein@pdftron.com>
2020-07-17 12:18:44 -0700

    bug fix (#782)
    Co-authored-by: Kristian Hein <kristianhein@gmail.com>

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -24,15 +24,15 @@ function Dropdown({ items, currentSelectionKey, translationPrefix, onClickItem }
 
   const [isOpen, setIsOpen] = useState(false);
 
-  const [itemsWidth, setItemsWidth] = useState(0);
+  const [itemsWidth, setItemsWidth] = useState(DEFAULT_WIDTH);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // useLayoutEffect(() => {
-  //   // Default to 0 so it's always a number.
-  //   const clientWidth = (overlayRef.current && overlayRef.current.clientWidth) || 0;
-  //   if (clientWidth !== items.itemsWidth) {
-  //     setItemsWidth(overlayRef.current.clientWidth);
-  //   }
-  // });
+  useLayoutEffect(() => {
+    // Default to always a number.
+    const clientWidth = (overlayRef.current && overlayRef.current.clientWidth) || DEFAULT_WIDTH;
+    if (clientWidth !== itemsWidth) {
+      setItemsWidth(overlayRef.current.clientWidth);
+    }
+  });
 
   const onClose = useCallback(() => setIsOpen(false), []);
   const onToggle = useCallback(() => setIsOpen(prev => !prev), []);
