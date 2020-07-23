769411463fff9199c728e08eee287e28be7886cd
Jussi Nieminen <jnieminen@pdftron.com>
2020-07-23 08:47:11 -0700

    Expose activeSearchResultChanged from DocumentViewer

--- src/core/eventListener.js
+++ src/core/eventListener.js
@@ -36,6 +36,7 @@ const getEventToObjectMap = () => {
     mouseRightUp: window.docViewer,
     pageComplete: window.docViewer,
     searchInProgress: window.docViewer,
+    activeSearchResultChanged: window.docViewer,
     textSelected: window.docViewer,
     beginRendering: window.docViewer,
     finishedRendering: window.docViewer,
