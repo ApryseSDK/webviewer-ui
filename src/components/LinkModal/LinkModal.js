032ea259e8b886f4c23bc1bcb883f7ffe311ef06
Zhijie Zhang <zhijiezhang@Zhijies-MacBook-Pro.local>
2020-07-30 12:44:36 -0700

    fix(LinkModal): hitting enter to add link annotations will refresh the page

--- src/components/LinkModal/LinkModal.js
+++ src/components/LinkModal/LinkModal.js
@@ -119,7 +119,9 @@ const LinkModal = () => {
     core.addAnnotations([highlight]);
   };
 
-  const addURLLink = () => {
+  const addURLLink = e => {
+    e.preventDefault();
+
     const links = createLink();
 
     const action = new window.Actions.URI({ uri: url });
@@ -137,7 +139,9 @@ const LinkModal = () => {
     closeModal();
   };
 
-  const addPageLink = () => {
+  const addPageLink = e => {
+    e.preventDefault();
+
     const links = createLink();
 
     const Dest = window.Actions.GoTo.Dest;
