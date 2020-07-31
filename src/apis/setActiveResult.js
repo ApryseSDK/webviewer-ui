56f61604dc414abe96e4536d37df184fa8e93cca
Jussi Nieminen <jnieminen@pdftron.com>
2020-07-31 09:05:14 -0700

    Improved code comment

--- src/apis/setActiveResult.js
+++ src/apis/setActiveResult.js
@@ -2,8 +2,8 @@ import core from 'core';
 
 export default (result, index) => {
   if (!result && index >= 0) {
-    // core doesn't have concept of active result index. In previous implementation
-    // it was theoretically have a case not setting result, but passing index and that should have worked
+    // core doesn't have concept of active result index. In previous implementation,
+    // there is theoretically a case where not setting result, but passing index and that should have worked
     // To have backward compatibility here, we check if user didn't give result and then try to use index
     // to retrieve result object from all current search results and set that as new active search result
     const searchResults = core.getPageSearchResults();
