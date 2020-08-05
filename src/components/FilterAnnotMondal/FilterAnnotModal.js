6eafbaa9ab7f4c7c8e49d675b48bc2bc599da60e
Andrey <a.so777@live.ca>
2020-08-05 15:50:02 -0700

    Styling updates

--- src/components/FilterAnnotMondal/FilterAnnotModal.js
+++ src/components/FilterAnnotMondal/FilterAnnotModal.js
@@ -28,16 +28,22 @@ const FilterAnnotModal = () => {
   const [typesFilter, setTypesFilter] = useState([]);
 
   const filterApply = () => {
-    dispatch(actions.setCustomNoteFilter(annot => {
-      return (typesFilter.includes(annot.Subject.toLowerCase().replace(/\s/g, '')) && (authorFilter.includes(annot.Author)));
-    }));
+    dispatch(
+      actions.setCustomNoteFilter(annot => {
+        return (
+          typesFilter.includes(annot.Subject.toLowerCase().replace(/\s/g, '')) && authorFilter.includes(annot.Author)
+        );
+      }),
+    );
     closeModal();
   };
 
   const filterClear = () => {
-    dispatch(actions.setCustomNoteFilter(annot => {
-      return true;
-    }));
+    dispatch(
+      actions.setCustomNoteFilter(annot => {
+        return true;
+      }),
+    );
     setAuthorFilter([]);
     setTypesFilter([]);
     closeModal();
@@ -71,63 +77,72 @@ const FilterAnnotModal = () => {
     <Swipeable onSwipedUp={closeModal} onSwipedDown={closeModal} preventDefaultTouchmoveEvent>
       <div className={modalClass} data-element="filterModal" onMouseDown={closeModal}>
         <div className="container" onMouseDown={e => e.stopPropagation()}>
-          <div className="swipe-indicator" />
-          <div className="filter-options">
-            <div className="filter">
-              <div className="heading">{t('option.filterAnnotModal.commentBy')}</div>
-              <div className="buttons">
-                {[...authors].map((val, index) => {
-                  return (
-                    <Choice
-                      type="checkbox"
-                      key={index}
-                      label={val}
-                      checked={authorFilter.includes(val)}
-                      id={val}
-                      onChange={e => {
-                        if (authorFilter.indexOf(e.target.getAttribute('id')) === -1) {
-                          setAuthorFilter([...authorFilter, e.target.getAttribute('id')]);
-                        } else {
-                          setAuthorFilter(authorFilter.filter(author => author !== e.target.getAttribute('id')));
-                        }
-                      }}
-                    />
-                  );
-                })}
+          {core.getAnnotationsList().length > 0 ? (
+            <div className="filter-modal">
+              <div className="swipe-indicator" />
+              <div className="filter-options">
+                <div className="filter">
+                  <div className="heading">{t('option.filterAnnotModal.commentBy')}</div>
+                  <div className="buttons">
+                    {[...authors].map((val, index) => {
+                      return (
+                        <Choice
+                          type="checkbox"
+                          key={index}
+                          label={val}
+                          checked={authorFilter.includes(val)}
+                          id={val}
+                          onChange={e => {
+                            if (authorFilter.indexOf(e.target.getAttribute('id')) === -1) {
+                              setAuthorFilter([...authorFilter, e.target.getAttribute('id')]);
+                            } else {
+                              setAuthorFilter(authorFilter.filter(author => author !== e.target.getAttribute('id')));
+                            }
+                          }}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="filter">
+                  <div className="heading">{t('option.filterAnnotModal.types')}</div>
+                  <div className="buttons">
+                    {[...annotTypes].map((val, index) => {
+                      return (
+                        <Choice
+                          type="checkbox"
+                          key={index}
+                          label={t(`annotation.${val}`)}
+                          checked={typesFilter.includes(val)}
+                          id={val}
+                          onChange={e => {
+                            if (typesFilter.indexOf(e.target.getAttribute('id')) === -1) {
+                              setTypesFilter([...typesFilter, e.target.getAttribute('id')]);
+                            } else {
+                              setTypesFilter(typesFilter.filter(type => type !== e.target.getAttribute('id')));
+                            }
+                          }}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="footer">
+                <div className="filter-annot-clear" onClick={filterClear}>
+                  {t('action.clear')}
+                </div>
+                <div className="filter-annot-apply" onClick={filterApply}>
+                  {t('action.apply')}
+                </div>
               </div>
             </div>
-            <div className="filter">
-              <div className="heading">{t('option.filterAnnotModal.types')}</div>
-              <div className="buttons">
-                {[...annotTypes].map((val, index) => {
-                  return (
-                    <Choice
-                      type="checkbox"
-                      key={index}
-                      label={t(`annotation.${val}`)}
-                      checked={typesFilter.includes(val)}
-                      id={val}
-                      onChange={e => {
-                        if (typesFilter.indexOf(e.target.getAttribute('id')) === -1) {
-                          setTypesFilter([...typesFilter, e.target.getAttribute('id')]);
-                        } else {
-                          setTypesFilter(typesFilter.filter(type => type !== e.target.getAttribute('id')));
-                        }
-                      }}
-                    />
-                  );
-                })}
-              </div>
+          ) : (
+            <div>
+              <div className="swipe-indicator" />
+              <div className="message">{t('message.noAnnotationsFilter')}</div>
             </div>
-          </div>
-          <div className="footer">
-            <div className="filter-annot-clear" onClick={filterClear}>
-              {t('action.clear')}
-            </div>
-            <div className="filter-annot-apply" onClick={filterApply}>
-              {t('action.apply')}
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </Swipeable>
