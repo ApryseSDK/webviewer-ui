6627b8b6656fabd1653c07e72059fbc5e8bc48dd
Kristian Hein <kristianhein@gmail.com>
2020-08-08 23:41:26 -0700

    progress

--- src/components/ToggleElementButton/ToggleElementButton.js
+++ src/components/ToggleElementButton/ToggleElementButton.js
@@ -39,11 +39,11 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onClick: () => {
     // hack for new ui
-    // if (ownProps.element === 'detachedSearchPanel') {
-    //   dispatch(actions.closeElement('detachedNotesPanel'));
-    // } else if (ownProps.element === 'detachedNotesPanel') {
-    //   dispatch(actions.closeElement('detachedSearchPanel'));
-    // }
+    if (ownProps.element === 'detachedSearchPanel') {
+      dispatch(actions.closeElement('detachedNotesPanel'));
+    } else if (ownProps.element === 'detachedNotesPanel') {
+      dispatch(actions.closeElement('detachedSearchPanel'));
+    }
     dispatch(actions.toggleElement(ownProps.element));
   },
 });
