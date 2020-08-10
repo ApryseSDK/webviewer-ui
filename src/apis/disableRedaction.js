cf525f91bb94445da4ae1a3c40b8c86b710f0439
Zhijie Zhang <zhijiezhang0124@gmail.com>
2020-08-10 12:13:15 -0700

    docs: remove deprecated warning messages for APIs

--- src/apis/disableRedaction.js
+++ src/apis/disableRedaction.js
@@ -1,12 +1,6 @@
 import Feature from 'constants/feature';
-import warnDeprecatedAPI from 'helpers/warnDeprecatedAPI';
 import disableFeatures from './disableFeatures';
 
 export default store => () => {
-  warnDeprecatedAPI(
-    'disableRedaction()',
-    'disableFeatures([instance.Feature.Redaction])',
-    '7.0',
-  );
   disableFeatures(store)([Feature.Redaction]);
 };
--- src/apis/rotateCounterClockwise.js
+++ src/apis/rotateCounterClockwise.js
@@ -1,11 +1,5 @@
 import core from 'core';
-import warnDeprecatedAPI from 'helpers/warnDeprecatedAPI';
 
 export default () => {
-  warnDeprecatedAPI(
-    'rotateCounterClockwise',
-    'docViewer.rotateCounterClockwise',
-    '7.0',
-  );
   core.rotateCounterClockwise();
 };
