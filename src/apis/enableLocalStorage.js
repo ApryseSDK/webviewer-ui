6c1318cc3a49b3597a63edf7c1502ebf3535026c
Zhijie Zhang <zhijiezhang0124@gmail.com>
2020-08-10 12:13:15 -0700

    docs: remove deprecated warning messages for APIs

--- src/apis/enableLocalStorage.js
+++ src/apis/enableLocalStorage.js
@@ -1,12 +1,6 @@
 import Feature from 'constants/feature';
-import warnDeprecatedAPI from 'helpers/warnDeprecatedAPI';
 import enableFeatures from './enableFeatures';
 
 export default store => () => {
-  warnDeprecatedAPI(
-    'enableLocalStorage()',
-    'enableFeatures([instance.Feature.LocalStorage])',
-    '7.0',
-  );
   enableFeatures(store)([Feature.LocalStorage]);
 };
--- src/apis/openElement.js
+++ src/apis/openElement.js
@@ -1,11 +1,5 @@
-import warnDeprecatedAPI from 'helpers/warnDeprecatedAPI';
 import actions from 'actions';
 
 export default store => dataElement => {
-  warnDeprecatedAPI(
-    'openElement(dataElement)',
-    'openElements([dataElement])',
-    '7.0',
-  );
   store.dispatch(actions.openElement(dataElement));
 };
