c72e63776394b0a2a0e7813612e7943ec95f44f3
unknown <andrewWayneYip@hotmail.com>
2020-08-18 17:13:53 -0700

    fix

---
+++ src/helpers/getLatestActivityDate.js
@@ -0,0 +1,22 @@
+export default annotation => {
+  const latestReplyActivityDate = annotation
+    .getReplies()
+    .reduce((acc, reply) => {
+      const replyDate = reply.DateModified || reply.DateCreated;
+      if (replyDate && new Date(replyDate) > new Date(acc)) {
+        return replyDate;
+      }
+      return acc;
+    }, 0);
+
+  if (latestReplyActivityDate) {
+    return latestReplyActivityDate;
+  }
+  if (annotation.DateModified) {
+    return annotation.DateModified;
+  }
+  if (annotation.DateCreated) {
+    return annotation.DateCreated;
+  }
+  return new Date();
+};
