c310ae92406f210ef56c06e28b92fd0c620524c8
achenPDFTron <achen@pdftron.com>
2020-08-25 08:49:28 -0700

    Changes to allow streaming: false to be passed explicitly and be differentiated from not passing the streaming option (#790)
    # Conflicts:
    #	src/helpers/loadDocument.js

--- src/helpers/loadDocument.js
+++ src/helpers/loadDocument.js
@@ -75,6 +75,10 @@ const extractXodOptions = options => {
     xodOptions.decryptOptions = options.decryptOptions;
   }
 
+  if (options.decrypt) {
+    xodOptions.decrypt = options.decrypt;
+  }
+
   if (options.streaming !== null) {
     xodOptions.streaming = options.streaming === 'true';
   }
