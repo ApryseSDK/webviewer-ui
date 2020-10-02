d3ebd06e6c23f0e5e9a1db1a3712765421088469
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-10-02 09:51:25 -0700

    Fixed issue where WV fails if icon is missing (#1960)

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -49,7 +49,13 @@ class Icon extends React.PureComponent {
   render() {
     const { className = '', color, glyph, fillClass = '', disabled } = this.props;
     const filter = (color && (color === 'rgba(255, 255, 255, 1)' || color === 'rgb(255, 255, 255)')) ? 'drop-shadow(0 0 .5px #333)' : undefined;
-    const svgElement = this.isInlineSvg() ? glyph : require(`../../../assets/icons/${this.props.glyph}.svg`);
+    let svgElement;
+
+    try {
+      svgElement = this.isInlineSvg() ? glyph : require(`../../../assets/icons/${this.props.glyph}.svg`);
+    } catch {
+      svgElement = undefined;
+    }
 
     return (
       <div
