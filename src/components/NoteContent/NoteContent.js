3366660cf8c99d3e3f1cbe35cc8c54c5e58a8582
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-10-08 07:52:23 -0700

    fix (#2044)
    Co-authored-by: unknown <andrewWayneYip@hotmail.com>

--- src/components/NoteContent/NoteContent.js
+++ src/components/NoteContent/NoteContent.js
@@ -23,6 +23,7 @@ import mentionsManager from 'helpers/MentionsManager';
 import { mapAnnotationToKey, getDataWithKey } from 'constants/map';
 import escapeHtml from 'helpers/escapeHtml';
 import getFillClass from 'helpers/getFillClass';
+import getLatestActivityDate from 'helpers/getLatestActivityDate';
 import useDidUpdate from 'hooks/useDidUpdate';
 import actions from 'actions';
 import selectors from 'selectors';
@@ -153,7 +154,7 @@ const NoteContent = ({ annotation, isEditing, setIsEditing, noteIndex, textAreaV
           <div className="author-and-time">
             {renderAuthorName(annotation)}
             <div className="date-and-time">
-              {dayjs(annotation.DateCreated || new Date()).format(noteDateFormat)}
+              {dayjs(getLatestActivityDate(annotation)).format(noteDateFormat)}
             </div>
           </div>
           <div className="state-and-overflow">
