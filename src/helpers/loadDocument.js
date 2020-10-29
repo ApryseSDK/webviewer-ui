4251239d019a740243b06b739fd1b7f4f2591bd7
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-10-29 14:02:18 -0700

    [7.1][Bugfix] fixes xodOptions in loadDocument cherry-pick (#2205)

--- src/helpers/loadDocument.js
+++ src/helpers/loadDocument.js
@@ -69,11 +69,7 @@ const transformPasswordOption = (password, dispatch) => {
 };
 
 const extractXodOptions = options => {
-  if (options.hasOwnProperty('xodOptions')) {
-    options = options.xodOptions
-  }
-
-  const xodOptions = {};
+  const xodOptions = options.xodOptions || {};
 
   if (options.decryptOptions) {
     xodOptions.decrypt = window.CoreControls.Encryption.decrypt;
