73fc1f08e4de4124c81401eb7b3699f4dede67ca
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-10-29 16:02:40 -0700

    [Feature] Updated mergeDocument to be able to merge with no document (#2197)
    * Updated mergeDocument to be able to merge with no document
    
    * Remove space because of lint

--- src/core/mergeDocument.js
+++ src/core/mergeDocument.js
@@ -2,4 +2,13 @@
  * https://www.pdftron.com/api/web/CoreControls.Document.html#mergeDocument__anchor
  */
 
-export default (documentToMerge, position) => window.docViewer.getDocument().mergeDocument(documentToMerge, position);
+export default async(documentToMerge, position) => {
+  const { docViewer } = window;
+  if (docViewer.getDocument()) {
+    return docViewer.getDocument().mergeDocument(documentToMerge, position);
+  } else {
+    await docViewer.loadDocument(documentToMerge);
+    const doc = docViewer.getDocument();
+    return { filename: doc.getFilename(), pages: [1] };
+  }
+};
