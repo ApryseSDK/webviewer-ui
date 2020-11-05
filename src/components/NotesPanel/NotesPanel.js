3083a530b1ac9d16e70e93d718775cf0f64ec064
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-11-05 13:19:48 -0800

    fix (#2235)
    Co-authored-by: unknown <andrewWayneYip@hotmail.com>

--- src/components/NotesPanel/NotesPanel.js
+++ src/components/NotesPanel/NotesPanel.js
@@ -122,15 +122,6 @@ const NotesPanel = ({ currentLeftPanelWidth }) => {
     return () => core.removeEventListener('annotationSelected', onAnnotationSelected);
   }, []);
 
-  let singleSelectedNoteIndex = -1;
-  useEffect(() => {
-    if (Object.keys(selectedNoteIds).length && singleSelectedNoteIndex !== -1) {
-      listRef.current?.scrollToRow(singleSelectedNoteIndex);
-    }
-    // we only want this effect to happen when we select some notes
-    // eslint-disable-next-line
-  }, [selectedNoteIds]);
-
   // useEffect(() => {
   //   if (isOpen) {
   //     dispatch(actions.closeElements(['searchPanel', 'searchOverlay']));
@@ -275,12 +266,23 @@ const NotesPanel = ({ currentLeftPanelWidth }) => {
     </div>
   );
 
+  let singleSelectedNoteIndex = -1;
   // keep track of the index of the single selected note in the sorted and filtered list
   // in order to scroll it into view in this render effect
   const ids = Object.keys(selectedNoteIds);
   if (ids.length === 1) {
     singleSelectedNoteIndex = notesToRender.findIndex(note => note.Id === ids[0]);
   }
+
+  useEffect(() => {
+    if (Object.keys(selectedNoteIds).length && singleSelectedNoteIndex !== -1) {
+      setTimeout(() => {
+        listRef.current?.scrollToRow(singleSelectedNoteIndex);
+      },0)
+    }
+    // we only want this effect to happen when we select some notes
+    // eslint-disable-next-line
+  }, [selectedNoteIds]); 
 
   let style = {};
   if (!isMobile) {
