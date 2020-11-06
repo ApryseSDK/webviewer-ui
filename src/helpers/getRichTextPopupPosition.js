f14e3f4536ee8edd20303f3b24d552f76d7b40ec
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-11-05 17:13:44 -0800

    [Bugfix] Fixed error when disabling the richtext popup (#2231)
    * Fixed error when disabling the richtext popup
    
    * Moved checks earlier
    
    * Removed duplicate check
    
    * Added additional checks
    
    * Added jest test
    
    * Fixed lint error

--- src/helpers/getRichTextPopupPosition.js
+++ src/helpers/getRichTextPopupPosition.js
@@ -2,26 +2,28 @@ import { calcPopupLeft, calcPopupTop } from 'helpers/getPopupPosition';
 import core from 'core';
 
 export default (annotation, popup) => {
+  if (!popup || !popup.current) {
+    return;
+  }
   const editorContainer = document.querySelector(
     `#pageWidgetContainer${annotation.PageNumber} [id="freetext-editor-${annotation.Id}"]`
   );
   const scrollContainer = core.getScrollViewElement();
   const padding = 2 * parseFloat(annotation.StrokeThickness) * core.getZoom();
-  let cBox = editorContainer.getBoundingClientRect();
-  cBox = {
+  const cBox = editorContainer.getBoundingClientRect();
+  const cInfo = {
     topLeft: {
       x: cBox.left + scrollContainer.scrollLeft - padding,
-      y: cBox.top + scrollContainer.scrollTop - padding,
+      y: cBox.top + scrollContainer.scrollTop - padding
     },
     bottomRight: {
       x: cBox.right + scrollContainer.scrollLeft + padding,
-      y: cBox.bottom + scrollContainer.scrollTop + padding,
-    },
+      y: cBox.bottom + scrollContainer.scrollTop + padding
+    }
   };
   const pBox = popup.current.getBoundingClientRect();
-
   return {
-    left: calcPopupLeft(cBox, pBox),
-    top: calcPopupTop(cBox, pBox),
+    left: calcPopupLeft(cInfo, pBox),
+    top: calcPopupTop(cInfo, pBox)
   };
 };
