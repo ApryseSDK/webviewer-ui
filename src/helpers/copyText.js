2834e55278e9b8c5280fbc4f2d89e47f44611dbb
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-11-13 13:41:32 -0800

    fix an issue with copy text at the second time in chrome (#2270)

--- src/helpers/copyText.js
+++ src/helpers/copyText.js
@@ -1,6 +1,11 @@
 import core from 'core';
 
 export default () => {
+  // The copyText function should works for 2 cases:
+  // 1. User copies from the side panel (window.getSelection())
+  // 2. User copies text from the document (core.getSelectedText())
+  // We manually to clear the value of #copy-textarea, and this will reset window.getSelection().toString() to ""
+  document.querySelector('#copy-textarea').value = '';
   if (window.getSelection()?.toString()) {
     // if we are selecting some text in the UI (i.e. text in note panel) just let it do the normal behaviour
     return;
