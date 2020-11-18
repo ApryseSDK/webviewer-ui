94f8af0246ce9df07bd0b2ee5560d4b1d8f556fe
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2020-11-17 18:24:56 -0800

    Bugfix/search panel clearing new document (#2273)

--- src/event-listeners/onDocumentUnloaded.js
+++ src/event-listeners/onDocumentUnloaded.js
@@ -1,7 +1,9 @@
 import actions from 'actions';
+import core from 'core';
 
 export default dispatch => () => {
   dispatch(actions.closeElements(['pageNavOverlay', 'notesPanel', 'searchPanel', 'leftPanel']));
   dispatch(actions.setOutlines([]));
   dispatch(actions.setTotalPages(0));
+  core.clearSearchResults();
 };
