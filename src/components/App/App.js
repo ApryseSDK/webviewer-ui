4961b71ff058920476cbc6cafb3355d8c474a428
achenPDFTron <achen@pdftron.com>
2020-12-03 12:09:04 -0800

    highlight will not select the text so that the highlighted text do not show up in note panel

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -58,6 +58,9 @@ const App = ({ removeEventHandlers }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // when highlighting text, the highlight annotation's content should not be set to the highlighted text
+    // this will prevent the highlighted text from showing up in the notes panel
+    Tools.TextAnnotationCreateTool['AUTO_SET_TEXT'] = false;
     defineReaderControlAPIs(store);
     fireEvent('viewerLoaded');
 
