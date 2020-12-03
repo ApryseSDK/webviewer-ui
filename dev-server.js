401fd205635cf0c6c3e2abac2e6c3fe9a75b27f1
achenPDFTron <achen@pdftron.com>
2020-12-03 11:40:01 -0800

    Changed port

--- dev-server.js
+++ dev-server.js
@@ -40,14 +40,14 @@ app.get('/sample-url', (req, res) => {
   );
 });
 
-app.listen(3000, '0.0.0.0', err => {
+app.listen(3003, '0.0.0.0', err => {
   if (err) {
     console.error(err);
   } else {
     // eslint-disable-next-line
-    console.info(`Listening at localhost:3000 (http://${ip.address()}:3000)`);
+    console.info(`Listening at localhost:3003 (http://${ip.address()}:3003)`);
     opn(
-      'http://localhost:3000/#d=https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf&a=1',
+      'http://localhost:3003/#d=https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf&a=1',
     );
   }
 });
