11c41f03988e4317528b1382229a29d4e92320d6
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-01-11 09:25:19 -0800

    Bugfix/no group annots when printing (#2424)
    * able to draw widget annots
    
    * rename fxn
    
    * addressed TODO
    
    * added more comments
    
    * addressed PR comments
    
    * renamed includeAnnots to be includeAnnotations and removed global includeAnnotations variable
    
    * changed all wording and variable of annot(s) to annotation(s)

--- src/helpers/print.js
+++ src/helpers/print.js
@@ -9,7 +9,6 @@ import { isSafari, isChromeOniOS } from 'helpers/device';
 import core from 'core';
 
 let pendingCanvases = [];
-let includeAnnotations = false;
 let printQuality = 1;
 let colorMap;
 
@@ -94,19 +93,18 @@ const printPdf = () =>
       });
   });
 
-export const creatingPages = (pagesToPrint, includeComments, includeAnnot, printQualty, sortStrategy, clrMap, dateFormat, onProgress) => {
+export const creatingPages = (pagesToPrint, includeComments, includeAnnotations, printQualty, sortStrategy, clrMap, dateFormat, onProgress) => {
   const createdPages = [];
   pendingCanvases = [];
-  includeAnnotations = includeAnnot;
   printQuality = printQualty;
   colorMap = clrMap;
 
   pagesToPrint.forEach(pageNumber => {
-    const printableAnnotations = getPrintableAnnotations(pageNumber);
-    createdPages.push(creatingImage(pageNumber, printableAnnotations));
+    const printableAnnotationNotes = getPrintableAnnotationNotes(pageNumber);
+    createdPages.push(creatingImage(pageNumber, includeAnnotations));
 
-    if (includeComments && printableAnnotations.length) {
-      const sortedNotes = getSortStrategies()[sortStrategy].getSortedNotes(printableAnnotations);
+    if (includeComments && printableAnnotationNotes) {
+      const sortedNotes = getSortStrategies()[sortStrategy].getSortedNotes(printableAnnotationNotes);
       createdPages.push(creatingNotesPage(sortedNotes, pageNumber, dateFormat));
     }
 
@@ -144,7 +142,7 @@ export const cancelPrint = () => {
   pendingCanvases.forEach(id => doc.cancelLoadCanvas(id));
 };
 
-const getPrintableAnnotations = pageNumber =>
+const getPrintableAnnotationNotes = pageNumber =>
   core
     .getAnnotationsList()
     .filter(
@@ -156,7 +154,7 @@ const getPrintableAnnotations = pageNumber =>
         annotation.Printable,
     );
 
-const creatingImage = (pageNumber, printableAnnotations) =>
+const creatingImage = (pageNumber, includeAnnotations) =>
   new Promise(resolve => {
     const pageIndex = pageNumber - 1;
     const zoom = 1;
@@ -164,16 +162,26 @@ const creatingImage = (pageNumber, printableAnnotations) =>
     const onCanvasLoaded = async canvas => {
       pendingCanvases = pendingCanvases.filter(pendingCanvas => pendingCanvas !== id);
       positionCanvas(canvas, pageIndex);
-
-      if (includeAnnotations) {
-        await drawAnnotationsOnCanvas(canvas, pageNumber);
-      } else {
-        // disable all printable annotations before draw
-        printableAnnotations.forEach(annot => (annot.Printable = false));
-        await drawAnnotationsOnCanvas(canvas, pageNumber);
-        // enable all printable annotations after draw
-        printableAnnotations.forEach(annot => (annot.Printable = true));
+      let printableAnnotInfo = [];
+      if (!includeAnnotations) {
+        // according to Adobe, even if we exclude annotations, it will still draw widget annotations
+        const annotatationsToPrint = core.getAnnotationsList().filter(annotation => {
+          return annotation.PageNumber === pageNumber && !(annotation instanceof window.Annotations.WidgetAnnotation);
+        });
+        // store the previous Printable value so that we can set it back later
+        printableAnnotInfo = annotatationsToPrint.map(annotation => ({
+          annotation, printable: annotation.Printable
+        }));
+        // set annotations to false so that it won't show up in the printed page
+        annotatationsToPrint.forEach(annotation => {
+          annotation.Printable = false;
+        });
       }
+      await drawAnnotationsOnCanvas(canvas, pageNumber);
+
+      printableAnnotInfo.forEach(info => {
+        info.annotation.Printable = info.printable;
+      });
 
       const img = document.createElement('img');
       img.src = canvas.toDataURL();
@@ -275,7 +283,7 @@ const positionCanvas = (canvas, pageIndex) => {
 const drawAnnotationsOnCanvas = (canvas, pageNumber) => {
   const widgetAnnotations = core
     .getAnnotationsList()
-    .filter(annot => annot.PageNumber === pageNumber && annot instanceof window.Annotations.WidgetAnnotation);
+    .filter(annotation => annotation.PageNumber === pageNumber && annotation instanceof window.Annotations.WidgetAnnotation);
   // just draw markup annotations
   if (widgetAnnotations.length === 0) {
     return core.drawAnnotations(pageNumber, canvas);
@@ -284,7 +292,6 @@ const drawAnnotationsOnCanvas = (canvas, pageNumber) => {
   const widgetContainer = createWidgetContainer(pageNumber - 1);
   return core.drawAnnotations(pageNumber, canvas, true, widgetContainer).then(() => {
     document.body.appendChild(widgetContainer);
-
     return import(/* webpackChunkName: 'html2canvas' */ 'html2canvas').then(({ default: html2canvas }) => {
       return html2canvas(widgetContainer, {
         canvas,
