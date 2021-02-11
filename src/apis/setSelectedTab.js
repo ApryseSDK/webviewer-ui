b9db5a91f2f7905de1abdfd9419f3e8dec4c67d1
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-02-11 11:08:47 -0800

    Add doc for `setSelectedTab` API (#2618)
    - Add doc for `setSelectedTab` API so that it's visible on our web doc

--- src/apis/setSelectedTab.js
+++ src/apis/setSelectedTab.js
@@ -1,4 +1,15 @@
-// TODO: add documentation
+/**
+ * Sets the currently selected tab of a Tabs component.
+ * @method WebViewerInstance#setSelectedTab
+ * @param {string} id The id of the Tabs component to set.
+ * @param {string} dataElement The 'dataElement' attribute of the TabPanel component to select.
+ * @example
+WebViewer(...)
+  .then(function(instance) {
+    // Set the currently selected tab of 'signatureModal' to be the 'Type' panel.
+    instance.setSelectedTab('signatureModal', 'textSignaturePanelButton');
+  });
+ */
 
 import actions from 'actions';
 
