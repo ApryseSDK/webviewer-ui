19b2099834aefc62daea1bbc6ab427130812930e
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-04-08 09:47:48 -0700

    [Docs] Add docstring for showWarningMessage (#2894)
    * Add docstring for showWarningMessage
    
    Was previously exposed to customers but undocumented
    
    * Remove errant uses of new keyword

--- src/apis/showWarningMessage.js
+++ src/apis/showWarningMessage.js
@@ -1,3 +1,29 @@
+/**
+ * Displays the custom error message
+ * @method WebViewerInstance#showWarningMessage
+ * @param {string} options.confirmBtnText The text that will be rendered in the confirm button
+ * @param {string} options.title The title of the modal
+ * @param {string} options.message The text that will rendered in the body of the modal
+ * @param {function} options.onConfirm The callback function that will be invoked when the user clicks the Confirm button. The callback must return a Promise that resolves.
+ * @param {function} options.onCancel The callback function that will be invoked when the user clicks the Cancel button. The callback must return a Promise that resolves.
+ * @example
+WebViewer(...)
+  .then(function(instance) {
+    instance.showWarningMessage({
+      title: 'This is my warning modal title',
+      message: 'This is the body of my modal',
+      confirmBtnText: 'Okay!',
+      onConfirm: () => {
+        console.log('The user clicked the confirm button');
+        return Promise.resolve();
+      },
+      onCancel: () => {
+        console.log('The user clicked the cancel button');
+        return Promise.resolve();
+      },
+    });
+  });
+ */
 import actions from 'actions';
 
 export default store => options => {
