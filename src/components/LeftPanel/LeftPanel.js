1a8fd2a34afedc4c6ab19223dd59570c9435e9b5
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-05-14 01:24:02 -0700

    [Buffix] Restrict user to move left panel to cover 100% of the page on IE (#3048)
    * fix a bug on the panel moving on IE
    
    * small update
    
    * update based on review comment

--- src/components/LeftPanel/LeftPanel.js
+++ src/components/LeftPanel/LeftPanel.js
@@ -17,6 +17,7 @@ import core from 'core';
 import selectors from 'selectors';
 import actions from 'actions';
 import useMedia from 'hooks/useMedia';
+import { isIE } from 'helpers/device';
 
 import './LeftPanel.scss';
 
@@ -141,7 +142,12 @@ const LeftPanel = () => {
           dataElement="leftPanelResizeBar"
           minWidth={minWidth}
           onResize={_width => {
-            dispatch(actions.setLeftPanelWidth(_width));
+            let maxAllowedWidth = window.innerWidth
+            // there will be a scroll bar in IE, so we don't allow 100% page width
+            if (isIE) {
+              maxAllowedWidth = maxAllowedWidth - 30;
+            } 
+            dispatch(actions.setLeftPanelWidth(Math.min(_width, maxAllowedWidth)));
           }}
         />}
     </div>
