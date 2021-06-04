f324e0c23143fa912b1bdb362e2b40fd1700b78f
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-06-03 23:32:32 -0700

    Fix issue with zooming to the mouse not going to the correct location (#3137)
    We weren't taking into account the offsets from the UI elements

--- src/core/zoomToMouse.js
+++ src/core/zoomToMouse.js
@@ -6,5 +6,14 @@
  * @see https://www.pdftron.com/api/web/Core.DocumentViewer.html#event:zoomUpdated__anchor
  */
 export default zoomFactor => {
-  window.documentViewer.zoomToMouse(zoomFactor, 0, 0);
+  // left panel width when it's not closed
+  // it still maintains its width when closed
+  const xOffset = document.querySelector('[data-element=leftPanel]:not(.closed)')?.offsetWidth || 0;
+
+  // height of main header and the header tools that may appear below
+  const headerHeight = document.querySelector('[data-element=header]')?.offsetHeight || 0;
+  const headerToolsHeight = document.querySelector('[data-element=toolsHeader]')?.offsetHeight || 0;
+  const yOffset = headerHeight + headerToolsHeight;
+
+  window.documentViewer.zoomToMouse(zoomFactor, xOffset, yOffset);
 };
