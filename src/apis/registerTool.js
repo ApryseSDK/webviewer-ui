dd4b1846b80827f3c295fdcbe1034e38e7c4022b
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-06-07 16:32:00 -0700

    [Feature] 3d customization (#3143)
    * initial
    
    * added missing file
    
    * measurement cleanup
    
    * cleanup
    
    * lint fix
    
    * update test
    
    * missed some cleanup
    
    * missed some cleanup
    
    Co-authored-by: Kristian Hein <kristianhein@gmail.com>

--- src/apis/registerTool.js
+++ src/apis/registerTool.js
@@ -1,9 +1,9 @@
 /**
  * Registers tool in the document viewer tool mode map, and adds a button object to be used in the header. See <a href='https://www.pdftron.com/documentation/web/guides/customizing-tools' target='_blank'>Customizing tools</a> to learn how to make a tool.
- * @method WebViewerInstance#registerTool
+ * @method UI.registerTool
  * @param {object} properties Tool properties.
  * @param {string} properties.toolName Name of the tool.
- * @param {Tools.Tool} properties.toolObject Instance of the tool.
+ * @param {Core.Tools.Tool} properties.toolObject Instance of the tool.
  * @param {string} properties.buttonImage Path to an image or base64 data for the tool button.
  * @param {string} [properties.buttonName] Name of the tool button that will be used in data-element.
  * @param {string} [properties.buttonGroup] Group of the tool button belongs to.
@@ -14,7 +14,7 @@
 WebViewer(...)
   .then(function(instance) {
     // assume myCustomTool and myCustomAnnotation are already defined
-    var myTool = {
+    const myTool = {
       toolName: 'MyTool',
       toolObject: myCustomTool,
       buttonImage: 'path/to/image',
@@ -23,18 +23,21 @@ WebViewer(...)
       tooltip: 'MyTooltip'
     };
 
-    instance.registerTool(myTool, annot => annot && annot.isCustomAnnot);
+    instance.UI.registerTool(myTool, undefined, annot => annot && annot.isCustomAnnot);
   });
  */
 
 import core from 'core';
 import { register, copyMapWithDataProperties } from 'constants/map';
 import actions from 'actions';
+import { setDefaultToolStyle } from 'src/helpers/setDefaultToolStyles';
+
 export default store => (tool, customAnnotationCheckFunc) => {
   registerToolInToolModeMap(tool);
   registerToolInRedux(store, tool);
   register(tool, customAnnotationCheckFunc);
   updateColorMapInRedux(store);
+  setDefaultToolStyle(tool.toolName);
 };
 
 const registerToolInRedux = (store, tool) => {
