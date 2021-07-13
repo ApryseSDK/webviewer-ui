92ac32cbc313f462972a167670029e1ee662e3e2
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-07-13 02:07:42 -0700

    Fix deprecated API warning in Reader Mode sample (#3306)

--- src/core/enableRedaction.js
+++ src/core/enableRedaction.js
@@ -2,5 +2,9 @@
  * https://www.pdftron.com/api/web/Core.AnnotationManager.html#enableRedaction__anchor
  */
 export default enable => {
-  window.documentViewer.getAnnotationManager().enableRedaction(enable);
+  if (enable) {
+    window.documentViewer.getAnnotationManager().enableRedaction();
+  } else {
+    window.documentViewer.getAnnotationManager().disableRedaction();
+  }
 };
--- src/core/setIsAdminUser.js
+++ src/core/setIsAdminUser.js
@@ -4,5 +4,9 @@
  * @see https://www.pdftron.com/api/web/Core.AnnotationManager.html#event:updateAnnotationPermission__anchor
  */
 export default isAdmin => {
-  window.documentViewer.getAnnotationManager().setIsAdminUser(isAdmin);
+  if (isAdmin) {
+    window.documentViewer.getAnnotationManager().promoteUserToAdmin();
+  } else {
+    window.documentViewer.getAnnotationManager().demoteUserFromAdmin();
+  }
 };
