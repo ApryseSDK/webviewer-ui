89250a9cb82cb13a97385fd33207c858c3e51824
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-11-08 14:53:54 -0800

    [Bugfix] Fix deprecated packages (#4044)

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
-import '@babel/polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import './miscMock';
 import withI18n from "./withI18n";
 import withMockRedux from "./withMockRedux";
