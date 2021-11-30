d24397fe77ed7519897e76251cb5c19c8b626d77
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2021-11-29 20:14:49 -0800

    Added docs for core.syncNamespaces (#4186)
    Moved UI version to UI namespace
    
    Co-authored-by: S0ulDrag0n <SD.S0ulDrag0n@gmail.com>

--- src/apis/syncNamespaces.js
+++ src/apis/syncNamespaces.js
@@ -1,10 +1,10 @@
 import core from 'core';
 
 /**
- * Syncs the namespaces under the Core namespace for this instance as well as others instances.
+ * Syncs the namespaces under the Core namespace for the window, this instance, as well as others instances.
  * <br/><br/>
- * This is required when working with multiple instances of WebViewer since each loaded instance ends up with different classes despite sharing the same name.
- * @method Core.syncNamespaces
+ * This is required for interoperability between multiple instances of WebViewer since each loaded instance ends up with different class references despite sharing the same name.
+ * @method UI.syncNamespaces
  * @param {WebViewerInstance | object} namespaces The object containing the namespaces that will be used for all instances. This can be a WebViewer instance or an object with the namespaces inside.
  * @param {object} [namespaces.PDFNet] The PDFNet namespace. This is probably the most common that needs to be synced.
  * @param {object} [namespaces.Actions] The Actions namespace.
@@ -13,6 +13,7 @@ import core from 'core';
  * @param {object} [namespaces.Tools] The Tools namespace.
  * @param {...WebViewerInstance} otherInstances Other instances that will share the same namespaces.
  * @example
+// Loaded PDFNet directly
 const Core = window.Core;
 const PDFNet = Core.PDFNet;
 Core.setWorkerPath('../../../lib/core');
@@ -20,8 +21,9 @@ Core.enableFullPDF();
 ...
 WebViewer(...)
   .then(function(instance) {
-    instance.Core.syncNamespaces({ PDFNet });
-    // instance.Core.syncNamespaces({ PDFNet }, instance2, instance3);  // Alternative; instance, instance2, and instance3 will share the same PDFNet namespace.
+    // Force this instance to use the loaded PDFNet instead of the one loaded specific to this instance
+    instance.UI.syncNamespaces({ PDFNet });
+    // instance.UI.syncNamespaces({ PDFNet }, instance2, instance3);  // Alternative; instance, instance2, and instance3 will share the same PDFNet namespace.
     ...
   });
  */
