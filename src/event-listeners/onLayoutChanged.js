2797108442d29af8e04e5d1c541b727f3b5c421a
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2022-01-11 20:50:04 -0800

    Minor fix related to outline updates (#4352)
    Co-authored-by: Sardor Isakov <sisardor@gmail.com>

--- src/event-listeners/onLayoutChanged.js
+++ src/event-listeners/onLayoutChanged.js
@@ -2,8 +2,9 @@ import core from 'core';
 import actions from 'actions';
 import getDefaultPageLabels from 'helpers/getDefaultPageLabels';
 
-export default dispatch => ({ added, removed }) => {
-  if (added.length || removed.length) {
+export default dispatch => ({ added, removed, moved }) => {
+  const movedKeys = Object.keys(moved);
+  if (added.length || removed.length || movedKeys.length) {
     const totalPages = core.getTotalPages();
     dispatch(actions.setTotalPages(totalPages));
 
