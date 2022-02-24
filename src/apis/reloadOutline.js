1f5db94c2c31aa5895cc31d9ba1a3f50c8544f7d
侯景业 <houjingye@houkeis-MacBook-Pro.local>
2022-02-24 13:44:51 -0800

    add missing files

---
+++ src/apis/reloadOutline.js
@@ -0,0 +1,31 @@
+/**
+ * Reloads the Bookmark Outline in the WebViewer UI.
+ * @method UI.reloadOutline
+ * @example
+WebViewer(...)
+  .then(function(instance) {
+    const { documentViewer } = instance.Core;
+
+    // you must have a document loaded when calling this api
+    documentViewer.addEventListener('documentLoaded', async () => {
+      const doc = documentViewer.getDocument();
+      const pdfDoc = await doc.getPDFDoc();
+      const firstBookmark = await doc.getFirstBookmark();
+
+      const bookmarkToDelete = await firstBookmark.find('bookmark-to-delete');
+      if (bookmarkToDelete !== null && await bookmarkToDelete.isValid()) {
+        await bookmarkToDelete.delete();
+        instance.UI.reloadOutline();
+      }
+    });
+  });
+ */
+
+import core from 'core';
+import actions from 'actions';
+
+export default store => () => {
+  core.getOutlines(outlines => {
+    store.dispatch(actions.setOutlines(outlines));
+  });
+};
---
+++ src/core/syncNamespaces.js
@@ -0,0 +1,4 @@
+/**
+ * https://www.pdftron.com/api/web/Core.html#syncNamespaces__anchor
+ */
+export default namespaces => window.Core.syncNamespaces(namespaces);
