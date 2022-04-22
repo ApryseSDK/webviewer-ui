9d440260914edfea726b56a3921ce1e55fb51141
Benjamin <benjamin.zachariae@gmail.com>
2022-04-22 11:14:20 +0200

    change to "shareType"

--- src/helpers/annotationShareType.js
+++ src/helpers/annotationShareType.js
@@ -1,22 +1,22 @@
 // CUSTOM WISEflow
 
 /**
- * Get share type using custom data type "sharetype"
+ * Get share type using custom data type "shareType"
  * @param {Annotation} annot annotation to add share type to
  * @return {string} share type [ASSESSORS, PARTICIPANTS, ALL, NONE]. If none is set, returns empty string.
  */
-const getAnnotationShareType = annot => annot.getCustomData('sharetype');
+const getAnnotationShareType = annot => annot.getCustomData('shareType');
 
 /**
- * Get share type using custom data type "sharetype"
+ * Get share type using custom data type "shareType"
  * @param {Annotation} annot
  * @param {string} shareType [ASSESSORS, PARTICIPANTS, ALL, NONE]
- * 
+ *
  * @returns {Annotation} annot with share type set
  */
 const setAnnotationShareType = (annot, shareType) => {
-  annot.setCustomData('sharetype', shareType);
+  annot.setCustomData('shareType', shareType);
   return annot;
-}
+};
 
 export { getAnnotationShareType, setAnnotationShareType };
