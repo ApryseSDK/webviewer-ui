3210c4d38be17f02e4a94110915c5b6c0515cdb9
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2022-09-08 15:51:14 -0700

    [Misc] change setzoomstepfactors api to take consistent parameter unit with other related apis (#5485)
    Co-authored-by: Johnny Kung <johnny@Johnnys-MacBook-Pro.local>
    Co-authored-by: wanboli <66745183+wanboli@users.noreply.github.com>

---
+++ playwright-tests/ui/set-zoom-step-factors.spec.ts
@@ -0,0 +1,175 @@
+import { loadViewerSample, loadConsoleSample } from '../../playwright-utils';
+import { test, expect } from '@playwright/test';
+
+test.describe('SetZoomStepFactors', () => {
+    test('should print error when there is zoomStepFactors is an empty array', async ({ page, browserName }) => {
+        test.skip(browserName === 'firefox', 'TODO: investigate why this test fails on firefox');
+        const waitForTimeout = {
+            chromium: 10000,
+            firefox: 10000,
+            webkit: 10000
+          };
+      
+        const { consoleLogs } = await loadConsoleSample(page, waitForTimeout[browserName], 'viewing/viewing');
+        
+        const { iframe, waitForInstance } = await loadViewerSample(page, 'viewing/viewing');
+        const instance = await waitForInstance();
+        await instance('setZoomStepFactors', []);
+        await page.waitForTimeout(5000);
+        const expectedConsoleLog = consoleLogs.filter(log => log.includes('zoomStepFactors'));
+        expect(expectedConsoleLog.length).toBe(1);
+    });
+
+    test('should print error when there is no zoomStepFactor with startZoom: 0', async ({ page, browserName }) => {
+        test.skip(browserName === 'firefox', 'TODO: investigate why this test fails on firefox');
+        const waitForTimeout = {
+            chromium: 10000,
+            firefox: 10000,
+            webkit: 10000
+          };
+      
+        const { consoleLogs, downloadedFiles } = await loadConsoleSample(page, waitForTimeout[browserName], 'viewing/viewing');
+        
+        const { iframe, waitForInstance } = await loadViewerSample(page, 'viewing/viewing');
+        const instance = await waitForInstance();
+        await instance('setZoomStepFactors', [
+            {
+                step: 50,
+                startZoom: 50
+            }
+        ]);
+
+        await page.waitForTimeout(5000);
+        const expectedConsoleLog = consoleLogs.filter(log => log.includes('zoomStepFactors'))
+        expect(expectedConsoleLog.length).toBe(1);
+    });
+
+    test('should accept both zoomStepFactor.step and zoomStepFactor.startZoom as percentage in strings', async ({ page }) => {
+        const { iframe, waitForInstance } = await loadViewerSample(page, 'viewing/viewing');
+        const instance = await waitForInstance();
+        await page.waitForTimeout(5000);
+        const firstZoomLevel = await instance('getZoomLevel');
+        await instance('setZoomStepFactors', [
+            {
+                step: '50',
+                startZoom: '0'
+            },
+            {
+                step: '100',
+                startZoom: '200'
+            }
+        ]);
+
+        await page.waitForTimeout(1000);
+        let zoomLevels = [firstZoomLevel];
+
+        for (let i = 0; i < 5; i++) {
+            await iframe.click('[data-element=zoomInButton]');
+            await page.waitForTimeout(1000);
+            const currentZoomLevel = await instance('getZoomLevel');
+            zoomLevels.push(Number(currentZoomLevel.toFixed(2)));
+        }
+
+        for (let i = 0; i < zoomLevels.length; i++) {
+            if (zoomLevels[i + 1]) {
+                zoomLevels[i] > 2?
+                    expect(zoomLevels[i + 1]).toBe(Number((zoomLevels[i] + 1).toFixed(2))) :
+                    expect(zoomLevels[i + 1]).toBe(Number((zoomLevels[i] + 0.5).toFixed(2)));
+            }
+        }
+    });
+
+    test('should accept both zoomStepFactor.step and zoomStepFactor.startZoom as percetage in number', async ({ page }) => {
+        const { iframe, waitForInstance } = await loadViewerSample(page, 'viewing/viewing');
+        const instance = await waitForInstance();
+        await page.waitForTimeout(5000);
+        const firstZoomLevel = await instance('getZoomLevel');
+        await instance('setZoomStepFactors', [
+            {
+                step: 50,
+                startZoom: 0
+            },
+            {
+                step: 100,
+                startZoom: 200
+            }
+        ]);
+
+        await page.waitForTimeout(1000);
+        let zoomLevels = [firstZoomLevel];
+
+        for (let i = 0; i < 5; i++) {
+            await iframe.click('[data-element=zoomInButton]');
+            await page.waitForTimeout(1000);
+            const currentZoomLevel = await instance('getZoomLevel');
+            zoomLevels.push(Number(currentZoomLevel.toFixed(2)));
+        }
+
+        for (let i = 0; i < zoomLevels.length; i++) {
+            if (zoomLevels[i + 1]) {
+
+                zoomLevels[i] > 2?
+                    expect(zoomLevels[i + 1]).toBe(Number((zoomLevels[i] + 1).toFixed(2))) :
+                    expect(zoomLevels[i + 1]).toBe(Number((zoomLevels[i] + 0.5).toFixed(2)));
+            }
+        }
+    });
+
+    test('should print: "Invalid step input." when zoomStepFactor.step cannot be parsed to number', async ({ page, browserName }) => {
+        test.skip(browserName === 'firefox', 'TODO: investigate why this test fails on firefox');
+        const waitForTimeout = {
+            chromium: 10000,
+            firefox: 10000,
+            webkit: 10000
+          };
+        const { consoleLogs, downloadedFiles } = await loadConsoleSample(page, waitForTimeout[browserName], 'viewing/viewing');
+        
+        const { waitForInstance } = await loadViewerSample(page, 'viewing/viewing');
+        const instance = await waitForInstance();
+        await page.waitForTimeout(5000);
+        const firstZoomLevel = await instance('getZoomLevel');
+        await instance('setZoomStepFactors', [
+            {
+                step: 'aa',
+                startZoom: 0
+            },
+            {
+                step: 100,
+                startZoom: 200
+            }
+        ]);
+
+        await page.waitForTimeout(5000);
+        const expectedConsoleLog = consoleLogs.filter(log => log.includes('Invalid step input.'))
+        expect(expectedConsoleLog.length).toBe(1);
+    });
+
+    test('should print: "Invalid step input." when zoomStepFactor.startZoom cannot be parsed to number', async ({ page, browserName }) => {
+        test.skip(browserName === 'firefox', 'TODO: investigate why this test fails on firefox');
+        const waitForTimeout = {
+            chromium: 10000,
+            firefox: 10000,
+            webkit: 10000
+          };
+        const { consoleLogs, downloadedFiles } = await loadConsoleSample(page, waitForTimeout[browserName], 'viewing/viewing');
+        
+        const { waitForInstance } = await loadViewerSample(page, 'viewing/viewing');
+        const instance = await waitForInstance();
+        await page.waitForTimeout(5000);
+        const firstZoomLevel = await instance('getZoomLevel');
+        await instance('setZoomStepFactors', [
+            {
+                step: 20,
+                startZoom: 0
+            },
+            {
+                step: 100,
+                startZoom: 'aa'
+            }
+        ]);
+
+        await page.waitForTimeout(5000);
+        const expectedConsoleLog = consoleLogs.filter(log => log.includes('Invalid startZoom input.'))
+        expect(expectedConsoleLog.length).toBe(1);
+    });
+})
--- src/apis/setZoomStepFactors.js
+++ src/apis/setZoomStepFactors.js
@@ -1,22 +1,26 @@
 /**
- * Set the zoom step size for zooming in/out.
+ * Set the zoom step size for zooming in/out. The API takes an array of zoomStepFactor that contains two properties:
+ * step: zoom step size when zooming in/out. It could be either percentage in a string or a number, ex. 50, '50', or '50%' all indicates 50%.
+ * startZoom: The zoom level that the step starts to apply. It could be either percentag in a string or a number, ex. 200, '200', or '200%' all indicates 200%.
  * @method UI.setZoomStepFactors
- * @param {zoomStepFactor[]} zoomStepFactors an array that contains objects of zoomStep and zoom start level. zoomStepFactors must contain at least one zoomStepFactor object that has startZoom: 0
- * @typedef {Object} zoomStepFactor
- * @property {number} zoomStepFactor.step zoom step size in %, should always be positive
- * @property {number} zoomStepFactor.startZoom zoom level that the zoom step size start to apply, should always be positive.
+ * @param {Object[]} zoomStepFactors An array that contains objects of zoomStep and zoom start level. zoomStepFactors must contain at least one zoomStepFactor object that has startZoom: 0
+ * @property {number|string} zoomStepFactors[].step Zoom step size as percentage in a string or a number, ex. '50', '50%', or 50 all indicates 50%.
+ * @property {number|string} zoomStepFactors[].startZoom Zoom level that the zoom step size starts to apply. It could be percentage in a string or a number, ex. '200', '200%', or 200 all indicates 200%.
  * @example
 WebViewer(...)
   .then(function(instance) {
     const documentViewer = instance.Core.documentViewer;
-
     // you must have a document loaded when calling this api
     documentViewer.addEventListener('documentLoaded', function() {
       instance.UI.setZoomStepFactors([
         {
-          step: 50,
-          startZoom: 0
-        }
+          step: '25', // indicates 25%, also accepts '25%'
+          startZoom: '0'
+        },
+        {
+          step: 50, // indicates 50%
+          startZoom: 200 // indicates the step is 50% after zoom level 200%
+        },
       ]);
     });
   });
@@ -24,31 +28,33 @@ WebViewer(...)
 
 import actions from 'actions';
 
-export default store => zoomStepFactors => {
+export default (store) => (zoomStepFactors) => {
   if (!zoomStepFactors.length) {
-    return console.warn('Invalid Input');
+    return console.warn('zoomStepFactors should have at least one zoomStepFactor object with startZoom equals to 0.');
   }
   const newZoomFactorsHasInitialZoomFactor =
-    zoomStepFactors.find(stepFactor => stepFactor.startZoom === 0);
+    zoomStepFactors.find((stepFactor) => parseFloat(stepFactor.startZoom) === 0);
   if (!newZoomFactorsHasInitialZoomFactor) {
     return console.warn('zoomStepFactors should have at least one zoomStepFactor object with startZoom equals to 0.');
   }
+  const convertedZoomStepFactors = [];
   for (let i = 0; i < zoomStepFactors.length; i++) {
-    const zoomStepFactor = zoomStepFactors[i];
-    if (
-      zoomStepFactor['step'] === undefined ||
-      typeof zoomStepFactor.step !== 'number' ||
-      zoomStepFactor['step'] < 0
-    ) {
-      return console.warn('zoomStepFactor object must have a property called "step" with a positive number as its value.');
+    const { step, startZoom } = zoomStepFactors[i];
+    const convertedZoomStepFactor = {};
+    const parsedStep = parseFloat(step);
+    if (isNaN(parsedStep)) {
+      return console.warn('Invalid step input.');
     }
-    if (
-      zoomStepFactor['startZoom'] === undefined ||
-      typeof zoomStepFactor.startZoom !== 'number' ||
-      zoomStepFactor['startZoom'] < 0
-    ) {
-      return console.warn('zoomStepFactor objectm ust have a property called "startZoom" with a positive number as its value.');
+    convertedZoomStepFactor.step = parsedStep;
+
+    const parsedStartZoom = parseFloat(startZoom);
+    if (isNaN(parsedStartZoom)) {
+      return console.warn('Invalid startZoom input.');
     }
+    convertedZoomStepFactor.startZoom = parsedStartZoom;
+
+    convertedZoomStepFactors.push(convertedZoomStepFactor);
   }
-  store.dispatch(actions.setZoomStepFactors(zoomStepFactors));
+
+  store.dispatch(actions.setZoomStepFactors(convertedZoomStepFactors));
 };
