ab43e00cb07e3b03380a7739866fa66c6fa96e3a
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-02-17 13:51:03 -0800

    [Bugfix] password modal cannot close when use wvs (#6229)
    Co-authored-by: 侯景业 <houjingye@houkeis-MacBook-Pro.local>

--- src/event-listeners/onDocumentLoaded.js
+++ src/event-listeners/onDocumentLoaded.js
@@ -11,11 +11,17 @@ import { print } from 'helpers/print';
 import outlineUtils from 'helpers/OutlineUtils';
 
 import onLayersUpdated from './onLayersUpdated';
+import i18next from 'i18next';
 
 let onFirstLoad = true;
 
-export default store => async () => {
+export default (store, documentViewerKey) => async () => {
   const { dispatch, getState } = store;
+  const docViewer = core.getDocumentViewer(documentViewerKey);
+  const documentCompletePromise = docViewer.getDocument()?.getDocumentCompletePromise();
+  documentCompletePromise?.then(() => {
+    dispatch(actions.closeElement('passwordModal'));
+  });
 
   dispatch(actions.openElement('pageNavOverlay'));
   dispatch(actions.setLoadingProgress(1));
@@ -42,109 +48,124 @@ export default store => async () => {
   }
 
   if (getHashParameters('a', false)) {
-    core.getDocumentViewer().enableAnnotations();
+    core.getDocumentViewers().forEach((documentViewer) => documentViewer.enableAnnotations());
   } else {
-    core.getDocumentViewer().disableAnnotations();
+    core.getDocumentViewers().forEach((documentViewer) => documentViewer.disableAnnotations());
   }
 
-  core.getOutlines(outlines => {
-    dispatch(actions.setOutlines(outlines));
-  });
+  // TODO compare: integrate with panels
+  if (documentViewerKey === 1) {
+    core.getOutlines((outlines) => {
+      dispatch(actions.setOutlines(outlines));
+    }, documentViewerKey);
 
-  const doc = core.getDocument();
-  doc.addEventListener('bookmarksUpdated', () => core.getOutlines(outlines => dispatch(actions.setOutlines(outlines))));
+    const doc = core.getDocument(documentViewerKey);
+    doc.addEventListener('bookmarksUpdated', () => core.getOutlines((outlines) => dispatch(actions.setOutlines(outlines)), documentViewerKey));
 
-  outlineUtils.setDoc(core.getDocument());
+    outlineUtils.setDoc(core.getDocument(documentViewerKey));
 
-  if (!doc.isWebViewerServerDocument()) {
-    doc.addEventListener('layersUpdated', async () => {
-      const newLayers = await doc.getLayersArray();
-      const currentLayers = selectors.getLayers(getState());
-      onLayersUpdated(newLayers, currentLayers, dispatch);
-    });
-    doc.getLayersArray().then(layers => {
-      if (layers.length === 0) {
-        dispatch(actions.disableElement('layersPanel', PRIORITY_ONE));
-        dispatch(actions.disableElement('layersPanelButton', PRIORITY_ONE));
+    if (!doc.isWebViewerServerDocument()) {
+      doc.addEventListener('layersUpdated', async () => {
+        const newLayers = await doc.getLayersArray();
+        const currentLayers = selectors.getLayers(getState());
+        onLayersUpdated(newLayers, currentLayers, dispatch);
+      });
+      doc.getLayersArray().then((layers) => {
+        if (layers.length === 0) {
+          dispatch(actions.disableElement('layersPanel', PRIORITY_ONE));
+          dispatch(actions.disableElement('layersPanelButton', PRIORITY_ONE));
 
-        const state = getState();
-        const activeLeftPanel = selectors.getActiveLeftPanel(state);
-        if (activeLeftPanel === 'layersPanel') {
-          // set the active left panel to another one that's not disabled so that users don't see a blank left panel
-          const nextActivePanel = getLeftPanelDataElements(state).find(
-            dataElement => !selectors.isElementDisabled(state, dataElement),
-          );
-          dispatch(actions.setActiveLeftPanel(nextActivePanel));
+          const state = getState();
+          const activeLeftPanel = selectors.getActiveLeftPanel(state);
+          if (activeLeftPanel === 'layersPanel') {
+            // set the active left panel to another one that's not disabled so that users don't see a blank left panel
+            const nextActivePanel = getLeftPanelDataElements(state).find(
+              (dataElement) => !selectors.isElementDisabled(state, dataElement),
+            );
+            dispatch(actions.setActiveLeftPanel(nextActivePanel));
+          }
+        } else {
+          dispatch(actions.enableElement('layersPanel', PRIORITY_ONE));
+          dispatch(actions.enableElement('layersPanelButton', PRIORITY_ONE));
+          onLayersUpdated(layers, undefined, dispatch);
         }
-      } else {
-        dispatch(actions.enableElement('layersPanel', PRIORITY_ONE));
-        dispatch(actions.enableElement('layersPanelButton', PRIORITY_ONE));
-        onLayersUpdated(layers, undefined, dispatch);
-      }
-    });
-  }
-
-  const docType = doc.getType();
-  if (docType === workerTypes.PDF || (docType === workerTypes.WEBVIEWER_SERVER && !doc.isWebViewerServerDocument())) {
-    dispatch(actions.enableElement('cropToolGroupButton', PRIORITY_ONE));
-    dispatch(actions.enableElement('contentEditButton', PRIORITY_ONE));
-  } else {
-    dispatch(actions.disableElement('cropToolGroupButton', PRIORITY_ONE));
-    dispatch(actions.disableElement('contentEditButton', PRIORITY_ONE));
-  }
-
-  if (core.isFullPDFEnabled()) {
-    const PDFNet = window.Core.PDFNet;
-    const docViewer = core.getDocumentViewer();
-    let isDocumentClosed = false;
-    const documentUnloadedHandler = () => {
-      isDocumentClosed = true;
-    };
-
-    const checkIfDocumentClosed = () => {
-      if (isDocumentClosed) {
-        docViewer.removeEventListener('documentUnloaded', documentUnloadedHandler);
-        throw new Error('setPageLabels is cancelled because the document got closed.');
-      }
-    };
-
-    docViewer.addEventListener('documentUnloaded', documentUnloadedHandler, { 'once': true });
-    checkIfDocumentClosed();
-    const pdfDoc = await docViewer.getDocument().getPDFDoc();
-    if (!pdfDoc) {
-      return;
+      });
     }
 
-    PDFNet.initialize().then(() => {
-      const main = async () => {
-        try {
-          checkIfDocumentClosed();
-          const pageCount = await pdfDoc.getPageCount();
-          const pageLabels = [];
+    const docType = doc.getType();
+    if (docType === workerTypes.PDF || (docType === workerTypes.WEBVIEWER_SERVER && !doc.isWebViewerServerDocument())) {
+      dispatch(actions.enableElement('cropToolGroupButton', PRIORITY_ONE));
+      dispatch(actions.enableElement('contentEditButton', PRIORITY_ONE));
+      dispatch(actions.enableElement('addParagraphToolGroupButton', PRIORITY_ONE));
+    } else if (docType === workerTypes.IMAGE) {
+      dispatch(actions.disableElement('contentEditButton', PRIORITY_ONE));
+      dispatch(actions.disableElement('addParagraphToolGroupButton', PRIORITY_ONE));
+    } else {
+      dispatch(actions.disableElement('cropToolGroupButton', PRIORITY_ONE));
+      dispatch(actions.disableElement('contentEditButton', PRIORITY_ONE));
+      dispatch(actions.disableElement('addParagraphToolGroupButton', PRIORITY_ONE));
+    }
 
-          for (let i = 1; i <= pageCount; i++) {
-            checkIfDocumentClosed();
-            const pageLabel = await pdfDoc.getPageLabel(i);
-            checkIfDocumentClosed();
-            const label = await pageLabel.getLabelTitle(i);
-            pageLabels.push(label.length > 0 ? label : i.toString());
-          }
+    if (core.isFullPDFEnabled()) {
+      const PDFNet = window.Core.PDFNet;
+      const docViewer = core.getDocumentViewer(documentViewerKey);
+      let isDocumentClosed = false;
+      const documentUnloadedHandler = () => {
+        isDocumentClosed = true;
+      };
 
-          checkIfDocumentClosed();
-          store.dispatch(actions.setPageLabels(pageLabels));
-        } catch (e) {
-          console.warn(e);
+      const checkIfDocumentClosed = () => {
+        if (isDocumentClosed) {
+          docViewer.removeEventListener('documentUnloaded', documentUnloadedHandler);
+          throw new Error('setPageLabels is cancelled because the document got closed.');
         }
       };
 
-      PDFNet.runWithCleanup(main);
-    });
+      docViewer.addEventListener('documentUnloaded', documentUnloadedHandler, { 'once': true });
+      checkIfDocumentClosed();
+      const pdfDoc = await docViewer.getDocument().getPDFDoc();
+      if (!pdfDoc) {
+        return;
+      }
+
+      PDFNet.initialize().then(() => {
+        const main = async () => {
+          try {
+            checkIfDocumentClosed();
+            const totalPageCount = await pdfDoc.getPageCount();
+            const displayedPageCount = core.getTotalPages();
+            const pageLabels = [];
+            if (totalPageCount !== displayedPageCount) {
+              const errorLoadingDocument = i18next.t('message.errorLoadingDocument', { totalPageCount, displayedPageCount });
+              store.dispatch(actions.showErrorMessage(errorLoadingDocument));
+              throw new Error(`pdfDoc.getPageCount() returns ${totalPageCount} and this does not match with documentViewer.getPageCount() which returns ${displayedPageCount}.`);
+            }
+
+            for (let i = 1; i <= totalPageCount; i++) {
+              checkIfDocumentClosed();
+              const pageLabel = await pdfDoc.getPageLabel(i);
+              checkIfDocumentClosed();
+              const label = await pageLabel.getLabelTitle(i);
+              pageLabels.push(label.length > 0 ? label : i.toString());
+            }
+
+            checkIfDocumentClosed();
+            store.dispatch(actions.setPageLabels(pageLabels));
+          } catch (e) {
+            console.warn(e);
+          }
+        };
+
+        PDFNet.runWithCleanup(main);
+      });
+    }
   }
 
   window.instance.UI.loadedFromServer = false;
   window.instance.UI.serverFailed = false;
 
-  window.documentViewer
+  const documentViewer = core.getDocumentViewer(documentViewerKey);
+  documentViewer
     .getAnnotationManager()
     .getFieldManager()
     .setPrintHandler(() => {
@@ -152,12 +173,12 @@ export default store => async () => {
         store.dispatch,
         selectors.isEmbedPrintSupported(store.getState()),
         selectors.getSortStrategy(store.getState()),
-        selectors.getColorMap(store.getState())
+        selectors.getColorMap(store.getState()),
       );
     });
 
   // init zoom level value in redux
-  dispatch(actions.setZoom(core.getZoom()));
+  dispatch(actions.setZoom(core.getZoom(documentViewerKey), documentViewerKey));
 
   fireEvent(Events.DOCUMENT_LOADED);
 };
