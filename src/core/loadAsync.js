da263ae38432088a7930f394b75af8cab7f39016
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-03-02 16:31:35 -0800

    [Misc] Clean up deprecated functions in webviewer core (#6299)

--- src/core/loadAsync.js
+++
@@ -1,6 +0,0 @@
-/**
- * https://www.pdftron.com/api/web/Core.DocumentViewer.html#loadAsync__anchor
- */
-export default (partRetriever, docOptions) => {
-  window.documentViewer.loadAsync(partRetriever, docOptions);
-};
