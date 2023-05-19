ce1901c511de3da1d96560588da50831ddc50b54
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-05-19 16:50:49 -0700

    [Misc] Added tooltip to page manipulation options button (#5683)

--- src/components/PageManipulationOverlayButton/PageManipulationOverlayButtonContainer.js
+++ src/components/PageManipulationOverlayButton/PageManipulationOverlayButtonContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Tooltip from 'components/Tooltip';
 import ToggleElementButton from '../ToggleElementButton';
 import { useDispatch, useSelector } from 'react-redux';
 import selectors from 'selectors';
@@ -8,25 +9,28 @@ function PageManipulationOverlayButtonContainer(props) {
   const { className, pageIndex } = props;
 
   const dispatch = useDispatch();
-  const selectedPageIndexes = useSelector(state => selectors.getSelectedThumbnailPageIndexes(state));
+  const selectedPageIndexes = useSelector((state) => selectors.getSelectedThumbnailPageIndexes(state));
 
   const onClickPageManipulationOverlayButton = () => {
     if (selectedPageIndexes.indexOf(pageIndex) === -1) {
       dispatch(actions.setSelectedPageThumbnails([pageIndex]));
     }
-  }
+  };
 
   return (
-    <div
-      className={className}
-      onClick={onClickPageManipulationOverlayButton}>
-      <ToggleElementButton
-        dataElement="pageManipulationOverlayButton"
-        element="pageManipulationOverlay"
-        img="icon-tool-more"
-      />
-    </div>
+    <Tooltip content="option.thumbnailPanel.moreOptions">
+      <div
+        className={className}
+        onClick={onClickPageManipulationOverlayButton}
+      >
+        <ToggleElementButton
+          dataElement="pageManipulationOverlayButton"
+          element="pageManipulationOverlay"
+          img="icon-tool-more"
+        />
+      </div>
+    </Tooltip>
   );
-};
+}
 
 export default PageManipulationOverlayButtonContainer;
