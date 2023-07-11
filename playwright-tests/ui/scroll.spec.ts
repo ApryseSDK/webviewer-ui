614b44577d11c5e76eb616a132cbe924b08fafb2
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-07-11 15:13:05 -0700

    [Bugfix] Fixed horizontal scrollbar WVR-2978 (#7180)

--- playwright-tests/ui/scroll.spec.ts
+++ playwright-tests/ui/scroll.spec.ts
@@ -1,5 +1,5 @@
 import { loadViewerSample } from '../../playwright-utils';
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('Scroll', () => {
   test('Should not scroll parent window if the display mode is not continuous', async ({ page }) => {
@@ -21,5 +21,44 @@ test.describe('Scroll', () => {
 
     const currentPage = await instance('getCurrentPage');
     expect(currentPage).toEqual(2);
+  });
+
+  test('Should not scroll parent window if the display mode is not continuous and page height is greater than the container height', async ({ page }) => {
+    const { iframe, waitForInstance } = await loadViewerSample(page, 'viewing/middle-of-the-page');
+    const instance = await waitForInstance();
+    await page.waitForTimeout(5000);
+
+    const documentContainer = await iframe.$('[data-element=documentContainer]');
+
+    const initialScrollY = await page.evaluate(() => window.scrollY);
+    const initialDocumentContainerScrollY = await documentContainer.evaluate((element) => element.scrollTop);
+
+    await instance('setZoomLevel', 1.8);
+
+    await page.waitForTimeout(2000);
+
+    const pageContainer = await iframe.$('#pageContainer1');
+    const { x, y } = await pageContainer.boundingBox();
+
+    await page.mouse.move(x + 100, y + 100);
+    await page.mouse.wheel(0, 1000);
+
+    const finalScrollY = await page.evaluate(() => window.scrollY);
+    const finalDocumentContainerScrollY = await documentContainer.evaluate((element) => element.scrollTop);
+
+    expect(finalScrollY).toEqual(initialScrollY);
+    expect(finalDocumentContainerScrollY).not.toEqual(initialDocumentContainerScrollY);
+  });
+  test('Should have scroll bar for both horizontal and vertical scroll', async ({ page }) => {
+    const { iframe, waitForInstance } = await loadViewerSample(page, 'viewing/blank');
+    const instance = await waitForInstance();
+
+    const [overflowX, overflowY] = await iframe.evaluate(async () => {
+      const scrollViewElementStyle = getComputedStyle(instance.Core.documentViewer.getScrollViewElement());
+      return [scrollViewElementStyle.overflowX, scrollViewElementStyle.overflowY];
+    });
+
+    expect(overflowX === 'hidden').toBe(false);
+    expect(overflowY === 'hidden').toBe(false);
   });
 });
