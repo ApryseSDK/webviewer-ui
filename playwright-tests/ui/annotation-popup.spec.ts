f8789bd0de9e1cdf3776ad49181ea1cfe3b8a98a
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-07-12 12:14:15 -0700

    [Bugfix] Fix FocusTrap when AnnotationPopup and ContextMenuPopup are open (#7187)

--- playwright-tests/ui/annotation-popup.spec.ts
+++ playwright-tests/ui/annotation-popup.spec.ts
@@ -1,5 +1,5 @@
 import { loadViewerSample, WebViewerInstance } from '../../playwright-utils';
-import { expect, test, Frame } from '@playwright/test';
+import { expect, test, Frame, ElementHandle } from '@playwright/test';
 import { drawRectangle } from '../common/rectangle';
 
 test.describe('Annotation Popup tests', () => {
@@ -60,5 +60,44 @@ test.describe('Annotation Popup tests', () => {
     });
     await page.waitForTimeout(500);
     expect(top >= 0).toBeTruthy();
+  });
+
+  test('Annotation popup should hand over the focus when the context menu is also open', async ({ page }) => {
+    const { iframe, waitForWVEvents, waitForInstance } = await loadViewerSample(page, 'viewing/viewing');
+    await waitForWVEvents(['pageComplete', 'annotationsLoaded']);
+    instance = await waitForInstance();
+    await page.waitForTimeout(500);
+    await iframe?.evaluate(() => {
+      const annotManager = window.instance.Core.documentViewer.getAnnotationManager();
+      const annots = annotManager.getAnnotationsList();
+      annotManager.deselectAllAnnotations();
+      annotManager.selectAnnotation(annots[0]);
+    });
+    await iframe?.waitForSelector('[data-element=annotationPopup]');
+
+    const pageContainer = await iframe?.$('#pageContainer1');
+    const { x, y } = await pageContainer?.boundingBox();
+    // right click to open the context menu, this should dismiss the annotationPopup
+    await page.mouse.click(x + 100, y + 20, { button: 'right' });
+    await iframe?.waitForSelector('[data-element=contextMenuPopup]');
+
+    const contextMenuPopup = await iframe?.$('[data-element=contextMenuPopup]');
+    const contextMenuButtons = await contextMenuPopup?.$$('.main-menu-button') as ElementHandle[];
+    // scroll document to trigger the annotationPopup to open again
+    await page.mouse.move(x + 100, y + 100);
+    await page.mouse.wheel(0, 100);
+    await page.waitForTimeout(1000);
+
+    const activeElement = await iframe?.evaluate(() => {
+      return document.activeElement?.getAttribute('data-element');
+    });
+    expect(activeElement).toBe(await contextMenuButtons[0].getAttribute('data-element'));
+    await page.waitForTimeout(1000);
+    // Tab to switch focus, this should work and not throw any error
+    await page.keyboard.press('Tab');
+    const newActiveElement = await iframe?.evaluate(() => {
+      return document.activeElement?.getAttribute('data-element');
+    });
+    expect(newActiveElement).toBe(await contextMenuButtons[1].getAttribute('data-element'));
   });
 });
