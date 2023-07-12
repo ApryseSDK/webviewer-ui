660d3f7a77d158dfe5f5ee757b7aeab7e1e0db90
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-07-11 18:30:42 -0700

    [Bugfix] Forms toolbar disappear when deselecting a form builder tool (#7184)

--- src/event-listeners/onFormFieldCreationModeEnded.js
+++ src/event-listeners/onFormFieldCreationModeEnded.js
@@ -1,12 +1,13 @@
 import actions from 'actions';
 import selectors from 'selectors';
 
-export default (dispatch, store) => () => {
+export default (dispatch, store, hotkeysManager) => () => {
   dispatch(actions.setCustomElementOverrides('downloadButton', { disabled: false }));
   dispatch(actions.setCustomElementOverrides('saveAsButton', { disabled: false }));
   dispatch(actions.setCustomElementOverrides('printButton', { disabled: false }));
   dispatch(actions.setCustomElementOverrides('filePickerButton', { disabled: false }));
-
+  dispatch(actions.enableElement('textPopup', 1));
+  hotkeysManager.on();
   // Ensure we are not left in the Forms toolbargroup when we end form builder mode
   const currentToolbarGroup = selectors.getCurrentToolbarGroup(store.getState());
   if (currentToolbarGroup === 'toolbarGroup-Forms') {
--- src/event-listeners/onFormFieldCreationModeStarted.js
+++ src/event-listeners/onFormFieldCreationModeStarted.js
@@ -1,8 +1,10 @@
 import actions from 'actions';
 
-export default (dispatch) => () => {
+export default (dispatch, hotkeysManager) => () => {
   dispatch(actions.setCustomElementOverrides('downloadButton', { disabled: true }));
   dispatch(actions.setCustomElementOverrides('saveAsButton', { disabled: true }));
   dispatch(actions.setCustomElementOverrides('printButton', { disabled: true }));
   dispatch(actions.setCustomElementOverrides('filePickerButton', { disabled: true }));
+  dispatch(actions.disableElement('textPopup', 1));
+  hotkeysManager.off();
 };
