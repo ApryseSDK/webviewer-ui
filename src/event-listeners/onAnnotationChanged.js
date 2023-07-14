6a2e695f0ad1cdf648ff7aeaf3781a5017acaefd
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-07-13 18:54:40 -0700

    [Bugfix] Undo/redo issue when annotation has replies (#7210)

--- src/event-listeners/onAnnotationChanged.js
+++ src/event-listeners/onAnnotationChanged.js
@@ -2,23 +2,23 @@ import getHashParameters from 'helpers/getHashParameters';
 import core from 'core';
 
 
-export default () => (annotations, action, info) => {
+export default (documentViewerKey) => (annotations, action, info) => {
   if (action === 'delete') {
-    deleteReplies(annotations);
+    deleteReplies(annotations, documentViewerKey, info);
   }
 
   const selectAnnotationOnCreation = getHashParameters('selectAnnotationOnCreation', false);
   if (selectAnnotationOnCreation) {
     if (action === 'add' && !info.imported) {
       if (annotations.length > 0 && !annotations[0].InReplyTo) {
-        core.selectAnnotation(annotations[0]);
+        core.selectAnnotation(annotations[0], documentViewerKey);
       }
     }
   }
 };
 
-const deleteReplies = annotations => {
-  annotations.forEach(annotation => {
-    core.deleteAnnotations(annotation.getReplies(), { 'imported': false, 'force': true });
+const deleteReplies = (annotations, documentViewerKey, info) => {
+  annotations.forEach((annotation) => {
+    core.deleteAnnotations(annotation.getReplies(), { 'imported': false, 'force': true, 'isUndoRedo': info?.isUndoRedo }, documentViewerKey);
   });
 };
