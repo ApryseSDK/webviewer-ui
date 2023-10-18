6142145bb9da273af3578b3649b1197b01682024
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-10-18 13:58:24 -0700

    [Bugfix] multi select note state popup wvr-3587 (#7723)

--- src/components/NoteState/NoteStatePopup.js
+++ src/components/NoteState/NoteStatePopup.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import useOnClickOutside from 'hooks/useOnClickOutside';
+import DataElements from 'constants/dataElement';
+
 import DataElementWrapper from 'components/DataElementWrapper';
 import PopupPortal from 'components/PopupPortal';
 import Icon from 'components/Icon';
@@ -26,9 +28,11 @@ const NoteStatePopup = ({
   const [t] = useTranslation();
   const [position, setPosition] = useState({ left: 'auto', right: 'auto', top: 'auto' });
   const popupRef = useRef();
+  const isMultiSelect = triggerElementName === DataElements.NOTE_MULTI_STATE_BUTTON;
 
   useOnClickOutside(popupRef, (e) => {
-    const triggerElement = getRootNode().querySelector(`[data-id="${triggerElementName}"]`);
+    const querySelector = isMultiSelect ? `[data-element="${DataElements.NOTE_MULTI_STATE_BUTTON}"]` : `[data-id="${triggerElementName}"]`;
+    const triggerElement = getRootNode().querySelector(querySelector);
     const clickedTrigger = triggerElement.contains(e.target);
     if (!clickedTrigger) {
       // we only want to close the popup if we clicked outside and not on the trigger
@@ -43,7 +47,7 @@ const NoteStatePopup = ({
   };
 
   useEffect(() => {
-    const position = getOverlayPositionBasedOn(triggerElementName, popupRef, false, 'data-id');
+    const position = getOverlayPositionBasedOn(triggerElementName, popupRef, false, isMultiSelect ? 'data-element' : 'data-id');
     setPosition(position);
   }, []);
 
--- tests/ui/test-ui-sample.test.js
+++ tests/ui/test-ui-sample.test.js
@@ -243,7 +243,7 @@ describe('Test UI APIs', function() {
     TriangleAnnotation.prototype.elementName = 'triangle';
     const triangleTool = new TriangleCreateTool(documentViewer);
 
-    UI.enableFeatures([UI.Feature.MultiViewerMode]);
+    UI.enterMultiViewerMode();
 
     const multiViewerPromise = new Promise((resolve) => {
       instance.UI.addEventListener(UI.Events.MULTI_VIEWER_READY, resolve);
@@ -276,23 +276,25 @@ describe('Test UI APIs', function() {
     };
     const instance = await setupWebViewerInstance(options);
 
-    // create a promise that resolves when the document loaded event fires
-    const documentLoadedPromise = new Promise((resolve) => {
-      instance.UI.addEventListener('documentLoaded', resolve);
+    const { documentViewer } = instance.Core;
+
+    // create a promise that resolves when the annotations loaded event fires
+    const annotationsLoadedPromise = new Promise((resolve) => {
+      documentViewer.addEventListener('annotationsLoaded', resolve);
     });
-    await documentLoadedPromise;
-  
+    await annotationsLoadedPromise;
+
     // click on the toggleNotesButton data element and wait for the notes panel to open
     const iframe = document.querySelector('#viewerDiv iframe');
     const notesToggleButton = iframe.contentDocument.querySelector('[data-element="toggleNotesButton"]');
     simulateClick(notesToggleButton);
     await delay(100);
-    
+
     // click on the filter button and wait for the filter modal to open
     let filterButton = iframe.contentDocument.querySelector('[data-element="filterAnnotationButton"]');
     simulateClick(filterButton);
     await delay(100);
-  
+
     // click through on the filter modal tab
     const filterModalTypeButton = iframe.contentDocument.querySelector('[data-element="annotationTypeFilterPanelButton"]');
     simulateClick(filterModalTypeButton);
@@ -300,21 +302,71 @@ describe('Test UI APIs', function() {
     // click on the filter modal option
     const filterModalOptionCheckbox = iframe.contentDocument.querySelector('[data-element="annotationTypeFilterPanel"] .ui__choice__label');
     simulateClick(filterModalOptionCheckbox);
-    
+
     // click on the filter modal apply button
     const filterModalApplyButton = iframe.contentDocument.querySelector('.filter-annot-apply');
     simulateClick(filterModalApplyButton);
 
     // assert that the filter button is active
     expect(filterButton.classList.contains('active')).to.equal(true);
-  
+
     // click on the toggleNotesButton data element twice to close and open the panel
     simulateClick(notesToggleButton);
     simulateClick(notesToggleButton);
-    await delay(100);    
-  
+    await delay(100);
+
     // assert that the filter button is still active
     filterButton = iframe.contentDocument.querySelector('[data-element="filterAnnotationButton"]');
     expect(filterButton.classList.contains('active')).to.equal(true);
+  });
+
+  // requires running with a production build locally
+  it('test that the multi note status popup appears when the button is clicked', async () => {
+    const options = {
+      initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+    };
+    const instance = await setupWebViewerInstance(options);
+
+    const { documentViewer, annotationManager } = instance.Core;
+
+    // create a promise that resolves when the annotations loaded event fires
+    const annotationsLoadedPromise = new Promise((resolve) => {
+      documentViewer.addEventListener('annotationsLoaded', resolve);
+    });
+    await annotationsLoadedPromise;
+
+    // click on the toggleNotesButton data element and wait for the notes panel to open
+    const iframe = document.querySelector('#viewerDiv iframe');
+    const notesToggleButton = iframe.contentDocument.querySelector('[data-element="toggleNotesButton"]');
+    simulateClick(notesToggleButton);
+    await delay(100);
+
+    // click on the multiSelectMode button and wait to enter multi select mode
+    const multiSelectModeButton = iframe.contentDocument.querySelector('[data-element="multiSelectModeButton"]');
+    simulateClick(multiSelectModeButton);
+    await delay(100);
+
+    // select some annotations and wait for the multi select status button to activate
+    const annotations = annotationManager.getAnnotationsList();
+    annotationManager.selectAnnotations([annotations[0], annotations[1]]);
+    await delay(100);
+    expect(annotationManager.getSelectedAnnotations().length).to.equal(2);
+
+    // click the multi select status button
+    const multiStateButton = iframe.contentDocument.querySelector('[data-element="multiStateButton"]');
+    expect(multiStateButton).to.not.equal(null);
+    expect(multiStateButton.classList.contains('disabled')).to.equal(false);
+    simulateClick(multiStateButton);
+
+    // check for note state popup
+    const notePopupState = iframe.contentDocument.querySelector('[data-element="notePopupState"]');
+    expect(notePopupState).to.not.equal(null);
+
+    // click on the note state popup Accepted option
+    const notePopupStateAccepted = iframe.contentDocument.querySelector('[data-element="notePopupStateAccepted"]');
+    simulateClick(notePopupStateAccepted);
+
+    expect(annotations[0].getStatus()).to.equal('Accepted');
+    expect(annotations[1].getStatus()).to.equal('Accepted');
   });
 });
