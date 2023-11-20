906f7e32e309ac57c4219d55bb56965d1409f4a4
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-11-20 12:03:01 -0800

    [Misc] Quick fix on the modular header check types for the set style (#7924)

--- src/apis/ModularComponents/modularHeader.js
+++ src/apis/ModularComponents/modularHeader.js
@@ -1,5 +1,8 @@
 import actions from 'actions';
-import { ITEM_TYPE, ALIGNMENT, DEFAULT_GAP } from 'constants/customizationVariables';
+import { ITEM_TYPE, DEFAULT_GAP, PLACEMENT, DEFAULT_STYLES } from 'constants/customizationVariables';
+import { isGapValid, isJustifyContentValid } from 'components/ModularComponents/Helpers/validation-helper';
+
+const { checkTypes, TYPES } = window.Core;
 
 export default (store) => (props) => {
   class ModularHeader {
@@ -7,20 +10,24 @@ export default (store) => (props) => {
       const {
         label,
         dataElement,
-        alignment,
+        justifyContent,
         grow = 0,
         float = false,
         gap = DEFAULT_GAP,
         position,
         placement,
+        opacityMode,
+        opacity,
         maxWidth,
         maxHeight,
         items = [],
+        style = {},
+        stroke = true,
       } = props;
       this.label = label;
       this.dataElement = dataElement;
       this.placement = placement;
-      this.alignment = alignment;
+      this.justifyContent = justifyContent;
       this.grow = grow;
       this.gap = gap;
       this.position = position;
@@ -30,12 +37,121 @@ export default (store) => (props) => {
       // items is a list of things. We want to clone them
       this.items = items.map((item) => ({ ...item }));
       this.itemValidTypes = Object.values(ITEM_TYPE);
+      this.opacityMode = opacityMode;
+      this.opacity = opacity;
+
+      this.stroke = stroke;
+      this.dimension = {
+        paddingTop: DEFAULT_STYLES.PADDING,
+        paddingBottom: DEFAULT_STYLES.PADDING,
+        borderWidth: DEFAULT_STYLES.WIDTH,
+      };
+
+      this.setStyle(style);
+    }
+
+    setStyle(userDefinedStyle) {
+      checkTypes([userDefinedStyle], [TYPES.OPTIONAL([TYPES.OBJECT({
+        padding: TYPES.OPTIONAL(TYPES.NUMBER),
+        borderColor: TYPES.OPTIONAL(TYPES.STRING),
+        borderWidth: TYPES.OPTIONAL(TYPES.STRING),
+        borderStyle: TYPES.OPTIONAL(TYPES.STRING),
+      })])], 'ModularHeader.setStyle');
+      if (Object.keys(userDefinedStyle).length === 0) {
+        this.style = {};
+        return;
+      }
+
+      // If user pass 'border-style: solid' the stroke should appear
+      if (userDefinedStyle.hasOwnProperty('borderStyle')) {
+        this.stroke = true;
+      }
+
+      const {
+        padding,
+        borderColor = DEFAULT_STYLES.BORDER_COLOR,
+        borderWidth = DEFAULT_STYLES.WIDTH,
+        borderStyle = DEFAULT_STYLES.BORDER_STYLE,
+      } = userDefinedStyle;
+
+      if (!padding) {
+        this.style = { ...userDefinedStyle };
+        return;
+      }
+
+      // Calculate top & bottom padding for dimensions
+      // If it is `padding: '10px'`, top & bottom padding will be 10 padding
+      // If it is `padding: '10px 5px 20px'`, top is 10, bottom is 20 padding
+      // User also can pass padding like this: `padding: 15`, then top-bottom is 15 padding
+      const parsePadding = (paddingStr) => {
+        const parts = paddingStr.split(' ').map((part) => parseInt(part, 10));
+        return (parts.length <= 2)
+          ? { paddingTop: parts[0], paddingBottom: parts[0] }
+          : { paddingTop: parts[0], paddingBottom: parts[2] };
+      };
+
+      const { paddingTop, paddingBottom } = typeof padding === 'string'
+        ? parsePadding(padding) : { paddingTop: padding, paddingBottom: padding };
+
+      this.dimension.paddingTop = paddingTop;
+      this.dimension.paddingBottom = paddingBottom;
+      this.dimension.borderWidth = parseInt(borderWidth, 10);
+
+      const style = { ...userDefinedStyle };
+
+      // Clear out border direction styles, e.g., borderBottomWidth, borderTopStyle
+      // New border styles will be applied according this.placement
+      for (const prop in style) {
+        if (prop.includes('border')) {
+          delete style[prop];
+        }
+      }
+
+      if (this.stroke) {
+        // This function return border style for given placement
+        // e.g. if the placement is Right it will return "borderLeftStyle"
+        const borderProperty = (prop) => {
+          if (this.float) {
+            return `border${prop}`;
+          }
+          let side = '';
+          switch (this.placement) {
+            case PLACEMENT.LEFT:
+              side = 'Right';
+              break;
+            case PLACEMENT.RIGHT:
+              side = 'Left';
+              break;
+            case PLACEMENT.BOTTOM:
+              side = 'Top';
+              break;
+            case PLACEMENT.TOP:
+              side = 'Bottom';
+              break;
+            default:
+              break;
+          }
+          return `border${side}${prop}`;
+        };
+
+        style[borderProperty('Style')] = borderStyle;
+        style[borderProperty('Width')] = borderWidth;
+        style[borderProperty('Color')] = borderColor;
+      }
+
+      this.style = style;
+    }
+
+    getDimensionTotal() {
+      const { paddingTop, paddingBottom, borderWidth } = this.dimension;
+      return paddingTop + paddingBottom + borderWidth;
     }
 
     setGap(gap) {
-      this.gap = gap;
-      // What if the header already exists in redux? we must update it
-      store.dispatch(actions.setGapBetweenHeaderItems(this.dataElement, gap));
+      if (isGapValid(gap)) {
+        this.gap = gap;
+        store.dispatch(actions.setGapBetweenHeaderItems(this.dataElement, gap));
+      }
     }
 
     // Validates the items type of a grouped item
@@ -99,28 +215,28 @@ export default (store) => (props) => {
       }
     };
 
-    setAlignment = (alignment) => {
-      const validAlignments = Object.values(ALIGNMENT);
-      if (!validAlignments.includes(alignment)) {
-        console.warn(`${alignment} is not a valid value for alignment. Please use one of the following: ${validAlignments}`);
-        return;
+    setJustifyContent = (justifyContent) => {
+      if (isJustifyContentValid(justifyContent)) {
+        this.justifyContent = justifyContent;
+        store.dispatch(actions.setHeaderJustifyContent(this.dataElement, justifyContent));
       }
-      store.dispatch(actions.setHeaderAlignment(this.dataElement, alignment));
     };
 
     setMaxWidth = (maxWidth) => {
-      if (isNaN(maxWidth)) {
+      if (isNaN(maxWidth) || maxWidth < 0) {
         console.warn(`${maxWidth} is not a valid value for maxWidth. Please use a number, which represents the maximum width of the header in pixels.`);
         return;
       }
+      this.maxWidth = maxWidth;
       store.dispatch(actions.setHeaderMaxWidth(this.dataElement, maxWidth));
     };
 
     setMaxHeight = (maxHeight) => {
-      if (isNaN(maxHeight)) {
+      if (isNaN(maxHeight) || maxHeight < 0) {
         console.warn(`${maxHeight} is not a valid value for maxHeight. Please use a number, which represents the maximum height of the header in pixels.`);
         return;
       }
+      this.maxHeight = maxHeight;
       store.dispatch(actions.setHeaderMaxHeight(this.dataElement, maxHeight));
     };
   }
