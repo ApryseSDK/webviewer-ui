e36e6221bf645dcdfb82f161cc8b255bb366c2fc
Kishan-Singh-ArtworkFlow <kishan.s@bizongo.com>
2023-11-29 15:30:18 +0530

    commented parallel scroll

--- src/components/MultiViewer/MultiViewer.js
+++ src/components/MultiViewer/MultiViewer.js
@@ -242,28 +242,28 @@ const MultiViewer = () => {
     }
   }, [width, width2]);
 
-  useEffect(() => {
-    const removeListeners = () => {
-      const hasSecondViewer = !!container2.current;
-      if (hasSecondViewer) {
-        container2.current.removeEventListener('scroll', onScrollDifferMode)
-      }
-    };
-    const addEventListeners = () => {
-      const hasSecondViewer = !!container2.current;
-      if (hasSecondViewer) {
-        container2.current.addEventListener('scroll', onScrollDifferMode);
-      }
-    };
+  // useEffect(() => {
+  //   const removeListeners = () => {
+  //     const hasSecondViewer = !!container2.current;
+  //     if (hasSecondViewer) {
+  //       container2.current.removeEventListener('scroll', onScrollDifferMode)
+  //     }
+  //   };
+  //   const addEventListeners = () => {
+  //     const hasSecondViewer = !!container2.current;
+  //     if (hasSecondViewer) {
+  //       container2.current.addEventListener('scroll', onScrollDifferMode);
+  //     }
+  //   };
 
-    if (currentReadOnlyMode.current) {
-      addEventListeners();
-    } else {
-      removeListeners();
-    }
-    return removeListeners;
+  //   if (currentReadOnlyMode.current) {
+  //     addEventListeners();
+  //   } else {
+  //     removeListeners();
+  //   }
+  //   return removeListeners;
 
-  }, [currentReadOnlyMode.current])
+  // }, [currentReadOnlyMode.current])
 
 
   useEffect(() => {
@@ -273,11 +273,11 @@ const MultiViewer = () => {
     }
   }, [initialSetup]);
 
-  const onScrollDifferMode = (e) => {
-    const { scrollTop, scrollLeft } = e.target;
-    container.current.scrollTop = scrollTop;
-    container.current.scrollLeft = scrollLeft;
-  }
+  // const onScrollDifferMode = (e) => {
+  //   const { scrollTop, scrollLeft } = e.target;
+  //   container.current.scrollTop = scrollTop;
+  //   container.current.scrollLeft = scrollLeft;
+  // }
 
   const setCurrentReadOnlyMode = (e) => {
     currentReadOnlyMode.current = e;
