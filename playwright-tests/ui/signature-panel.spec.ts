e03eb48d0c2c55c7191e453cf0b30e78b00850be
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2023-12-06 08:52:25 -0800

    [Bugfix] Allow new signatures to appear in digital signatures panel (#8016)

--- playwright-tests/ui/signature-panel.spec.ts
+++ playwright-tests/ui/signature-panel.spec.ts
@@ -1,5 +1,64 @@
 import { loadViewerSample } from '../../playwright-utils';
 import { expect, test } from '@playwright/test';
+import { drawRectangle } from '../common/rectangle';
+
+const signatureConfig = [
+  { x: 20, y: 20, width: 150, height: 50 },
+  { x: 20, y: 400, width: 150, height: 50 }
+]
+
+/**
+  * @ignore
+  * Setup up the Signature Panel and switches WV to Form Builder mode
+  * @param page
+  * @param iframe
+  * @param instance
+  */
+async function setupSignaturePanels(page, iframe, instance) {
+  await instance('setToolbarGroup', 'toolbarGroup-Forms');
+  await iframe?.evaluate(() => {
+    window.instance.UI.enableFeatures([window.instance.UI.Feature.SavedSignaturesTab]);
+    window.instance.UI.enableElements(['leftPanel', 'signaturePanelButton']);
+    window.instance.UI.setToolMode(window.instance.Core.Tools.SignatureFormFieldCreateTool);
+  });
+
+  await iframe.click('[data-element=leftPanelButton]');
+  await page.waitForTimeout(200);
+  await iframe.click('[data-element=signaturePanelButton]');
+}
+
+/**
+ * @ignore
+ * Draws two signatures to the document and applies them by exiting Form Builder mode
+ * @param page
+ * @param iframe
+ * @param pageContainer
+ */
+async function drawSignaturesAndApply(page, iframe, pageContainer) {
+  const { x: xContainer, y: yContainer } = await pageContainer.boundingBox();
+
+  await drawRectangle(page, xContainer + signatureConfig[0].x, yContainer + signatureConfig[0].y, signatureConfig[0].width, signatureConfig[0].height);
+  await drawRectangle(page, xContainer + signatureConfig[1].x, yContainer + signatureConfig[1].y, signatureConfig[1].width, signatureConfig[1].height);
+
+  await page.waitForTimeout(1000);
+  await iframe.click('[data-element=applyFormFieldButton]');
+  await page.waitForTimeout(1000);
+}
+
+/**
+ * @ignore
+ * Small routine to setup the Signature mode, draw two sigantures to the document and apply them
+ * We than switch out of Form Builder mode to confirm the Panel updates
+ * @param page
+ * @param iframe
+ * @param instance
+ * @param pageContainer
+ */
+async function drawTwoSignaturesRoutine(page, iframe, instance, pageContainer) {
+  await setupSignaturePanels(page, iframe, instance);
+  await page.waitForTimeout(1000);
+  await drawSignaturesAndApply(page, iframe, pageContainer);
+}
 
 test.describe('Signature Panel', () => {
   test('should show the same time zone for both signing and verification', async ({ page }) => {
@@ -17,5 +76,36 @@ test.describe('Signature Panel', () => {
     });
 
     expect(areTimeZonesTheSame).toBeTruthy();
+  });
+
+  test('I can populate the Signature Panel with new items', async ({ page }) => {
+    const { iframe, waitForInstance } = await loadViewerSample(page, 'full-apis/ViewerEditTest/');
+    const instance = await waitForInstance();
+    await page.waitForTimeout(5000);
+    const pageContainer = await iframe.$('#pageContainer1');
+
+    await drawTwoSignaturesRoutine(page, iframe, instance, pageContainer);
+
+    const signatures = await iframe.$$('.signature-widget-info');
+    expect(signatures.length).toBe(2);
+  });
+
+  test('items are removed from the Signature Panel when the item is deleted', async ({ page }) => {
+    const { iframe, waitForInstance } = await loadViewerSample(page, 'full-apis/ViewerEditTest/');
+    const instance = await waitForInstance();
+    await page.waitForTimeout(5000);
+    const pageContainer = await iframe.$('#pageContainer1');
+
+    await drawTwoSignaturesRoutine(page, iframe, instance, pageContainer);
+
+    await instance('setToolbarGroup', 'toolbarGroup-Forms');
+    await iframe?.evaluate(() => {
+      const annotList = window.instance.Core.annotationManager.getAnnotationsList();
+      window.instance.Core.annotationManager.deleteAnnotations(annotList);
+    });
+    await page.waitForTimeout(1000);
+
+    const signatures = await iframe.$$('.signature-widget-info');
+    expect(signatures.length).toBe(0);
   });
 });
--- src/components/SignaturePanel/SignaturePanel.js
+++ src/components/SignaturePanel/SignaturePanel.js
@@ -22,12 +22,16 @@ const SignaturePanel = () => {
     isDisabled,
     certificate,
     trustLists,
-    currentLanguage
+    currentLanguage,
+    revocationChecking,
+    revocationProxyPrefix,
   ] = useSelector((state) => [
     selectors.isElementDisabled(state, 'signaturePanel'),
     selectors.getCertificates(state),
     selectors.getTrustLists(state),
-    selectors.getCurrentLanguage(state)
+    selectors.getCurrentLanguage(state),
+    selectors.getIsRevocationCheckingEnabled(state),
+    selectors.getRevocationProxyPrefix(state),
   ]);
   const [translate] = useTranslation();
 
@@ -40,14 +44,73 @@ const SignaturePanel = () => {
     dispatch(actions.setVerificationResult({}));
   }, [setShowSpinner, dispatch]);
 
+  const onAnnotationChanged = ((annotations, action) => {
+    const isInFormCreationMode = core.getAnnotationManager().getFormFieldCreationManager().isInFormFieldCreationMode();
+
+    if (action === 'add') {
+      addSignatureWidgetAnnotations(core.getAnnotationManager().getAnnotationsList());
+    } else if (action === 'delete' && isInFormCreationMode) {
+      removeAnnotations(annotations);
+    }
+  });
+
+  const addNonSignedFields = () => {
+    const currentAnnotations = core.getAnnotationManager().getAnnotationsList();
+    addSignatureWidgetAnnotations(currentAnnotations);
+  };
+
+  const addSignatureWidgetAnnotations = (annotations) => {
+    const currentFields = [];
+    annotations.forEach((annotation) => {
+      if (annotation instanceof window.Core.Annotations.SignatureWidgetAnnotation) {
+        currentFields.push(annotation.getField());
+      }
+    });
+    const newSet = new Set(currentFields);
+    setFields([...newSet]);
+  };
+
+  const removeAnnotations = (annotations) => {
+    annotations.forEach((annotation) => {
+      removeMatchingWidget(annotation);
+    });
+
+    addNonSignedFields();
+  };
+
+  const removeMatchingWidget = (annotation) => {
+    const annot = annotation;
+    const widgetFieldName = annot.getCustomData('trn-form-field-name');
+    const isRectanglePlaceholder = annotation instanceof window.Core.Annotations.RectangleAnnotation && widgetFieldName;
+    if (isRectanglePlaceholder) {
+      const annotationManager = core.getAnnotationManager();
+      const annotationList = annotationManager.getAnnotationsList();
+      const widgetToDelete = annotationList.filter((annotation) => {
+        return annotation.getCustomData('trn-editing-rectangle-id') === annot.Id;
+      });
+      annotationManager.deleteAnnotations(widgetToDelete);
+    }
+  };
+
+  const resetFields = () => {
+    setFields([]);
+    addNonSignedFields();
+  };
+
   useEffect(() => {
     // This ensures that when the document loads, the state of this component is
     // updated accordingly
     core.addEventListener('documentLoaded', onDocumentLoaded);
     core.addEventListener('documentUnloaded', onDocumentUnloaded);
+    core.addEventListener('annotationChanged', onAnnotationChanged);
+    core.addEventListener('formFieldCreationModeStarted', resetFields);
+    core.addEventListener('formFieldCreationModeEnded', resetFields);
     return () => {
       core.removeEventListener('documentLoaded', onDocumentLoaded);
       core.removeEventListener('documentUnloaded', onDocumentUnloaded);
+      core.removeEventListener('annotationChanged', onAnnotationChanged);
+      core.removeEventListener('formFieldCreationModeStarted', resetFields);
+      core.removeEventListener('formFieldCreationModeEnded', resetFields);
     };
   }, [onDocumentUnloaded]);
 
@@ -60,12 +123,11 @@ const SignaturePanel = () => {
       // Field will not exist in the document
       core.getAnnotationsLoadedPromise().then(() => {
         setShowSpinner(true);
-        setVerificationResult(document, certificate, trustLists, currentLanguage, dispatch)
+        setVerificationResult(document, certificate, trustLists, currentLanguage, revocationChecking, revocationProxyPrefix, dispatch)
           .then(async (verificationResult) => {
             const fieldManager = core.getAnnotationManager().getFieldManager();
             setFields(Object.keys(verificationResult).map((fieldName) => fieldManager.getField(fieldName)));
             setCertificateErrorMessage('');
-            setShowSpinner(false);
           })
           .catch((e) => {
             if (e && e.message) {
@@ -73,6 +135,12 @@ const SignaturePanel = () => {
             } else {
               console.error(e);
             }
+          })
+          .then(() => {
+            addNonSignedFields();
+          })
+          .finally(() => {
+            setShowSpinner(false);
           });
       });
     } else {
