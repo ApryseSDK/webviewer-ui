4129f3803a231356074a4e8045097353022d97f5
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2024-01-18 16:51:54 -0800

    [Bugfix] Context menu is in wrong state (#8193)

--- src/hooks/useOnContextMenuOpen/useOnContextMenuOpen.js
+++ src/hooks/useOnContextMenuOpen/useOnContextMenuOpen.js
@@ -1,22 +1,23 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import selectors from 'selectors';
 import actions from 'actions';
 import core from 'core';
 import DataElements from 'constants/dataElement';
-
 import useOnRightClick from 'hooks/useOnRightClick';
-
 import { isMobile as isMobileCSS, isMobileDevice } from 'helpers/device';
+import { isOfficeEditorMode } from 'helpers/officeEditor';
 
 export default function useOnContextMenuOpen() {
   const [
     popupItems,
     isRightClickAnnotationPopupEnabled,
+    activeDocumentViewerKey,
   ] = useSelector(
     (state) => [
       selectors.getPopupItems(state, DataElements.CONTEXT_MENU_POPUP),
       selectors.isRightClickAnnotationPopupEnabled(state),
+      selectors.getActiveDocumentViewerKey(state),
     ],
     shallowEqual,
   );
@@ -29,18 +30,27 @@ export default function useOnContextMenuOpen() {
   // if it's on, on tablet + phone, it will be available without being draggable
   const isMobile = !!isMobileDevice || isMobileCSS();
 
-  useOnRightClick(
-    useCallback((e) => {
-      const { pageX: left, pageY: top } = e;
+  // Use this to store isRightClickAnnotationPopupEnabled value to avoid stale closure
+  const isRightClickAnnotationPopupEnabledRef = useRef();
 
-      const annotationUnderMouse = core.getAnnotationByMouseEvent(e);
-      if ((!isRightClickAnnotationPopupEnabled && !isMobile) || (isRightClickAnnotationPopupEnabled && !annotationUnderMouse)) {
+  useEffect(() => {
+    isRightClickAnnotationPopupEnabledRef.current = isRightClickAnnotationPopupEnabled;
+  }, [isRightClickAnnotationPopupEnabled]);
+
+  useOnRightClick(
+    useCallback(async (e) => {
+      const { pageX: left, pageY: top } = e;
+      const annotationUnderMouse = core.getAnnotationByMouseEvent(e, activeDocumentViewerKey);
+      if ((!isRightClickAnnotationPopupEnabledRef.current && !isMobile) || (isRightClickAnnotationPopupEnabledRef.current && !annotationUnderMouse)) {
         if (popupItems.length > 0) {
           setClickPosition({ left, top });
+          if (isOfficeEditorMode()) {
+            await core.getOfficeEditor().onRightClick(e);
+          }
           dispatch(actions.openElement(DataElements.CONTEXT_MENU_POPUP));
         }
       }
-    }, [popupItems, isRightClickAnnotationPopupEnabled])
+    }, [popupItems, activeDocumentViewerKey])
   );
 
   return { clickPosition };
