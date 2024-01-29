3d8af66af051282e461abecb705da430c9ed9d94
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2024-01-29 13:32:53 -0800

    [Misc] Update UI.setNoteDateFormat jsdoc (#8244)

--- src/apis/setNoteDateFormat.js
+++ src/apis/setNoteDateFormat.js
@@ -1,5 +1,6 @@
 /**
- * Sets the format for displaying the date when a note is create/modified. A list of formats can be found {@link https://github.com/iamkun/dayjs/blob/master/docs/en/API-reference.md#format-formatstringwithtokens-string dayjs API}.
+ * Sets the format for displaying the date when a note is create/modified. The default format is 'MMM D, LT'.
+ * A list of formats can be found {@link https://day.js.org/docs/en/display/format dayjs API}.
  * @method UI.setNoteDateFormat
  * @param {string} format The format of date to display
  * @example
@@ -11,6 +12,6 @@ WebViewer(...)
 
 import actions from 'actions';
 
-export default store => noteDateFormat => {
+export default (store) => (noteDateFormat) => {
   store.dispatch(actions.setNoteDateFormat(noteDateFormat));
 };
