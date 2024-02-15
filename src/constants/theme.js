1395589c0e20988ea65612d173e86c192ee87977
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2024-02-15 09:27:59 -0800

    [Misc] Theme is not available in types.d.ts (#8382)

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,6 +1,6 @@
 /**
  * Contains string enumeration for all themes for WebViewer. They are used to set the viewer theme.
- * @typedef UI.Theme
+ * @name UI.Theme
  * @property {string} DARK The theme where the WebViewer will be dark.
  * @property {string} LIGHT The theme where the WebViewer will be light.
  * @example
