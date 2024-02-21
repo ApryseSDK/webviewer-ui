42cb124ede2aea9c91a352d352a9c5532344bc82
Bryan Fox <bryanfox1986@gmail.com>
2024-02-21 14:29:15 -0500

    playwright + snapshots

--- playwright-tests/ui/page-navigation-overlay.spec.ts
+++ playwright-tests/ui/page-navigation-overlay.spec.ts
@@ -1,16 +1,30 @@
-import { loadViewerSample } from '../../playwright-utils';
-import { expect, test } from '@playwright/test';
+import { WebViewerInstance, loadViewerSample } from '../../playwright-utils';
+import { Frame, expect, test } from '@playwright/test';
 
 test.describe('Page navigation overlay', () => {
-  test('Should show page labels', async ({ page }) => {
-    const { iframe, waitForInstance } = await loadViewerSample(page, 'viewing/viewing-with-pdf-prime');
-    const instance = await waitForInstance();
+  let iframe: Frame;
+  let instance: WebViewerInstance;
+
+  test.beforeEach(async ({ page }) => {
+    const { iframe: sampleIframe, waitForInstance } = await loadViewerSample(page, 'viewing/viewing-with-pdf-prime');
+    iframe = sampleIframe as Frame;
+    instance = await waitForInstance();
     await instance('enableElements', ['pageNavOverlay']);
     await instance('disableFadePageNavigationComponent');
     await instance('loadDocument', '/test-files/51-A4-layout.pdf');
     await page.waitForTimeout(10000);
-
-    const pageNavigationOverlay = await iframe.$('.PageNavOverlay');
-    expect(await pageNavigationOverlay.screenshot()).toMatchSnapshot(['page-navigation-overlay', 'page-nav-overlay-labels.png']);
   });
-})
+
+  test('Should show page labels', async () => {
+    const pageNavigationOverlay = await iframe.$('.PageNavOverlay');
+    expect(await pageNavigationOverlay?.screenshot()).toMatchSnapshot(['page-navigation-overlay', 'page-nav-overlay-labels.png']);
+  });
+
+  test('Should set page labels correctly using setPageLabels API with fullAPI', async () => {
+    const newLabels = ['i', 'ii', 'iii'];
+    await instance('setPageLabels', newLabels);
+    const pageNavigationOverlay = await iframe.$('.PageNavOverlay');
+    const label = await pageNavigationOverlay?.$('input');
+    expect(await label?.inputValue()).toBe(newLabels[0]);
+  });
+});
