013ada3cd18c8c2e1a142075593e265e8bf4b8f0
webviewer-ui <72055195+webviewer-ui@users.noreply.github.com>
2024-05-31 14:49:57 -0700

    [Misc] Modular UI JSON Console Warnings (#9035) (master → master)

--- src/apis/exportModularComponents.js
+++ src/apis/exportModularComponents.js
@@ -1,34 +1,46 @@
 /**
- * Exports the current state of the modular components, headers, and panels in the UI as JSON data. Removes any functions found in the components or invalid items after validation.
+ * Exports the current state of the modular components, headers, and panels in the UI as JSON data. Removes any invalid items after validation. Also removes any prebuilt flyouts and overflow flyouts. Functions found in components such as onClick cannot be exported in JSON data. Use a Function Map to redefine them on import. See https://docs.apryse.com/api/web/UI.html#.importModularComponents__anchor for more information.
  * @method UI.exportModularComponents
- * @return {Object} JSON object containing maps of components, headers, and panels to used in the UI. ex: { modularComponents: { ... }, modularHeaders: { ... }, panels: { ... } }
+ * @return {Object} JSON object containing maps of components, headers, panels, and flyouts to be used in the UI. ex: { modularComponents: { ... }, modularHeaders: { ... }, panels: { ... }, flyouts: { ... } }
  * @example
  * WebViewer(...)
  .then(function (instance) {
     instance.UI.exportModularComponents();
  */
 import { PANEL_TYPE } from './addPanel';
+import { ITEM_TYPE, PREBUILT_FLYOUTS, OVERFLOW_FLYOUTS } from 'constants/customizationVariables';
 
 export default (store) => () => {
   const state = store.getState();
   const { checkTypes } = window.Core;
 
-  const validateComponents = (component) => {
-    for (const key in component) {
-      const value = component[key];
+  const validateComponents = (componentObject) => {
+    for (const key in componentObject) {
+      if (key === 'undefined' || key === 'null') {
+        console.warn(`Null or undefined item found. Removing item: ${key} found in components`);
+        delete componentObject[key];
+        continue;
+      }
+      const value = componentObject[key];
       if (typeof value === 'object') {
         // Recursively validate nested objects
-        component[key] = validateComponents(value);
+        componentObject[key] = validateComponents(value);
         // Check if the object is empty after validation and remove it
-        if (Object.keys(component[key]).length === 0) {
-          delete component[key];
+        if (Object.keys(componentObject[key]).length === 0) {
+          delete componentObject[key];
         }
       } else if (typeof value === 'function') {
-        console.warn(`Function values not supported in export JSON. Removing item: ${key} found in component: ${JSON.stringify(component)}`);
-        delete component[key];
+        const storedModularComponentFunctions = state.viewer.modularComponentFunctions;
+        const component = componentObject.dataElement || JSON.stringify(componentObject);
+        if (Object.values(storedModularComponentFunctions).includes(value)) {
+          componentObject[key] = Object.keys(storedModularComponentFunctions).find((funcKey) => storedModularComponentFunctions[funcKey] === value);
+        } else {
+          console.warn(`Function in ${key} of ${component} not found in Function Map and cannot be included in export JSON. Ensure that the following is included to your Function Map to define it on import:\n`, `\nfunctionMap: {\n  ...,\n  '${'{functionName}'}': ${value},\n}\n`, '\nSee https://docs.apryse.com/api/web/UI.html#.importModularComponents__anchor for more information.');
+          componentObject[key] = '';
+        }
       }
     }
-    return component;
+    return componentObject;
   };
 
   const validateHeaders = (modularHeaders) => {
@@ -50,6 +62,7 @@ export default (store) => () => {
   const modularComponents = validateComponents(state.viewer.modularComponents);
   const modularHeaders = validateHeaders(state.viewer.modularHeaders);
   const panelList = state.viewer.genericPanels;
+  const flyoutMap = state.viewer.flyoutMap;
 
   const convertPanelsToMap = (panelsArray) => {
     const panelsMap = {};
@@ -62,12 +75,55 @@ export default (store) => () => {
     return panelsMap;
   };
 
+  const prepareFlyouts = (flyouts) => {
+    const normalizedFlyouts = {};
+    for (const key in flyouts) {
+      const flyout = flyouts[key];
+      // Skip prebuilt flyouts and overflow flyouts
+      if (flyout.className && [...PREBUILT_FLYOUTS, ...OVERFLOW_FLYOUTS].includes(flyout.className)) {
+        continue;
+      }
+      const items = processItems(flyout.items, key);
+      normalizedFlyouts[key] = { ...flyout, items };
+    }
+    return normalizedFlyouts;
+  };
+
+  const processItems = (items, parentKey) => {
+    return items.map((item) => {
+      if (item.children && Array.isArray(item.children)) {
+        item.children = processItems(item.children, item.dataElement); // Recursively process children
+      }
+      if (item === ITEM_TYPE.DIVIDER || (item.type && item.type === ITEM_TYPE.DIVIDER)) {
+        return ITEM_TYPE.DIVIDER;
+      }
+      if (!item.dataElement && item) {
+        console.warn(`Invalid item found in flyout: ${parentKey} - ${JSON.stringify(item)} missing dataElement`);
+        return null;
+      }
+      const allowedTypes = [ITEM_TYPE.PRESET_BUTTON, ITEM_TYPE.BUTTON, ITEM_TYPE.STATEFUL_BUTTON, ITEM_TYPE.TOGGLE_BUTTON];
+      if (!allowedTypes.includes(item.type)) {
+        const allowedTypesString = allowedTypes.join(', ');
+        console.warn(`Only ${allowedTypesString} items are supported in export JSON. Removing item: ${item.dataElement} found in component: ${parentKey}`);
+        return null;
+      }
+      if (!modularComponents[item.dataElement]) {
+        const newComponent = validateComponents({ [item.dataElement]: item });
+        modularComponents[item.dataElement] = newComponent[item.dataElement];
+      }
+
+      return item.dataElement;
+    }).filter((item) => item !== null);
+  };
+
   const panels = convertPanelsToMap(panelList);
+  const flyouts = prepareFlyouts(flyoutMap);
 
   const allData = {
     modularComponents,
     modularHeaders,
     panels,
+    flyouts,
   };
 
   return allData;
--- src/apis/importModularComponents.js
+++ src/apis/importModularComponents.js
@@ -1,26 +1,42 @@
 /**
- * Creates a new modular UI using the provided JSON data. The data is validated and will throw an error if a part of its structure is not valid. Existing UI components will be replaced with the new components provided in the JSON data.
+ * Creates a new modular UI using the provided JSON data and function map. The data is validated and will throw an error if a part of its structure is not valid. Existing UI components will be replaced with the new components provided in the JSON data.
  * @method UI.importModularComponents
  * @param {Object} data The JSON data containing maps of components, headers, and panels to be used in the UI. If any of the properties are not provided, they will not be used in the UI. ex: { modularComponents: { ... }, modularHeaders: { ... }, panels: { ... } }
  * @property {Object} modularComponents The map of components to be used in the UI. Refer to: {@link ItemProperties} for properties of modularComponents
  * @property {Object} modularHeaders The map of headers to be used in the UI. Refer to: {@link ContainerProperties} for properties of modularHeaders
  * @property {Object} panels The map of panels to be used in the UI. Refer to: {@link PanelProperties} for properties of panels
+ * @property {Object} flyouts The map of flyouts to be used in the UI.
+ * @property {string} flyouts.dataElement A unique string that identifies the flyout.
+ * @property {Array<string>} flyouts.items An array of strings that represent the items in the flyout. Each string should be the dataElement of a component in the modularComponents map.
+ * @param {object} functionMap A map of functions to be used in the components. The keys should match the function names used in the components. The values should be the actual functions to be called.
  * @example
  * WebViewer(...)
  .then(function (instance) {
     const response = await fetch('./modular-components.json');
     const data = await response.json();
 
-    instance.UI.importModularComponents(data);
+      const functionMap = {
+      'alertClick': () => alert("Alert triggered!"),
+      'singlePageOnClick': (update) => {
+        update('DoublePage');
+      },
+      'doublePageOnClick': (update) => {
+        update('SinglePage');
+      },
+      'statefulButtonMount': () => {},
+      'statefulButtonUnmount': () => {},
+      };
+
+    instance.UI.importModularComponents(data, functionMap);
  */
 import actions from 'actions';
 import setPanels from './setPanels';
-import { ITEM_TYPE } from 'constants/customizationVariables';
+import { ITEM_TYPE, PREBUILT_FLYOUTS } from 'constants/customizationVariables';
 import { panelNames } from 'constants/panel';
 
 const { checkTypes, TYPES } = window.Core;
 
-const validateComponents = (components) => {
+const validateComponents = (components, functionMap) => {
   const normalizedComponent = TYPES.OBJECT({
     dataElement: TYPES.STRING,
     type: TYPES.ONE_OF(Object.values(ITEM_TYPE)),
@@ -28,7 +44,16 @@ const validateComponents = (components) => {
   const componentKeys = Object.keys(components);
   componentKeys.forEach((key) => {
     try {
+      if (components[key]._dataElement && !components[key].dataElement) {
+        components[key].dataElement = components[key]._dataElement;
+      }
       checkTypes([components[key]], [normalizedComponent], 'UI.importModularComponents.validateComponents');
+      const COMPONENT_FUNCTION_KEYS = ['onClick', 'mount', 'unmount'];
+      Object.keys(components[key]).forEach((prop) => {
+        if (COMPONENT_FUNCTION_KEYS.includes(prop) && (!components[key][prop] || !functionMap[components[key][prop]])) {
+          console.warn(`Function: ${components[key][prop] || prop} not defined on component: ${key}. Use Function Map to define it. See https://docs.apryse.com/api/web/UI.html#.importModularComponents__anchor for more information.`);
+        }
+      });
       if (components[key].items) {
         components[key].items.forEach((item) => {
           if (!components[item]) {
@@ -37,7 +62,7 @@ const validateComponents = (components) => {
         });
       }
     } catch (error) {
-      throw new Error(`Invalid component found: ${key} - ${error.message}`);
+      throw new Error(`Import has been aborted.\nInvalid component found: ${key} - ${error.message}`);
     }
   });
 };
@@ -51,7 +76,7 @@ const validateHeaders = (headers, components) => {
   headerKeys.forEach((key) => {
     try {
       checkTypes([headers[key]], [normalizedHeader], 'UI.importModularComponents.validateHeaders');
-      if (headers[key].items) {
+      if (headers[key].items?.length > 0) {
         headers[key].items.forEach((item) => {
           if (!components[item]) {
             throw new Error(`Invalid item found in header: ${key} - ${item} not found in components`);
@@ -59,7 +84,7 @@ const validateHeaders = (headers, components) => {
         });
       }
     } catch (error) {
-      throw new Error(`Invalid header found: ${key} - ${error.message}`);
+      throw new Error(`Import has been aborted.\nInvalid header found: ${key} - ${error.message}`);
     }
   });
 };
@@ -75,19 +100,61 @@ const validatePanels = (panels) => {
     try {
       checkTypes([panels[key]], [normalizedPanel], 'UI.importModularComponents.validatePanels');
     } catch (error) {
-      throw new Error(`Invalid panel found: ${key} - ${error.message}`);
+      throw new Error(`Import has been aborted.\nInvalid panel found: ${key} - ${error.message}`);
+    }
+  });
+};
+
+const flyoutItemType = TYPES.OBJECT({
+  dataElement: TYPES.STRING,
+  type: TYPES.ONE_OF([ITEM_TYPE.PRESET_BUTTON, ITEM_TYPE.BUTTON, ITEM_TYPE.STATEFUL_BUTTON, ITEM_TYPE.TOGGLE_BUTTON]),
+  children: TYPES.OPTIONAL(TYPES.ARRAY(TYPES.STRING)) // Recursive type reference
+});
+
+
+const validateFlyoutItems = (items, components, key) => {
+  items.forEach((item) => {
+    if (item === ITEM_TYPE.DIVIDER) {
+      return; // Skip validation for dividers
+    }
+    if (!components[item]) {
+      throw new Error(`Invalid item found in flyout: ${key} - ${item} not found in components`);
+    }
+    checkTypes([components[item]], [flyoutItemType], `UI.importModularComponents.validateFlyouts - ${JSON.stringify(components[item])}`);
+
+    // Recursively validate children if they exist
+    if (components[item].children && Array.isArray(components[item].children)) {
+      validateFlyoutItems(components[item].children, components, key);
     }
   });
 };
 
 
-const validateJSONStructure = (jsonData) => {
+const validateFlyouts = (flyouts, components) => {
+  const normalizedFlyout = TYPES.OBJECT({
+    dataElement: TYPES.STRING,
+    items: TYPES.ARRAY(TYPES.STRING)
+  });
+  const flyoutKeys = Object.keys(flyouts);
+  flyoutKeys.forEach((key) => {
+    try {
+      checkTypes([flyouts[key]], [normalizedFlyout], 'UI.importModularComponents.validateFlyouts');
+      if (flyouts[key].items?.length > 0) {
+        validateFlyoutItems(flyouts[key].items, components, key);
+      }
+    } catch (error) {
+      throw new Error(`Invalid flyout found: ${key} - ${error.message}`);
+    }
+  });
+};
+
+const validateJSONStructure = (jsonData, functionMap) => {
   if (!jsonData) {
     throw new Error('No data provided');
   }
 
   if (jsonData.modularComponents) {
-    validateComponents(jsonData.modularComponents);
+    validateComponents(jsonData.modularComponents, functionMap);
   }
 
   if (jsonData.modularHeaders) {
@@ -97,17 +164,83 @@ const validateJSONStructure = (jsonData) => {
   if (jsonData.panels) {
     validatePanels(jsonData.panels);
   }
+
+  if (jsonData.flyouts) {
+    validateFlyouts(jsonData.flyouts, jsonData.modularComponents);
+  }
 };
 
-export default (store) => async (data) => {
-  validateJSONStructure(data);
-
-  const headersMap = data.modularHeaders || {};
-  const componentMap = data.modularComponents || {};
-  const panels = data.panels || {};
+export default (store) => async (components, functions = {}) => {
+  validateJSONStructure(components, functions);
+  const headersMap = components.modularHeaders || {};
+  const componentMap = components.modularComponents || {};
+  const panels = components.panels || {};
+  const flyouts = components.flyouts || {};
 
   const panelList = Object.values(panels).map((panel) => panel);
 
+  store.dispatch(actions.setModularComponentFunctions(functions));
+
+  const getFunctionFromFunctionMap = (functionString) => {
+    const storedModularComponentFunctions = store.getState().viewer.modularComponentFunctions;
+    return storedModularComponentFunctions[functionString] || (() => {});
+  };
+
+  Object.values(componentMap).forEach((component) => {
+    if (component.type === ITEM_TYPE.BUTTON) {
+      component.onClick = getFunctionFromFunctionMap(component.onClick);
+    }
+    if (component.type === ITEM_TYPE.STATEFUL_BUTTON) {
+      for (const key in component.states) {
+        const state = component.states[key];
+        if (state.onClick) {
+          state.onClick = getFunctionFromFunctionMap(state.onClick);
+        }
+      }
+      component.mount = getFunctionFromFunctionMap(component.mount);
+      component.unmount = getFunctionFromFunctionMap(component.unmount);
+    }
+  });
+
   store.dispatch(actions.setModularHeadersAndComponents(componentMap, headersMap));
   setPanels(store)(panelList);
+
+  // keep prebuilt flyouts
+  const allFlyouts = store.getState().viewer.flyoutMap;
+  const prebuiltFlyouts = {};
+  Object.keys(allFlyouts).forEach((key) => {
+    const flyoutClassName = allFlyouts[key].className;
+    if (flyoutClassName && PREBUILT_FLYOUTS.includes(flyoutClassName)) {
+      prebuiltFlyouts[key] = allFlyouts[key];
+    }
+  });
+
+  // clear out existing flyouts
+  store.dispatch(actions.setFlyouts({}));
+
+  const mapItems = (items) => {
+    return items.map((item) => {
+      if (item === ITEM_TYPE.DIVIDER) {
+        return ITEM_TYPE.DIVIDER;
+      }
+      if (typeof item === 'string') {
+        item = componentMap[item];
+      }
+      if (item.children) {
+        item.children = mapItems(item.children);
+      }
+      return item;
+    });
+  };
+
+  // Add normalized flyouts
+  Object.values(flyouts).forEach((flyout) => {
+    flyout.items = mapItems(flyout.items);
+    store.dispatch(actions.addFlyout(flyout));
+  });
+
+  // add prebuilt flyouts
+  Object.values(prebuiltFlyouts).forEach((flyout) => {
+    store.dispatch(actions.addFlyout(flyout));
+  });
 };
