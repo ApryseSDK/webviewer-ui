73f43d6f3b4ec11c41c671b9b44dfeefd63d9bb2
webviewer-ui <webviewer-ui@apryse.com>
2025-11-19 10:24:51 -0800

    [Bugfix] Cleanup aro promise leak 11.9 (#12139) (r11.9 → 11.9)

--- src/event-listeners/onPageComplete.js
+++ src/event-listeners/onPageComplete.js
@@ -3,6 +3,10 @@ import core from 'core';
 
 export default (store) => async (pageNumber) => {
   const state = store.getState();
+  if (!state.viewer.shouldAddA11yContentToDOM) {
+    return;
+  }
+
   const documentViewerKey = state.viewer.activeDocumentViewerKey;
   const aroModeManager = core.getDocumentViewer().getAccessibleReadingOrderManager();
   const isStructuredFile = await aroModeManager.isStructuredFile();
